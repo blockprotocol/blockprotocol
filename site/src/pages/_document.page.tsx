09816acbd82cc5c9f69f17acf1fa08dc3119e25b
Akash Joshi <akash.joshi@viit.ac.in>
2021-10-14 13:28:52 +0530

    fix: replace svg logo with png

--- site/src/pages/_document.page.tsx
+++ site/src/pages/_document.page.tsx
@@ -23,7 +23,7 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap"
             rel="stylesheet"
           ></link>
-          <link rel="shortcut icon" href="/assets/logo.svg" />
+          <link rel="shortcut icon" href="/assets/logo.png" />
         </Head>
         <body>
           <Main />
