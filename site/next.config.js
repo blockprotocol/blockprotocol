8f89e54eba43ef65f62b57706419c9b63726a29a
Alexander Kachkaev <alexander@kachkaev.ru>
2021-12-07 22:43:53 +0000

    Disable eslint and tsc in `next build`

--- site/next.config.js
+++ site/next.config.js
@@ -6,6 +6,13 @@ module.exports = withFonts(
     pageExtensions: ["page.ts", "page.tsx", "api.ts"],
     webpack5: false,
     optimizeFonts: true,
+
+    // We call linters in GitHub Actions for all pull requests. By not linting
+    // again during `next build`, we save CI minutes and unlock more feedback.
+    // Thus, we can get Playwright test results and Preview releases for WIP PRs.
+    eslint: { ignoreDuringBuilds: true },
+    typescript: { ignoreBuildErrors: true },
+
     async redirects() {
       return [{ source: "/", destination: "/partners", permanent: false }];
     },
