b308d24b09d50033928b158e39951abd0729a27e
teenoh <ugbalaachinike@gmail.com>
2021-12-13 16:35:45 +0100

    ch: update theme override file

--- site/theme-override.d.ts
+++ site/theme-override.d.ts
@@ -1,12 +1,13 @@
 declare module "@mui/material/styles" {
   interface Palette {
     purple: {
-      50: string;
+      100: string;
       200: string;
       300: string;
       400: string;
       500: string;
       600: string;
+      700: string;
       subtle: string;
     };
     gray: {
