f5c25c56c8afe1fc550cd5b898379314559a5914
Alexander Kachkaev <alexander@kachkaev.ru>
2021-12-18 17:11:35 +0000

    Embedding application implementation

--- site/src/util/mdxComponents.tsx
+++ site/src/util/mdxComponents.tsx
@@ -22,20 +22,6 @@ export const mdxComponents: Record<string, React.ReactNode> = {
   Typography,
   InfoCardWrapper,
   InfoCard,
-  SomethingToThinkAbout: ({ children }: { children: ReactNode }) => {
-    return (
-      <div
-        style={{
-          border: "1px solid black",
-          margin: "10px -15px",
-          padding: "0 15px",
-        }}
-      >
-        <div style={{ fontSize: "3em", textAlign: "center" }}>🤔</div>
-        {children}
-      </div>
-    );
-  },
   h1: (props: TypographyProps) => {
     return (
       <Link href="#">
