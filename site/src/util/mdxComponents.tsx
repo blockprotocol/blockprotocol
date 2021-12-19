efb111a70d4bc18e5578dc7ff0cbbf270f9e80fb
Alexander Kachkaev <alexander@kachkaev.ru>
2021-12-19 23:45:11 +0000

    Convert spec to MDX (#28)

--- site/src/util/mdxComponents.tsx
+++ site/src/util/mdxComponents.tsx
@@ -1,7 +1,7 @@
 import slugify from "slugify";
-import { ReactNode } from "react";
+import { HTMLAttributes, HTMLProps, ReactNode } from "react";
 import { TypographyProps, Typography, Box, Paper } from "@mui/material";
-import { Link, LinkProps } from "../components/Link";
+import { Link } from "../components/Link";
 import { InfoCardWrapper } from "../components/InfoCard/InfoCardWrapper";
 import { InfoCard } from "../components/InfoCard/InfoCard";
 
@@ -55,5 +55,69 @@ export const mdxComponents: Record<string, React.ReactNode> = {
   p: (props: TypographyProps) => (
     <Typography mb={2} variant="bpBodyCopy" {...props} />
   ),
-  a: (props: LinkProps) => <Link {...props} />,
+  a: (props: HTMLProps<HTMLAnchorElement>) => {
+    const { href, ref, ...rest } = props;
+    void ref;
+    return href ? (
+      <Link {...rest} href={href} />
+    ) : (
+      // eslint-disable-next-line jsx-a11y/anchor-has-content -- special case for creating bookmarks (for cross-linking)
+      <a id={props.id} />
+    );
+  },
+
+  // TODO: Improve style & implementation of below components
+
+  Frame: ({ children, emoji }: { children?: ReactNode; emoji?: ReactNode }) => {
+    return (
+      <div
+        style={{
+          border: "1px solid black",
+          margin: "10px -15px",
+          padding: "0 15px",
+        }}
+      >
+        <div style={{ fontSize: "3em", textAlign: "center" }}>{emoji}</div>
+        {children}
+      </div>
+    );
+  },
+  ol: (props: HTMLAttributes<HTMLOListElement>) => (
+    <ol style={{ margin: 20, listStyle: "auto" }} {...props} />
+  ),
+  ul: (props: HTMLAttributes<HTMLUListElement>) => (
+    <ul style={{ margin: 20, listStyle: "unset" }} {...props} />
+  ),
+  inlineCode: (props: HTMLAttributes<HTMLElement>) => (
+    <code
+      {...props}
+      // TODO: link to theme
+      style={{ color: "#d18d5b", fontSize: "95%" }}
+    />
+  ),
+  code: (props: HTMLAttributes<HTMLElement>) => {
+    const isLanguageBlockMethod = props.className === "language-block-method";
+    if (isLanguageBlockMethod) {
+      const anchor = `${props.children}`.match(/^[\w]+/)?.[0] ?? "";
+      return (
+        <div id={anchor} style={{ fontWeight: "bold", color: "#d18d5b" }}>
+          <Link href={`#${anchor}`}>{props.children}</Link>
+        </div>
+      );
+    }
+
+    return (
+      <code
+        {...props}
+        // TODO: link to theme
+        style={{
+          color: "#d18d5b",
+          fontSize: "95%",
+          display: "block",
+          paddingBottom: 20,
+          textShadow: "none",
+        }}
+      />
+    );
+  },
 };
