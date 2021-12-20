d815025821d476d36dfbc2e61aefccf622fcf80b
Akash Joshi <akash.joshi@viit.ac.in>
2021-12-20 16:37:30 +0530

    feat: add most of the content

---
+++ site/src/components/pages/home/BlocksSlider.tsx
@@ -0,0 +1,219 @@
+import { Box, Typography, useMediaQuery, useTheme } from "@mui/material";
+import { useMemo, useRef, VoidFunctionComponent } from "react";
+import Slider, { Settings } from "react-slick";
+import { ArrowLeftIcon } from "../../SvgIcon/ArrowLeft";
+import { ArrowRightIcon } from "../../SvgIcon/ArrowRight";
+
+// @todo this should be populated with data from registry
+// @todo build reusable carousel component
+
+const ITEMS = [
+  {
+    id: 1,
+    title: "Table",
+    image: "/assets/table-block.svg",
+    hideBoxShadow: false,
+  },
+  {
+    id: 2,
+    title: "Code",
+    image: "/assets/code-block.svg",
+    hideBoxShadow: false,
+  },
+  {
+    id: 3,
+    title: "Headings",
+    image: "/assets/heading-block.svg",
+    hideBoxShadow: true,
+  },
+  {
+    id: 4,
+    title: "Table",
+    image: "/assets/table-block.svg",
+    hideBoxShadow: false,
+  },
+  {
+    id: 5,
+    title: "Code",
+    image: "/assets/code-block.svg",
+    hideBoxShadow: false,
+  },
+  {
+    id: 6,
+    title: "Headings",
+    image: "/assets/heading-block.svg",
+    hideBoxShadow: true,
+  },
+  {
+    id: 7,
+    title: "Headings",
+    image: "/assets/heading-block.svg",
+    hideBoxShadow: true,
+  },
+  {
+    id: 9,
+    title: "Code",
+    image: "/assets/code-block.svg",
+    hideBoxShadow: false,
+  },
+  {
+    id: 10,
+    title: "Headings",
+    image: "/assets/heading-block.svg",
+    hideBoxShadow: true,
+  },
+  {
+    id: 8,
+    title: "Headings",
+    image: "/assets/heading-block.svg",
+    hideBoxShadow: true,
+  },
+];
+
+export const BlocksSlider: VoidFunctionComponent = () => {
+  const slider = useRef<Slider>();
+
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
+
+  const settings = useMemo(
+    () =>
+      ({
+        centerMode: true,
+        infinite: false,
+        slidesToShow: isMobile ? 1 : 3,
+        slidesToScroll: 1,
+        swipeToSlide: true,
+        initialSlide: 0,
+        nextArrow: <Box sx={{ display: "none !important" }} />,
+        prevArrow: <Box sx={{ display: "none !important" }} />,
+        customPaging: () => (
+          <Box
+            sx={{
+              height: "12px",
+              width: "12px",
+              borderRadius: "50%",
+            }}
+          />
+        ),
+        dots: true,
+      } as Settings),
+    [isMobile],
+  );
+
+  return (
+    <Box
+      sx={{
+        backgroundColor: ({ palette }) => palette.gray[20],
+        overflow: "hidden",
+
+        width: "100%",
+        mb: 6,
+        position: "relative",
+        zIndex: 2,
+        py: 8,
+        // "& .slick-slide": {
+        //   // mx: 2,
+        //   margin: "0 32px",
+        // },
+        // "& .slick-list": {
+        //   margin: "0 -32px",
+        // },
+        "& .slick-dots": {
+          "li div": {
+            backgroundColor: ({ palette }) => palette.gray[30],
+          },
+          "li.slick-active div": {
+            backgroundColor: ({ palette }) => palette.purple[700],
+          },
+        },
+      }}
+    >
+      <Slider
+        ref={(node) => {
+          if (node) {
+            slider.current = node;
+          }
+        }}
+        {...settings}
+      >
+        {ITEMS.map(({ title, image, id, hideBoxShadow }) => (
+          <Box
+            key={id}
+            sx={{
+              px: 2,
+            }}
+          >
+            <Typography sx={{ textAlign: "left", mb: 1.5 }}>{title}</Typography>
+            <Box
+              component="img"
+              sx={{
+                display: "block",
+                width: "100%",
+                boxShadow: hideBoxShadow ? 0 : 2,
+              }}
+              src={image}
+            />
+          </Box>
+        ))}
+      </Slider>
+      <Box>
+        {["prev", "next"].map((position) => (
+          <Box
+            key={position}
+            sx={{
+              position: "absolute",
+              display: { xs: "none", md: "flex" },
+              alignItems: "center",
+              top: 0,
+              bottom: 0,
+              ...(position === "prev"
+                ? {
+                    left: 0,
+                    pl: 4.5,
+                    justifyContent: "flex-start",
+                  }
+                : {
+                    right: 0,
+                    pr: 4.5,
+                    justifyContent: "flex-end",
+                  }),
+              width: 200,
+              zIndex: 10,
+              background: `linear-gradient(to ${
+                position === "prev" ? "right" : "left"
+              }, #FFFFFF 55%, transparent)`,
+            }}
+          >
+            <Box
+              sx={{
+                height: 44,
+                width: 44,
+                borderRadius: "50%",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                backgroundColor: "white",
+                cursor: "pointer",
+                //   check support for filter
+                filter: `drop-shadow(0px 1px 2px rgba(27, 58, 117, 0.12)) drop-shadow(0px 4px 8px rgba(27, 58, 117, 0.07))`,
+                //   boxShadow: 1
+              }}
+              onClick={() =>
+                position === "next"
+                  ? slider.current?.slickNext()
+                  : slider.current?.slickPrev()
+              }
+            >
+              {position === "prev" ? (
+                <ArrowLeftIcon sx={{ width: 24, height: 24 }} />
+              ) : (
+                <ArrowRightIcon sx={{ width: 24, height: 24 }} />
+              )}
+            </Box>
+          </Box>
+        ))}
+      </Box>
+    </Box>
+  );
+};
--- site/src/pages/hub/[block].page.tsx
+++ site/src/pages/hub/[block].page.tsx
@@ -1,8 +1,29 @@
-import { Breadcrumbs, Container, Typography, Icon } from "@mui/material";
+import {
+  Breadcrumbs,
+  Container,
+  Typography,
+  Icon,
+  Box,
+  useMediaQuery,
+  useTheme,
+  Tab,
+  Tabs,
+} from "@mui/material";
+import { Validator } from "jsonschema";
 import { NextPage } from "next";
-import React from "react";
+import { useRouter } from "next/router";
+import React, {
+  useEffect,
+  useMemo,
+  useState,
+  VoidFunctionComponent,
+} from "react";
+import { tw } from "twind";
 
 import { Link, LinkProps } from "../../components/Link";
+import { BlocksSlider } from "../../components/pages/home/BlocksSlider";
+import { Snippet } from "../../components/Snippet";
+import { BlockMetadata } from "../api/blocks.api";
 
 const ChevronRight: React.VFC = () => (
   <Icon sx={{ fontSize: "0.8rem" }} className="fas fa-chevron-right" />
@@ -14,25 +35,268 @@ const BreadcrumbLink: React.FC<LinkProps> = ({ children, ...props }) => (
   </Link>
 );
 
-const Block: NextPage = () => {
-  const name = "Video";
-  const slug = "video";
+const validator = new Validator();
+
+/* eslint-disable global-require */
+const blockDependencies = {
+  react: require("react"),
+  "react-dom": require("react-dom"),
+  twind: require("twind"),
+};
+
+type BlockExports = { default: React.FC };
+/** @sync @hashintel/block-protocol */
+type BlockSchema = Record<string, any>;
+type BlockDependency = keyof typeof blockDependencies;
+
+interface PageState {
+  schema?: BlockSchema;
+  metadata?: BlockMetadata;
+  blockModule?: BlockExports;
+}
+
+const blockRequire = (name: BlockDependency) => {
+  if (!(name in blockDependencies)) {
+    throw new Error(`missing dependency ${name}`);
+  }
+
+  return blockDependencies[name];
+};
+
+const blockEval = (source: string): BlockExports => {
+  const exports_ = {};
+  const module_ = { exports: exports_ };
+
+  // eslint-disable-next-line no-new-func
+  const moduleFactory = new Function("require", "module", "exports", source);
+  moduleFactory(blockRequire, module_, exports_);
+
+  return module_.exports as BlockExports;
+};
+
+const Bullet: VoidFunctionComponent = () => {
+  return (
+    <span style={{ color: "#DDE7F0", marginLeft: 5, marginRight: 5 }}>•</span>
+  );
+};
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
 
   return (
-    <Container>
-      <Breadcrumbs separator={<ChevronRight />}>
-        <BreadcrumbLink href="/">Home</BreadcrumbLink>
-        <BreadcrumbLink href="/hub">Block Hub</BreadcrumbLink>
-        <BreadcrumbLink href={`/hub/${slug}`}>{name}</BreadcrumbLink>
-      </Breadcrumbs>
-      <Typography variant="bpHeading1" mt={4}>
-        <img
-          style={{ display: "inline-block" }}
-          alt="icon"
-          src="/blocks/@hash/video/public/play-box-outline.svg"
-        />{" "}
-        {name}
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+const Block: NextPage = () => {
+  const org = "@hash";
+  const { block } = useRouter().query;
+
+  const [text, setText] = useState("{}");
+  const [{ metadata, schema, blockModule }, setPageState] = useState<PageState>(
+    {},
+  );
+
+  const theme = useTheme();
+
+  const md = useMediaQuery(theme.breakpoints.up("md"));
+  const isDesktopSize = md;
+
+  // console.log({ metadata, schema, blockModule });
+
+  useEffect(() => {
+    if (!org || !block) return;
+    void fetch(`/blocks/${org}/${block}/metadata.json`)
+      .then((res) => res.json())
+      .then((metadata_) =>
+        Promise.all([
+          metadata_,
+          fetch(`/blocks/${org}/${block}/${metadata_.schema}`).then((res) =>
+            res.json(),
+          ),
+          fetch(`/blocks/${org}/${block}/${metadata_.source}`).then((res) =>
+            res.text(),
+          ),
+        ]),
+      )
+      .then(([metadata_, schema_, source]) => {
+        setPageState({
+          metadata: metadata_,
+          schema: schema_,
+          blockModule: blockEval(source),
+        });
+      });
+  }, [org, block]);
+
+  /** used to recompute props and errors on dep changes (caching has no benefit here) */
+  const [props, errors] = useMemo<[object | undefined, string[]]>(() => {
+    let result;
+
+    try {
+      result = JSON.parse(text);
+    } catch (err) {
+      return [result, [(err as Error).message]];
+    }
+
+    const errorMessages = validator
+      .validate(result, schema ?? {})
+      .errors.map((err) => `ValidationError: ${err.stack}`);
+
+    return [result, errorMessages];
+  }, [text, schema]);
+
+  const [currentTab, setCurrentTab] = useState(0);
+
+  if (!metadata || !schema) return null;
+
+  return (
+    <Container sx={{ marginTop: 5, marginBottom: 10 }}>
+      {!isDesktopSize && (
+        <Breadcrumbs separator={<ChevronRight />}>
+          <BreadcrumbLink href="/">Home</BreadcrumbLink>
+          <BreadcrumbLink href="/hub">Block Hub</BreadcrumbLink>
+          <BreadcrumbLink href={`/hub/${block}`}>
+            {metadata.displayName}
+          </BreadcrumbLink>
+        </Breadcrumbs>
+      )}
+
+      <Box sx={{ display: "flex" }}>
+        <Typography variant="bpHeading1">
+          <Box
+            sx={{ display: "inline-block", height: "2em", width: "2em" }}
+            component="img"
+            src={`/blocks/@hash/video/${metadata.icon}`}
+          />
+        </Typography>
+        <div>
+          <Typography variant="bpHeading1" mt={4}>
+            {metadata.displayName}
+          </Typography>
+          <div>{metadata.description}</div>
+          <div>
+            By <Link href="https://hash.ai/@hash">{org}</Link> <Bullet /> V
+            {metadata.version} <Bullet /> Updated Recently
+          </div>
+        </div>
+      </Box>
+
+      <div className="flex flex-row mb-10">
+        <div className={tw`w-3/5 pr-5`}>
+          <div
+            style={{ height: 320 }}
+            className={tw`bg-white rounded-2xl w-full p-4`}
+          >
+            {blockModule && <blockModule.default {...props} />}
+          </div>
+        </div>
+        <div className={tw`w-2/5`}>
+          <Tabs
+            value={currentTab}
+            onChange={(_event, newValue: number) => setCurrentTab(newValue)}
+          >
+            <Tab label="Input Schema" />
+            <Tab label="Input Data" />
+          </Tabs>
+          <TabPanel value={currentTab} index={0}>
+            <div
+              style={{ height: 320, fontSize: 14 }}
+              className={tw`rounded-2xl bg-gray-800 p-3 w-full`}
+            >
+              <Snippet
+                className={tw`font-mono overflow-scroll h-full`}
+                source={JSON.stringify(schema, null, 2)}
+                language="json"
+              />
+            </div>
+          </TabPanel>
+          <TabPanel value={currentTab} index={1}>
+            <div
+              style={{ height: 320, fontSize: 14 }}
+              className={tw`rounded-2xl bg-white p-3 w-full`}
+            >
+              <textarea
+                value={text}
+                onChange={(event) => setText(event.target.value)}
+                style={{ minHeight: "100%" }}
+                className={tw`font-mono resize-none bg-white w-full overflow-scroll`}
+                placeholder="Your block input goes here..."
+              />
+            </div>
+          </TabPanel>
+        </div>
+      </div>
+
+      <div className="flex flex-row mb-10">
+        <div className={tw`w-3/5 pr-5`}>
+          <b>About</b>
+          <p>
+            Store information in rows and columns in a classic table layout.
+            Longer description talking about parameters and how to use like a
+            readme goes in here. Tables have filters, search, ability to add and
+            remove columns and rows, multiple views. Tables have filters,
+            search, ability to add and remove columns and rows, multiple views.
+            Tables have filters, search, ability to add and remove columns and
+            rows, multiple views. Tables have filters, search, ability to add
+            and remove columns and rows, multiple views.
+          </p>
+        </div>
+        <div className={tw`w-2/5`}>
+          <b>Repository</b>
+          <Box sx={{ display: "flex" }}>
+            <img
+              alt="GitHub Link"
+              style={{ marginRight: 5 }}
+              src="/assets/link.svg"
+            />{" "}
+            <Link href="https://github.com/hash/video">
+              github.com/hash/video
+            </Link>
+          </Box>
+        </div>
+      </div>
+      <div>
+        {errors.length ? (
+          <p className={tw`mb-2`}>
+            The provided input raised the following errors:
+          </p>
+        ) : (
+          <p className={tw`mb-2`}>
+            The provided input is schema conform. See below the rendered output.
+          </p>
+        )}
+        {errors.length > 0 && (
+          <ul className={tw`rounded-2xl list-square mb-2 px-8 py-4 bg-red-200`}>
+            {errors.map((err) => (
+              <li key={err}>{err}</li>
+            ))}
+          </ul>
+        )}
+      </div>
+
+      <Typography variant="bpHeading2" mb={3}>
+        Explore more blocks
       </Typography>
+
+      <BlocksSlider />
     </Container>
   );
 };
