08aa7f7f4016c53daf09c7be73c43de918959f98
Ben Werner <ben.werner01@gmail.com>
2022-01-04 20:29:38 +0000

    ft: User and VerificationCode model classes

---
+++ site/src/lib/model/user.model.ts
@@ -0,0 +1,146 @@
+import { Db, WithId, ObjectId, DBRef } from "mongodb";
+import {
+  VerificationCode,
+  VerificationCodeDocument,
+} from "./verificationCode.model";
+
+export type SerializedUser = {
+  id: string;
+  preferredName: string;
+};
+
+export type UserProperties = {
+  preferredName: string;
+  email: string;
+  loginCodes: DBRef[];
+};
+
+type UserConstructorArgs = {
+  id: string;
+} & UserProperties;
+
+type UserDocument = WithId<UserProperties>;
+
+export class User {
+  id: string;
+
+  preferredName: string;
+
+  email: string;
+
+  loginCodes: DBRef[];
+
+  static COLLECTION_NAME = "bp-users";
+
+  constructor({ id, preferredName, email, loginCodes }: UserConstructorArgs) {
+    this.id = id;
+    this.preferredName = preferredName;
+    this.loginCodes = loginCodes;
+    this.email = email;
+  }
+
+  private static fromDocument({ _id, ...remainingDocument }: UserDocument) {
+    return new User({
+      id: _id.toString(),
+      ...remainingDocument,
+    });
+  }
+
+  static async getById(
+    db: Db,
+    params: { userId: string },
+  ): Promise<User | null> {
+    const { userId } = params;
+    const userDocument = await db
+      .collection<UserDocument>(this.COLLECTION_NAME)
+      .findOne({ _id: new ObjectId(userId) });
+
+    return userDocument ? User.fromDocument(userDocument) : null;
+  }
+
+  static async getByEmail(
+    db: Db,
+    params: { email: string },
+  ): Promise<User | null> {
+    const { email } = params;
+
+    const userDocument = await db
+      .collection<UserDocument>(User.COLLECTION_NAME)
+      .findOne({ email });
+
+    return userDocument ? User.fromDocument(userDocument) : null;
+  }
+
+  static async create(
+    db: Db,
+    params: { preferredName: string; email: string },
+  ): Promise<User> {
+    const userProperties: UserProperties = {
+      ...params,
+      loginCodes: [],
+    };
+
+    const { insertedId } = await db
+      .collection<UserDocument>(User.COLLECTION_NAME)
+      .insertOne(userProperties);
+
+    return new User({ id: insertedId.toString(), ...userProperties });
+  }
+
+  async createLoginCode(db: Db): Promise<VerificationCode> {
+    const loginCode = await VerificationCode.create(db);
+
+    await db.collection<UserDocument>(User.COLLECTION_NAME).updateOne(
+      { _id: new ObjectId(this.id) },
+      {
+        $push: {
+          loginCodes: loginCode.toRef(),
+        },
+      },
+    );
+
+    return loginCode;
+  }
+
+  async getLoginCode(
+    db: Db,
+    params: { loginCodeId: string },
+  ): Promise<VerificationCode | null> {
+    if (
+      !this.loginCodes.find(({ oid }) => oid.toString() === params.loginCodeId)
+    ) {
+      return null;
+    }
+
+    const loginCode = await db
+      .collection<VerificationCodeDocument>(VerificationCode.COLLECTION_NAME)
+      .findOne({
+        $and: [
+          {
+            _id: { $in: this.loginCodes.map(({ oid }) => oid) },
+          },
+          {
+            _id: new ObjectId(params.loginCodeId),
+          },
+        ],
+      });
+
+    return loginCode ? VerificationCode.fromDocument(loginCode) : null;
+  }
+
+  async sendLoginCode(db: Db): Promise<VerificationCode> {
+    const loginCode = await this.createLoginCode(db);
+
+    /** @todo: rate-limit sending of login codes */
+
+    /** @todo: send email */
+    // eslint-disable-next-line no-console
+    console.log("Login code: ", loginCode.code);
+
+    return loginCode;
+  }
+
+  serialize(): SerializedUser {
+    return { id: this.id, preferredName: this.preferredName };
+  }
+}
