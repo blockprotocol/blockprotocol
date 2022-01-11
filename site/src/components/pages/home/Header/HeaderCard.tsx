fee005fa1a94c7910637af46a2fdf79ff8f87866
teenoh <ugbalaachinike@gmail.com>
2022-01-11 10:27:41 +0100

    ft: increase animation repeatDelay and namespace ids

--- site/src/components/pages/home/Header/HeaderCard.tsx
+++ site/src/components/pages/home/Header/HeaderCard.tsx
@@ -24,61 +24,98 @@ const lineDrawAnimationProps = {
 export const HeaderCard: VFC<HeaderCardProps> = ({ hideGradient }) => {
   useEffect(() => {
     const tl = gsap
-      .timeline({ repeat: -1 })
-      .to(["#person", "#image", "#jobTitle", "#fullName", "#issue"], {
-        autoAlpha: 0,
+      .timeline({ repeat: -1, repeatDelay: 2 })
+      .to(
+        [
+          "#hcard_person",
+          "#hcard_image",
+          "#hcard_jobTitle",
+          "#hcard_fullName",
+          "#hcard_issue",
+        ],
+        {
+          autoAlpha: 0,
+        },
+      )
+      .from(
+        [
+          "#hcard_person_circle_outer3",
+          "#hcard_person_circle_outer2",
+          "#hcard_person_circle_outer",
+        ],
+        { ...circleAnimationProps, duration: 2 },
+      )
+      .to(
+        "#hcard_person",
+        {
+          autoAlpha: 1,
+          duration: 2,
+        },
+        "<",
+      )
+      .from("#hcard_person__card", lineDrawAnimationProps)
+      .to(["#hcard_jobTitle", "#hcard_image", "#hcard_fullName"], {
+        autoAlpha: 1,
+      })
+      .from("#hcard_person__fullName", lineDrawAnimationProps)
+      .from(
+        ["#hcard_fullName_circle_inner", "#hcard_fullName_circle_outer"],
+        circleAnimationProps,
+      )
+      .to(
+        ["#card_title"],
+        {
+          fill: "#6048E5",
+        },
+        "<",
+      )
+      .to(["#hcard_fullName_circle"], {
+        autoAlpha: 0.5,
+      })
+      .from("#hcard_person__jobTitle", lineDrawAnimationProps, "<")
+      .from(
+        ["#hcard_jobTitle_circle_inner", "#hcard_jobTitle_circle_outer"],
+        circleAnimationProps,
+      )
+      .to(
+        ["#card_subtitle"],
+        {
+          fill: "#6048E5",
+        },
+        "<",
+      )
+      .to(["#hcard_jobTitle_circle"], {
+        autoAlpha: 0.5,
+      })
+      .from("#hcard_person__image", lineDrawAnimationProps, "<")
+      .from(
+        ["#hcard_image_circle_inner", "#hcard_image_circle_outer"],
+        circleAnimationProps,
+      )
+      .from("#card_circle", circleAnimationProps, "<")
+      .to(["#hcard_image_circle"], {
+        autoAlpha: 0.5,
       })
       .from(
         [
-          "#person_concentric_1",
-          "#person_concentric_2",
-          "#person_concentric_3",
+          "#hcard_issue_circle_inner",
+          "#hcard_issue_circle_outer",
+          "#hcard_issue_circle_outer2",
         ],
         circleAnimationProps,
+        "<",
       )
-      .to("#person", {
-        autoAlpha: 1,
-      })
-      .from("#person__card", lineDrawAnimationProps)
-      .to(["#jobTitle", "#image", "#fullName"], {
-        autoAlpha: 1,
-      })
-      .from("#person__fullName", lineDrawAnimationProps)
-      .from(
-        ["#fullName_circle_inner", "#fullName_circle_outer"],
-        circleAnimationProps,
-      )
-      .to(["#card_title"], {
-        fill: "#6048E5",
-      })
-      .from("#person__jobTitle", lineDrawAnimationProps)
-      .from(
-        ["#jobTitle_circle_inner", "#jobTitle_circle_outer"],
-        circleAnimationProps,
-      )
-      .to(["#card_subtitle"], {
-        fill: "#6048E5",
-      })
-      .to(["#jobTitle_circle"], {
-        autoAlpha: 0.5,
-      })
-      .from("#person__image", lineDrawAnimationProps)
-      .from(
-        ["#image_circle_inner", "#image_circle_outer"],
-        circleAnimationProps,
-      )
-      .from("#card_circle", circleAnimationProps)
-      .to(["#image_circle"], {
-        autoAlpha: 0.5,
-      })
-      .from(
-        ["#issue_circle_inner", "#issue_circle_outer", "#issue_circle_outer2"],
-        circleAnimationProps,
-      )
-      .to(["#issue"], {
+      .to(["#hcard_issue"], {
         autoAlpha: 0.92,
       })
-      .from("#issue__card", lineDrawAnimationProps);
+      .from("#hcard_issue__card", lineDrawAnimationProps)
+      .to(
+        "#hcard_card-issue",
+        {
+          stroke: "#6048E5",
+        },
+        "<0.5",
+      );
 
     return () => {
       tl.kill();
