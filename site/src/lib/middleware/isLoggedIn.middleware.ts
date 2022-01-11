8d84e1b5c919e4e622dc8168e2a470e94927a3ac
Ben Werner <42802102+benwerner01@users.noreply.github.com>
2022-01-11 10:34:00 +0000

    [SITE] Signup API Routes (#83)
    * ft(site): connectToDatabase method, mongodb dev docker-compose
    
    * ft: User and VerificationCode model classes
    
    * ft: seed-db script
    
    * ft: signup api route
    
    * ft: verifyEmail API route
    
    * ft: completeSignup API route
    
    * ft: isShortnameTaken API route
    
    * chore: signup API routes documentation
    
    * refactor: rename verifiedEmail boolean to hasVerifiedEmail
    
    * refactor: store user on verification code in datastore
    
    * ft: API shortname validation
    
    * fix: redundant use of $and in mongo query, remove hardcoded isSignedUp value

---
+++ site/src/lib/middleware/isLoggedIn.middleware.ts
@@ -0,0 +1,25 @@
+import { Middleware } from "next-connect";
+import { BaseApiRequest, BaseApiResponse } from "../handler/baseHandler";
+import { formatErrors } from "../../util/api";
+import { User } from "../model/user.model";
+
+export type IsLoggedInRequestExtensions = {
+  user: User;
+};
+
+export const isLoggedInMiddleware: Middleware<
+  BaseApiRequest,
+  BaseApiResponse
+> = (req, res, next) => {
+  const { user } = req;
+
+  if (!user) {
+    return res.status(401).send(
+      formatErrors({
+        msg: "You must be logged in to perform this request",
+      }),
+    );
+  }
+
+  next();
+};
---
+++ site/src/pages/api/completeSignup.api.ts
@@ -0,0 +1,53 @@
+import { body as bodyValidator, validationResult } from "express-validator";
+import {
+  createAuthenticatedHandler,
+  AuthenticatedApiRequest,
+} from "../../lib/handler/authenticatedHandler";
+import { BaseApiResponse } from "../../lib/handler/baseHandler";
+import { SerializedUser, User } from "../../lib/model/user.model";
+import { formatErrors } from "../../util/api";
+
+export type ApiPutMeRequestBody = {
+  shortname: string;
+  preferredName: string;
+};
+
+export type ApiMeResponse = {
+  user: SerializedUser;
+};
+
+export default createAuthenticatedHandler<undefined, ApiMeResponse>(false)
+  .use(
+    bodyValidator("shortname").isString().notEmpty(),
+    bodyValidator("preferredName").isString().notEmpty(),
+  )
+  .post(
+    async (
+      req: AuthenticatedApiRequest & { body: ApiPutMeRequestBody },
+      res: BaseApiResponse<ApiMeResponse>,
+    ) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json(formatErrors(...errors.array()));
+      }
+
+      const { db, user, body } = req;
+
+      const { shortname, preferredName } = body;
+
+      if (user.shortname && user.shortname !== shortname) {
+        return res.status(400).json(
+          formatErrors({
+            msg: "Cannot update shortname of user",
+            param: "shortname",
+            value: shortname,
+          }),
+        );
+      }
+      if (await User.validateShortname(db, shortname, res)) {
+        await user.update(db, { shortname, preferredName });
+
+        res.status(200).json({ user: user.serialize() });
+      }
+    },
+  );
