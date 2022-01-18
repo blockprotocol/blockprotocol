aa9640ea763c04c6dc6d677add57780987551ae4
Ben Werner <42802102+benwerner01@users.noreply.github.com>
2022-01-18 08:28:17 -0800

    [SITE] mailchimp (#103)
    * [SITE] API Authentication Routes (#74)
    
    * ft(site): connectToDatabase method, mongodb dev docker-compose
    
    * ft: User and VerificationCode model classes
    
    * ft: session, db and passport middleware
    
    * ft: base route handler and authenticated route handler
    
    * ft: sendLoginCode api route
    
    * ft: loginWithLoginCode api route
    
    * ft: me api route, logout api route
    
    * ft: seed-db script
    
    * chore: auth API documentation
    
    * fix: use SESSION_SECRET env var
    
    * ft: rate limit sending of login codes
    
    * [SITE] BP Site login UI (#84)
    
    * ft: navbar login buttons
    
    * ft: api client
    
    * ft: login modal
    
    * ft: Button loading animation
    
    * ft: react user context
    
    * ft: Navbar account dropdown menu
    
    * ft: login page
    
    * refactor: prevent unnecessary me queries
    
    * refactor: FRONTEND_URL environment variable
    
    * refactor: use loginInfo object to store related react state
    
    * chore: reorder BP site readme
    
    * fix: add missing batch update
    
    * refactor: move react contexts out of components directory
    
    * chore: todo comments
    
    * ft: subscribe new user to mailchimp
    
    * ft: set mailchimp shortname and preferred name
    
    * fix: set existing mailchimp list member to subscribed on signup
    
    * ft: ensure user is mailchimp member

--- site/src/lib/config.ts
+++ site/src/lib/config.ts
@@ -6,3 +6,5 @@ export const FRONTEND_DOMAIN =
 export const USE_HTTPS = process.env.NODE_ENV !== "development";
 
 export const FRONTEND_URL = `http${USE_HTTPS ? "s" : ""}://${FRONTEND_DOMAIN}`;
+
+export const isProduction = process.env.NEXT_PUBLIC_VERCEL_ENV === "production";
