8a4e601fd47a14ffd4ffa0bfb6ef026fe9d4db16
teenoh <ugbalaachinike@gmail.com>
2022-01-19 03:07:31 +0100

    chore: add requested changes

--- site/src/pages/api/blocks.api.ts
+++ site/src/pages/api/blocks.api.ts
@@ -1,7 +1,8 @@
-import { NextApiHandler } from "next";
+import { query as queryValidator } from "express-validator";
 import { BlockMetadata } from "blockprotocol";
 import { formatErrors } from "../../util/api";
 import blocksData from "../../../blocks-data.json";
+import { createBaseHandler } from "../../lib/handler/baseHandler";
 
 const validateApiKey = (apiKey: string | string[] | undefined) => {
   if (!apiKey || typeof apiKey !== "string") {
@@ -11,39 +12,45 @@ const validateApiKey = (apiKey: string | string[] | undefined) => {
   return true;
 };
 
-type Response = { results: BlockMetadata[] } | { errors: any };
-
-const blocks: NextApiHandler<Response> = (_req, res) => {
-  const query = _req.query.q;
-  const apiKey = _req.headers["x-api-key"];
-
-  if (!validateApiKey(apiKey)) {
-    return res.status(401).json(
-      formatErrors({
-        msg: "Unauthorized",
-      }),
-    );
-  }
-
-  let data: BlockMetadata[] = [];
-
-  // do we return data when the query string is empty?
-
-  if (query && typeof query === "string") {
-    data = blocksData.filter(
-      ({ displayName, variants }) =>
-        displayName?.toLowerCase().includes(query) ||
-        variants?.some((variant) =>
-          variant.displayName?.toLowerCase().includes(query),
-        ),
-    );
-  }
-
-  // @todo paginate response
-
-  res.status(200).json({
-    results: data,
-  });
+export type ApiSearchRequestQuery = {
+  q: string;
 };
 
-export default blocks;
+export type ApiSearchResponse = {
+  results: BlockMetadata[];
+};
+
+export default createBaseHandler<null, ApiSearchResponse>()
+  .use(queryValidator("q").isString().toLowerCase())
+  .get(async (req, res) => {
+    const { q: query } = req.query as ApiSearchRequestQuery;
+    const apiKey = req.headers["x-api-key"];
+
+    if (!validateApiKey(apiKey)) {
+      return res.status(401).json(
+        formatErrors({
+          msg: "Unauthorized",
+        }),
+      );
+    }
+
+    let data: BlockMetadata[] = blocksData;
+
+    if (query) {
+      data = blocksData.filter(
+        ({ displayName, variants, author, name }) =>
+          [displayName, author, name].some((item) =>
+            item?.toLowerCase().includes(query),
+          ) ||
+          variants?.some((variant) =>
+            variant.displayName?.toLowerCase().includes(query),
+          ),
+      );
+    }
+
+    // @todo paginate response
+
+    res.status(200).json({
+      results: data,
+    });
+  });
