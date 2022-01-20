2443a43d5be22e4c5171b2204f710b440ec105b4
espretto <espretto@posteo.net>
2022-01-20 17:00:46 +0100

    fix(frontend): replace mui's troublesome scroll-lock impl

--- site/src/util/muiUtils.tsx
+++ site/src/util/muiUtils.tsx
@@ -1,3 +1,5 @@
+import { useLayoutEffect } from "react";
+
 export const parseIntFromPixelString = (pixelString: string): number => {
   if (!pixelString.endsWith("px")) {
     throw new Error(`The pixel string "${pixelString}" doesn't end with "px"`);
@@ -5,3 +7,20 @@ export const parseIntFromPixelString = (pixelString: string): number => {
 
   return parseInt(pixelString.slice(0, -2), 10);
 };
+
+/**
+ * MUI's implementation applies a padding to the body which may break the layout.
+ * This version applies different yet equally effective styles to the document element.
+ *
+ * Used to replace the functionality behind `disableScollLock` property of MUI's
+ * components modal, drawer, menu, popover, modal.
+ */
+export const useScrollLock = (active: boolean) =>
+  useLayoutEffect(() => {
+    document.documentElement.style.cssText = active
+      ? "position: fixed; overflow-y: scroll; width: 100%"
+      : "";
+    return () => {
+      document.documentElement.style.cssText = "";
+    };
+  }, [active]);
