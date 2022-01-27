d219bd7a8b8e4e80a9111c4ef870eaa866ff5d6b
Ciaran Morinan <37743469+CiaranMn@users.noreply.github.com>
2022-01-27 20:20:31 +0000

    fix: ensure FRONTEND_URL reaches client (#147)

--- site/src/lib/config.ts
+++ site/src/lib/config.ts
@@ -1,5 +1,5 @@
-export const FRONTEND_URL = process.env.FRONTEND_URL
-  ? process.env.FRONTEND_URL
+export const FRONTEND_URL = process.env.NEXT_PUBLIC_FRONTEND_URL
+  ? process.env.NEXT_PUBLIC_FRONTEND_URL
   : process.env.NEXT_PUBLIC_VERCEL_URL
   ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
   : "http://localhost:3000";
