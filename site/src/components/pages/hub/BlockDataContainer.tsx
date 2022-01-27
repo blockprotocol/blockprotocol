e69337a162c37a598cb3f03b28fc95f64fd15c00
Akash Joshi <akash.joshi@viit.ac.in>
2022-01-27 21:15:36 +0530

    fix: remove initialWidth from block state

--- site/src/components/pages/hub/BlockDataContainer.tsx
+++ site/src/components/pages/hub/BlockDataContainer.tsx
@@ -43,7 +43,6 @@ export const BlockDataContainer: VoidFunctionComponent<
   const [blockState, setBlockState] = useState<Record<string, unknown>>({
     accountId: "test-account-id",
     entityId: "test-entity-id",
-    initialWidth: 600,
   });
 
   const updateEntities: BlockProtocolUpdateEntitiesFunction = useCallback(
