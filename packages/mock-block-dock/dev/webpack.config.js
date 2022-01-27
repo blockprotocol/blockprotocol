7f40021268c090e206424ebecbd0b328d2c5a199
Ciaran Morinan <37743469+CiaranMn@users.noreply.github.com>
2022-01-27 17:22:10 +0000

    New block dev package: mock-block-dock (#143)

---
+++ packages/mock-block-dock/dev/webpack.config.js
@@ -0,0 +1,53 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: "./dev/DevApp.tsx",
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "./dev/index.html",
+    }),
+    new webpack.EnvironmentPlugin({
+      "process.env.NODE_ENV": process.env.NODE_ENV,
+    }),
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(t|j)sx?$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  devServer: {
+    hot: true,
+    contentBase: __dirname,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+      "Access-Control-Allow-Headers": [
+        "X-Requested-With, content-type, Authorization",
+        "X-Requested-With",
+        "content-type",
+        "Authorization",
+        "sentry-trace",
+      ],
+    },
+  },
+  resolve: {
+    extensions: [
+      ".ts", // Add typescript support
+      ".tsx", // Add typescript + react support
+      ".js", // Preserving webpack default
+      ".jsx", // Preserving webpack default
+      ".json", // Preserving webpack default
+      ".css", // Preserving webpack default
+    ],
+  },
+};
