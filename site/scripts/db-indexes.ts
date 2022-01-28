d38cbb0cf450ee779c21b2a7aa0e0ba6b28747b1
Ahmad Sattar <thehabbos007@gmail.com>
2022-01-28 14:50:54 +0100

    Catch and log errors

--- site/scripts/db-indexes.ts
+++ site/scripts/db-indexes.ts
@@ -10,27 +10,45 @@ import {
 import { connectToDatabase } from "../src/lib/mongodb";
 import { EntityType } from "../src/lib/model/entityType.model";
 
+const catchAndLog = async (func: () => Promise<any>) => {
+  try {
+    await func();
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error(`❌ Error trying to create index, ${error.toString()}`);
+    }
+  }
+};
+
 // Actions done in the following script should be _idempotent_ as they run in dev/prod
 void (async () => {
   const { client, db } = await connectToDatabase();
-  await db
-    .collection<UserDocument>(User.COLLECTION_NAME)
-    .createIndex({ email: 1 }, { unique: true });
+  await catchAndLog(() =>
+    db
+      .collection<UserDocument>(User.COLLECTION_NAME)
+      .createIndex({ email: 1 }, { unique: true }),
+  );
 
-  await db
-    .collection(EntityType.COLLECTION_NAME)
-    .createIndex({ entityTypeId: 1 }, { unique: true });
+  await catchAndLog(() =>
+    db
+      .collection(EntityType.COLLECTION_NAME)
+      .createIndex({ entityTypeId: 1 }, { unique: true }),
+  );
 
-  await db
-    .collection(EntityType.COLLECTION_NAME)
-    .createIndex({ user: 1, "schema.title": 1 }, { unique: true });
+  await catchAndLog(() =>
+    db
+      .collection(EntityType.COLLECTION_NAME)
+      .createIndex({ user: 1, "schema.title": 1 }, { unique: true }),
+  );
 
-  await db
-    .collection<VerificationCodeDocument>(VerificationCode.COLLECTION_NAME)
-    .createIndex(
-      { createdAt: 1 },
-      { expireAfterSeconds: VerificationCode.PRUNE_AGE_MS / 60 },
-    );
+  await catchAndLog(() =>
+    db
+      .collection<VerificationCodeDocument>(VerificationCode.COLLECTION_NAME)
+      .createIndex(
+        { createdAt: 1 },
+        { expireAfterSeconds: VerificationCode.PRUNE_AGE_MS / 60 },
+      ),
+  );
 
   await client.close();
   console.log("✅ Created MongoDB indexes");
