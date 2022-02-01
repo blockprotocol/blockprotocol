73fa88841bf9615a50a85223c15739a90ded9d15
Ahmad Sattar <thehabbos007@gmail.com>
2022-02-01 11:10:13 +0100

    Some suggested fixes

--- site/scripts/create-db-indexes.ts
+++ site/scripts/create-db-indexes.ts
@@ -10,7 +10,7 @@ import {
 import { connectToDatabase } from "../src/lib/mongodb";
 import { EntityType } from "../src/lib/model/entityType.model";
 
-const catchAndLog = async (func: () => Promise<any>) => {
+const catchAndLog = async (func: () => Promise<void>) => {
   try {
     await func();
   } catch (error) {
@@ -21,35 +21,37 @@ const catchAndLog = async (func: () => Promise<any>) => {
 };
 
 // Actions done in the following script should be _idempotent_ as they run in dev/prod
-void (async () => {
+const script = async () => {
   const { client, db } = await connectToDatabase();
-  await catchAndLog(() =>
-    db
+  await catchAndLog(async () => {
+    await db
       .collection<UserDocument>(User.COLLECTION_NAME)
-      .createIndex({ email: 1 }, { unique: true, sparse: true }),
-  );
+      .createIndex({ email: 1 }, { unique: true, sparse: true });
+  });
 
-  await catchAndLog(() =>
-    db
+  await catchAndLog(async () => {
+    await db
       .collection(EntityType.COLLECTION_NAME)
-      .createIndex({ entityTypeId: 1 }, { unique: true }),
-  );
+      .createIndex({ entityTypeId: 1 }, { unique: true });
+  });
 
-  await catchAndLog(() =>
-    db
+  await catchAndLog(async () => {
+    await db
       .collection(EntityType.COLLECTION_NAME)
-      .createIndex({ user: 1, "schema.title": 1 }, { unique: true }),
-  );
+      .createIndex({ user: 1, "schema.title": 1 }, { unique: true });
+  });
 
-  await catchAndLog(() =>
-    db
+  await catchAndLog(async () => {
+    await db
       .collection<VerificationCodeDocument>(VerificationCode.COLLECTION_NAME)
       .createIndex(
         { createdAt: 1 },
         { expireAfterSeconds: VerificationCode.PRUNE_AGE_MS / 60 },
-      ),
-  );
+      );
+  });
 
   await client.close();
   console.log("✅ Created MongoDB indexes");
-})();
+};
+
+void script();
