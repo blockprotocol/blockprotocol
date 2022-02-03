0274cb34baab8bc10bf7c61f2a6cfacd43a3adf8
Alexander Kachkaev <alexander@kachkaev.ru>
2022-02-03 14:20:56 +0000

    Reduce diff

--- site/scripts/generate-sitemap.ts
+++ site/scripts/generate-sitemap.ts
@@ -1,11 +1,11 @@
-import fs from "fs";
+import { writeFileSync } from "fs";
 import path from "path";
 import { generateSiteMap } from "../src/lib/sitemap";
 
 const script = async () => {
   const sitemap = generateSiteMap();
 
-  fs.writeFileSync(
+  writeFileSync(
     path.join(process.cwd(), `site-map.json`),
     JSON.stringify(sitemap, null, "\t"),
   );
