850cfcf20326606095e3e74ed5dab3b4d02bf08d
Ahmad Sattar <thehabbos007@gmail.com>
2022-02-04 12:48:22 +0100

    Enhance multipart middleware

---
+++ site/src/lib/middleware/multipartUploads.ts
@@ -0,0 +1,114 @@
+import busboy from "busboy";
+import { Middleware } from "next-connect";
+import { BaseApiResponse } from "../handler/baseHandler";
+import { AuthenticatedApiRequest } from "../handler/authenticatedHandler";
+import { formatErrors } from "../../util/api";
+
+export type MultipartUploadsOptions = {
+  fieldsLimit: number;
+  filesLimit: number;
+  maxFileSize: number;
+};
+
+export type UploadedFileBuffer = {
+  mime: string;
+  buffer: Buffer;
+};
+
+export type Field = {
+  mime: string;
+  encoding: string;
+  truncated: boolean;
+  value: string;
+};
+
+export type MultipartExtensions = {
+  uploads?: Record<string, UploadedFileBuffer>;
+  fields?: Record<string, Field>;
+};
+
+const parseForm = async (
+  { fieldsLimit, filesLimit, maxFileSize }: MultipartUploadsOptions,
+  req: AuthenticatedApiRequest<unknown>,
+): Promise<MultipartExtensions> => {
+  return new Promise((resolve, reject) => {
+    const form = busboy({
+      headers: req.headers,
+      limits: {
+        fields: fieldsLimit,
+        files: filesLimit,
+        fileSize: maxFileSize,
+      },
+    });
+    const uploads: Record<string, UploadedFileBuffer> = {};
+    const fields: Record<string, Field> = {};
+
+    const buffers: Record<string, Uint8Array[]> = {};
+    form.on("file", (fieldName, file, info) => {
+      buffers[fieldName] = [];
+      file.on("data", (data) => {
+        buffers[fieldName].push(data);
+      });
+      file.on("limit", () => {
+        reject(
+          new Error(
+            `Supplied file is too big. Maximum size allowed is ${maxFileSize} bytes.`,
+          ),
+        );
+      });
+      file.on("end", () => {
+        uploads[fieldName] = {
+          mime: info.mimeType,
+          buffer: Buffer.concat(buffers[fieldName]),
+        };
+      });
+    });
+
+    form.on("field", (name, value, info) => {
+      fields[name] = {
+        mime: info.mimeType,
+        encoding: info.encoding,
+        truncated: info.valueTruncated,
+        value,
+      };
+    });
+    form.on("error", (err) => {
+      reject(err);
+    });
+    form.on("filesLimit", () => {
+      reject(new Error(`Limit of ${filesLimit} files exceeded.`));
+    });
+    form.on("fieldsLimit", () => {
+      reject(new Error(`Limit of ${fieldsLimit} value fields exceeded.`));
+    });
+    form.on("finish", () => {
+      resolve({ uploads, fields });
+    });
+    req.pipe(form);
+  });
+};
+
+export const multipartUploads: (
+  filesLimit: MultipartUploadsOptions,
+) => Middleware<
+  AuthenticatedApiRequest<MultipartExtensions>,
+  BaseApiResponse
+> = (options: MultipartUploadsOptions) => async (req, res, next) => {
+  const contentType = req.headers["content-type"];
+  if (contentType?.includes("multipart/form-data")) {
+    try {
+      req.body = await parseForm(options, req);
+
+      next();
+    } catch (error) {
+      res.status(400).json(
+        formatErrors({
+          msg: error,
+          param: "image",
+        }),
+      );
+    }
+  } else {
+    next();
+  }
+};
--- site/src/pages/api/uploadUserAvatar.api.ts
+++ site/src/pages/api/uploadUserAvatar.api.ts
@@ -1,3 +1,5 @@
+import mimeType from "mime-types";
+
 import { uploadFileBufferToS3 } from "../../lib/awsS3";
 import {
   AuthenticatedApiRequest,
@@ -5,42 +7,94 @@ import {
 } from "../../lib/handler/authenticatedHandler";
 
 import {
-  FormRequestExtensions,
-  imageUploadMultipart,
-} from "../../lib/middleware/imageUploadMultipart";
+  MultipartExtensions,
+  multipartUploads,
+} from "../../lib/middleware/multipartUploads";
 import { formatErrors } from "../../util/api";
 
 type Response = {
   avatarUrl: string;
 };
 
+const checkFiletypeAllowed = (extension: string | false) => {
+  const imageExts = ["jpg", "jpeg", "png", "gif", "svg"];
+  if (!imageExts.some((ext) => ext === extension)) {
+    throw new Error(
+      `Please provide a file in one of the formats: ${imageExts.join(", ")}.`,
+    );
+  }
+};
+
+const checkSvg = (svgBuffer: Buffer) => {
+  if (
+    svgBuffer
+      .toString()
+      .match(
+        /(script|entity|onerror|onload|onmouseover|onclick|onfocus|foreignObject|<a)/i,
+      )
+  ) {
+    throw new Error("Given SVG content is disallowed.");
+  }
+};
+
+async function validateImage(mime: string, buffer: Buffer) {
+  const extension = mimeType.extension(mime);
+
+  if (!extension) {
+    throw new Error(`Could not determine extension from file upload`);
+  }
+
+  checkFiletypeAllowed(extension);
+
+  if (extension === "svg") {
+    await checkSvg(buffer);
+  }
+}
+
 export default createAuthenticatedHandler<
-  AuthenticatedApiRequest & FormRequestExtensions,
+  AuthenticatedApiRequest & MultipartExtensions,
   Response
 >()
-  .use(imageUploadMultipart)
+  .use(
+    multipartUploads({
+      fieldsLimit: 0,
+      filesLimit: 1,
+      maxFileSize: 5 * 1024 * 1024, // bytes = 5MB
+    }),
+  )
   .post(async (req, res) => {
     const { body, user, db } = req;
 
-    if (body?.upload && body.upload?.mime) {
-      const { mime, buffer: stream } = body.upload;
-      const { fullUrl, s3Key } = await uploadFileBufferToS3(
-        stream,
-        mime || "",
-        new Date().valueOf().toString(),
-        `avatars/${user.id}`,
-      );
+    if (body?.uploads?.image) {
+      try {
+        const { mime, buffer } = body.uploads?.image;
+        await validateImage(mime, buffer);
 
-      // @todo should we delete previous user avatar before replacing it?
-      await user.update(db, {
-        userAvatar: { url: fullUrl, s3Key },
-      });
+        const { fullUrl, s3Key } = await uploadFileBufferToS3(
+          buffer,
+          mime || "",
+          new Date().valueOf().toString(),
+          `avatars/${user.id}`,
+        );
 
-      res.status(200).json({ avatarUrl: fullUrl });
+        // @todo should we delete previous user avatar before replacing it?
+        await user.update(db, {
+          userAvatar: { url: fullUrl, s3Key },
+        });
+
+        res.status(200).json({ avatarUrl: fullUrl });
+      } catch (error) {
+        res.status(400).json(
+          formatErrors({
+            msg: error,
+            param: "image",
+          }),
+        );
+      }
     } else {
       res.status(400).json(
         formatErrors({
-          msg: "No file given. Please upload a file.",
+          msg: "No file given in 'image' field. Please upload a file.",
           param: "image",
         }),
       );
