be20367564a0569581effc13a48b028cc3d3730b
Ben Werner <ben.werner01@gmail.com>
2022-02-09 10:59:40 -0500

    chore: group component overrides by category in `src/theme/components/index.ts`

--- site/src/theme/components/index.ts
+++ site/src/theme/components/index.ts
@@ -1,47 +1,66 @@
 import { ThemeOptions } from "@mui/material";
 
-import { MuiCssBaselineThemeOptions } from "./utils/MuiCssBaselineThemeOptions";
-import { MuiTypographyThemeOptions } from "./dataDisplay/MuiTypographyThemeOptions";
-import { MuiIconButtonThemeOptions } from "./dataDisplay/MuiIconButtonThemeOptions";
-import { MuiPaperThemeOptions } from "./surfaces/MuiPaperThemeOptions";
-import { MuiButtonThemeOptions } from "./inputs/MuiButtonThemeOptions";
-import { MuiContainerThemeOptions } from "./layout/MuiContainerThemeOptions";
-import { MuiIconThemeOptions } from "./dataDisplay/MuiIconThemeOptions";
-import { MuiSkeletonThemeOptions } from "./feedback/MuiSkeletonThemeOptions";
-import { MuiListItemButtonThemeOptions } from "./dataDisplay/MuiListItemButtonThemeOptions";
-import { MuiListItemIconThemeOptions } from "./dataDisplay/MuiListItemIconThemeOptions";
-import { MuiListItemTextThemeOptions } from "./dataDisplay/MuiListItemTextThemeOptions";
-import { MuiLinkThemeOptions } from "./navigation/MuiLinkThemeOptions";
-import { MuiCheckboxThemeOptions } from "./inputs/MuiCheckboxThemeOptions";
-import { MuiTabsThemeOptions } from "./navigation/MuiTabsThemeOptions";
-import { MuiTabThemeOptions } from "./navigation/MuiTabThemeOptions";
-import { MuiBreadcrumbsThemeOptions } from "./navigation/MuiBreadcrumbsThemeOptions";
 import { MuiFormControlThemeOptions } from "./inputs/MuiFormControlThemeOptions";
 import { MuiInputLabelThemeOptions } from "./inputs/MuiInputLabelThemeOptions";
 import { MuiInputBaseThemeOptions } from "./inputs/MuiInputBaseThemeOptions";
 import { MuiOutlinedInputThemeOptions } from "./inputs/MuiOutlinedInputThemeOptions";
 import { MuiTextFieldThemeOptions } from "./inputs/MuiTextFieldThemeOptions";
+import { MuiButtonThemeOptions } from "./inputs/MuiButtonThemeOptions";
+import { MuiCheckboxThemeOptions } from "./inputs/MuiCheckboxThemeOptions";
+
+import { MuiTypographyThemeOptions } from "./dataDisplay/MuiTypographyThemeOptions";
+import { MuiIconButtonThemeOptions } from "./dataDisplay/MuiIconButtonThemeOptions";
+import { MuiIconThemeOptions } from "./dataDisplay/MuiIconThemeOptions";
+import { MuiListItemButtonThemeOptions } from "./dataDisplay/MuiListItemButtonThemeOptions";
+import { MuiListItemIconThemeOptions } from "./dataDisplay/MuiListItemIconThemeOptions";
+import { MuiListItemTextThemeOptions } from "./dataDisplay/MuiListItemTextThemeOptions";
+
+import { MuiPaperThemeOptions } from "./surfaces/MuiPaperThemeOptions";
+
+import { MuiContainerThemeOptions } from "./layout/MuiContainerThemeOptions";
+
+import { MuiBreadcrumbsThemeOptions } from "./navigation/MuiBreadcrumbsThemeOptions";
+import { MuiLinkThemeOptions } from "./navigation/MuiLinkThemeOptions";
+import { MuiTabsThemeOptions } from "./navigation/MuiTabsThemeOptions";
+import { MuiTabThemeOptions } from "./navigation/MuiTabThemeOptions";
+
+import { MuiSkeletonThemeOptions } from "./feedback/MuiSkeletonThemeOptions";
+
+import { MuiCssBaselineThemeOptions } from "./utils/MuiCssBaselineThemeOptions";
 
 export const components: ThemeOptions["components"] = {
-  MuiCssBaseline: MuiCssBaselineThemeOptions,
-  MuiTypography: MuiTypographyThemeOptions,
-  MuiIconButton: MuiIconButtonThemeOptions,
-  MuiPaper: MuiPaperThemeOptions,
+  /** ===== INPUTS ==== */
   MuiButton: MuiButtonThemeOptions,
-  MuiContainer: MuiContainerThemeOptions,
-  MuiIcon: MuiIconThemeOptions,
-  MuiSkeleton: MuiSkeletonThemeOptions,
-  MuiListItemButton: MuiListItemButtonThemeOptions,
-  MuiListItemIcon: MuiListItemIconThemeOptions,
-  MuiListItemText: MuiListItemTextThemeOptions,
-  MuiLink: MuiLinkThemeOptions,
   MuiCheckbox: MuiCheckboxThemeOptions,
-  MuiTabs: MuiTabsThemeOptions,
-  MuiTab: MuiTabThemeOptions,
-  MuiBreadcrumbs: MuiBreadcrumbsThemeOptions,
+  MuiTextField: MuiTextFieldThemeOptions,
   MuiFormControl: MuiFormControlThemeOptions,
   MuiInputLabel: MuiInputLabelThemeOptions,
   MuiInputBase: MuiInputBaseThemeOptions,
   MuiOutlinedInput: MuiOutlinedInputThemeOptions,
-  MuiTextField: MuiTextFieldThemeOptions,
+
+  /** ===== DATA DISPLAY ===== */
+  MuiTypography: MuiTypographyThemeOptions,
+  MuiIconButton: MuiIconButtonThemeOptions,
+  MuiIcon: MuiIconThemeOptions,
+  MuiListItemButton: MuiListItemButtonThemeOptions,
+  MuiListItemIcon: MuiListItemIconThemeOptions,
+  MuiListItemText: MuiListItemTextThemeOptions,
+
+  /** ===== SURFACES ===== */
+  MuiPaper: MuiPaperThemeOptions,
+
+  /** ===== LAYOUT ===== */
+  MuiContainer: MuiContainerThemeOptions,
+
+  /** ===== NAVIGATION ===== */
+  MuiBreadcrumbs: MuiBreadcrumbsThemeOptions,
+  MuiLink: MuiLinkThemeOptions,
+  MuiTabs: MuiTabsThemeOptions,
+  MuiTab: MuiTabThemeOptions,
+
+  /** ===== FEEDBACK ===== */
+  MuiSkeleton: MuiSkeletonThemeOptions,
+
+  /** ===== UTILS ===== */
+  MuiCssBaseline: MuiCssBaselineThemeOptions,
 };
