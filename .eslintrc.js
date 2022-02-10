de946a589e08b398c0b6049a479e5ed16c599ef0
Alexander Kachkaev <alexander@kachkaev.ru>
2022-02-10 18:19:28 +0000

    Tweak `eslintrc`

--- .eslintrc.js
+++ .eslintrc.js
@@ -206,55 +206,61 @@ module.exports = {
         "no-restricted-imports": [
           "error",
           {
-            name: "fs",
-            message:
-              "Please use 'fs-extra' for promise-based API, extra methods and consistency.",
-          },
-          {
-            name: "@mui/material",
-            importNames: ["Link"],
-            message:
-              "Please use the custom src/components/Link component instead to ensure Next.js and MUI compatibility.",
-          },
-          {
-            name: "@mui/material/Link",
-            message:
-              "Please use the custom src/components/Link component instead to ensure Next.js and MUI compatibility.",
-          },
-          {
-            name: "next",
-            importNames: ["Link"],
-            message:
-              "Please use the custom src/components/Link component instead to ensure Next.js and MUI compatibility.",
-          },
-          {
-            name: "next/link",
-            message:
-              "Please use the custom src/components/Link component instead to ensure Next.js and MUI compatibility.",
-          },
-          {
-            name: "@mui/material",
-            importNames: ["Button", "TextField", "Popover"],
-            message:
-              "Please use the custom wrapper component in src/component instead.",
-          },
-          {
-            name: "@mui/material/Button",
-            importNames: ["default"],
-            message:
-              "Please use the custom src/components/Button component instead.",
-          },
-          {
-            name: "@mui/material/TextField",
-            importNames: ["default"],
-            message:
-              "Please use the custom src/components/TextField component instead.",
-          },
-          {
-            name: "@mui/material/Popover",
-            importNames: ["default"],
-            message:
-              "Please use the custom src/components/Popover component instead.",
+            paths: [
+              {
+                name: "@mui/material",
+                importNames: ["Link"],
+                message:
+                  "Please use the custom src/components/Link component instead to ensure Next.js and MUI compatibility.",
+              },
+              {
+                name: "@mui/material/Link",
+                message:
+                  "Please use the custom src/components/Link component instead to ensure Next.js and MUI compatibility.",
+              },
+              {
+                name: "next",
+                importNames: ["Link"],
+                message:
+                  "Please use the custom src/components/Link component instead to ensure Next.js and MUI compatibility.",
+              },
+              {
+                name: "next/link",
+                message:
+                  "Please use the custom src/components/Link component instead to ensure Next.js and MUI compatibility.",
+              },
+              {
+                name: "@mui/material",
+                importNames: ["Button", "TextField", "Popover"],
+                message:
+                  "Please use the custom wrapper component in src/component instead.",
+              },
+              {
+                name: "@mui/material/Button",
+                importNames: ["default"],
+                message:
+                  "Please use the custom src/components/Button component instead.",
+              },
+              {
+                name: "@mui/material/TextField",
+                importNames: ["default"],
+                message:
+                  "Please use the custom src/components/TextField component instead.",
+              },
+              {
+                name: "@mui/material/Popover",
+                importNames: ["default"],
+                message:
+                  "Please use the custom src/components/Popover component instead.",
+              },
+            ],
+            patterns: [
+              {
+                group: ["fs", "fs/*"],
+                message:
+                  "Please use 'fs-extra' for promise-based API, extra methods and consistency.",
+              },
+            ],
           },
         ],
       },
