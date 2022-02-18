6352213847b2b02cced541ac932f961614fa1224
Alexander Kachkaev <alexander@kachkaev.ru>
2022-02-18 18:04:36 +0000

    Remove next-fonts and next-images

--- site/assets.d.ts
+++ site/assets.d.ts
@@ -1,5 +1,3 @@
-declare module "*.ttf";
-declare module "*.png";
 declare module "*.svg" {
   const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
   export default ReactComponent;
--- site/next.config.js
+++ site/next.config.js
@@ -1,39 +1,34 @@
-const withImages = require("next-images");
-const withFonts = require("next-fonts");
+module.exports = {
+  pageExtensions: ["page.ts", "page.tsx", "api.ts"],
 
-module.exports = withFonts(
-  withImages({
-    pageExtensions: ["page.ts", "page.tsx", "api.ts"],
-    optimizeFonts: true,
+  // We call linters in GitHub Actions for all pull requests. By not linting
+  // again during `next build`, we save CI minutes and unlock more feedback.
+  // Thus, we can get Playwright test results and Preview releases for WIP PRs.
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
 
-    // We call linters in GitHub Actions for all pull requests. By not linting
-    // again during `next build`, we save CI minutes and unlock more feedback.
-    // Thus, we can get Playwright test results and Preview releases for WIP PRs.
-    eslint: { ignoreDuringBuilds: true },
-    typescript: { ignoreBuildErrors: true },
-    async redirects() {
-      return [
-        {
-          source: "/discord",
-          destination: "https://discord.gg/PefPteFe5j",
-          permanent: true,
-        },
-        {
-          source: "/faq",
-          destination: "/docs/faq",
-          permanent: true,
-        },
-        {
-          source: "/partners",
-          destination: "/contact",
-          permanent: true,
-        },
-        {
-          source: "/partners/submitted",
-          destination: "/contact/submitted",
-          permanent: true,
-        },
-      ];
-    },
-  }),
-);
+  async redirects() {
+    return [
+      {
+        source: "/discord",
+        destination: "https://discord.gg/PefPteFe5j",
+        permanent: true,
+      },
+      {
+        source: "/faq",
+        destination: "/docs/faq",
+        permanent: true,
+      },
+      {
+        source: "/partners",
+        destination: "/contact",
+        permanent: true,
+      },
+      {
+        source: "/partners/submitted",
+        destination: "/contact/submitted",
+        permanent: true,
+      },
+    ];
+  },
+};
