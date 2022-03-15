ce3ceaa1d1dacd94d7efe45c08f6ee061289491f
Alexander Kachkaev <alexander@kachkaev.ru>
2022-03-15 14:24:43 +0000

    Use `<Typography />`

--- site/src/components/pages/user/Placeholder.tsx
+++ site/src/components/pages/user/Placeholder.tsx
@@ -1,4 +1,4 @@
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import { ReactNode, VoidFunctionComponent } from "react";
 
 export interface PlaceholderProps {
@@ -15,31 +15,23 @@ export const Placeholder: VoidFunctionComponent<PlaceholderProps> = ({
   return (
     <Box
       sx={{
-        border: ({ palette }) => "1px dashed ${palette.gray[40]}",
+        border: ({ palette }) => `1px dashed ${palette.gray[40]}`,
         borderRadius: "8px",
         textAlign: "center",
         paddingY: { xs: 6, sm: 12 },
         paddingX: 4,
       }}
     >
-      <Box
+      <Typography variant="bpHeading4">{header}</Typography>
+      <Typography
         sx={{
-          fontSize: 24,
-          color: ({ palette }) => palette.gray[80],
-        }}
-      >
-        {header}
-      </Box>
-      <Box
-        sx={{
-          fontSize: 16,
           color: ({ palette }) => palette.gray[70],
           paddingTop: 1,
           paddingBottom: 2,
         }}
       >
         {tip}
-      </Box>
+      </Typography>
       <Box>{actions}</Box>
     </Box>
   );
