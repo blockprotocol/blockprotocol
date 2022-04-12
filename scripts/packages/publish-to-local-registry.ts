0565a12c21fac866ba8c5e1f15ed63ba2bea9ff0
Alexander Kachkaev <alexander@kachkaev.ru>
2022-04-12 17:11:56 +0100

    Temporarily stop running `yarn build` in `block-template`

--- scripts/packages/publish-to-local-registry.ts
+++ scripts/packages/publish-to-local-registry.ts
@@ -55,16 +55,16 @@ const script = async () => {
   await addUserProcess;
 
   logStepEnd();
-  logStepStart(
-    `Run yarn build in block-template (to check if dist files are published)`,
-  );
+  // logStepStart(
+  //   `Run yarn build in block-template (to check if dist files are published)`,
+  // );
 
-  await execa("yarn", ["build"], {
-    ...defaultExecaOptions,
-    cwd: path.resolve(`packages/block-template`),
-  });
+  // await execa("yarn", ["build"], {
+  //   ...defaultExecaOptions,
+  //   cwd: path.resolve(`packages/block-template`),
+  // });
 
-  logStepEnd();
+  // logStepEnd();
 
   for (const packageName of packageNames) {
     const packageDirPath = path.resolve(`packages/${packageName}`);
