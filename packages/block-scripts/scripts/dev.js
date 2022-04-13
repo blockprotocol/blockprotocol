a02574919e269a75a47194520bc6fc9186045543
Alexander Kachkaev <alexander@kachkaev.ru>
2022-04-13 19:02:59 +0100

    Try fixing CI

--- packages/block-scripts/scripts/dev.js
+++ packages/block-scripts/scripts/dev.js
@@ -1,8 +1,21 @@
-// import { cleanDist } from "../shared/clean-dist.js";
+import webpack from "webpack";
+import webpackMainConfig from "../config/webpack-main.config.cjs";
+import { cleanDist } from "../shared/clean-dist.js";
 import { serveDist } from "../shared/serve-dist.js";
 
 const script = async () => {
-  // await cleanDist();
+  await cleanDist();
+
+  webpack(
+    {
+      ...webpackMainConfig,
+      mode: "development",
+      watch: true,
+    },
+    (args) => {
+      console.log(args);
+    },
+  );
 
   serveDist(9090);
   // await Promise.any([webpack({})]);
