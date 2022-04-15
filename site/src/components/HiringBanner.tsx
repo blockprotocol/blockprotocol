a0c61e0fafba0c6abb792ac5b0827c5aff087868
teenoh <ugbalaachinike@gmail.com>
2022-04-15 20:40:28 +0100

    cleanups

--- site/src/components/HiringBanner.tsx
+++ site/src/components/HiringBanner.tsx
@@ -7,18 +7,22 @@ import { Box, Container, Typography } from "@mui/material";
 import { FontAwesomeIcon } from "./icons";
 import { Link } from "./Link";
 
+const CAREERS_SITE = "https://hash.ai/careers";
+
 export const HiringBanner = () => {
   return (
-    <Link href="https://hash.ai/careers" tabIndex={0}>
+    <Link href={CAREERS_SITE} target="_blank" rel="noopener">
       <Box
         sx={({ transitions }) => ({
           position: "relative",
-          border: `1px solid #80ABFF`,
+          borderTop: "1px solid #80ABFF",
           py: 3.25,
           transition: transitions.create("opacity"),
           cursor: "pointer",
-          background:
-            "linear-gradient(90.78deg, #52C5F7 -2.72%, #6851F4 31.33%, #6249EF 100.36%)",
+          background: {
+            xs: "linear-gradient(96.49deg, #52C5F7 -17.34%, #6249EF 38.34%)",
+            lg: "linear-gradient(90.78deg, #52C5F7 -2.72%, #6851F4 31.33%, #6249EF 100.36%)",
+          },
           "&:after": {
             content: "''",
             position: "absolute",
@@ -26,7 +30,10 @@ export const HiringBanner = () => {
             left: 0,
             height: "100%",
             width: "100%",
-            background: `linear-gradient(90.8deg, #52C5F7 8.95%, #6851F4 67.3%, #6249EF 117.05%)`,
+            background: {
+              xs: "linear-gradient(87.78deg, #52C5F7 0.78%, #6249EF 81.21%)",
+              lg: "linear-gradient(90.8deg, #52C5F7 8.95%, #6851F4 67.3%, #6249EF 117.05%)",
+            },
             opacity: 0,
             transition: transitions.create("opacity"),
           },
@@ -39,7 +46,7 @@ export const HiringBanner = () => {
         <Container
           sx={{
             display: "flex",
-            alignItems: { xs: "flex-start", lg: "center" },
+            alignItems: "center",
             justifyContent: "center",
             position: "relative",
             zIndex: 2,
@@ -53,35 +60,31 @@ export const HiringBanner = () => {
               display: "flex",
               alignItems: "center",
               justifyContent: "center",
+              alignSelf: { xs: "flex-start", lg: "center" },
               backgroundColor: palette.teal[100],
               color: palette.purple[600],
-              mr: { xs: 2, lg: 1 },
+              mr: 2,
               flexShrink: 0,
             })}
           >
             <FontAwesomeIcon icon={faBullhorn} />
           </Box>
 
-          <Box display="flex">
+          <Box display="flex" alignItems="center" flexWrap="wrap">
             <Typography
               variant="bpSmallCopy"
               sx={({ palette }) => ({
                 color: palette.teal[200],
                 fontWeight: "medium",
-                mr: { xs: 0, md: 1.5 },
-                mb: { xs: 1.5, md: 0 },
+                mr: { xs: 0, lg: 1.5 },
+                mb: { xs: 1.5, lg: 0 },
               })}
             >
               <Box component="span" mr="0.5ch">
                 We're hiring full-stack TypeScript/React and PHP plugin
                 developers to help grow the
               </Box>
-              <Box
-                component="span"
-                display="inline-flex"
-                alignItems="center"
-                mr={1.5}
-              >
+              <Box component="span" display="inline-flex" alignItems="center">
                 Block Protocol
                 <FontAwesomeIcon
                   icon={faArrowsLeftRight}
@@ -89,26 +92,29 @@ export const HiringBanner = () => {
                 />
                 WordPress ecosystem.
               </Box>
-
-              <Typography
-                component="span"
-                variant="bpSmallCopy"
-                color="currentcolor"
-                sx={({ palette }) => ({
-                  fontWeight: 700,
-                  color: palette.gray[10],
-                  lineHeight: 1.1,
-                  display: { xs: "flex", md: "inline-flex" },
-                  alignItems: "center",
-                  borderBottom: `1px solid currentColor`,
-                })}
-              >
-                Learn more
-                <FontAwesomeIcon
-                  icon={faArrowRight}
-                  sx={{ ml: 0.5, fontSize: 14 }}
-                />
-              </Typography>
+            </Typography>
+            <Typography
+              component="span"
+              variant="bpSmallCopy"
+              color="currentcolor"
+              sx={({ palette }) => ({
+                fontWeight: 700,
+                color: palette.gray[10],
+                lineHeight: 1.1,
+                display: { xs: "flex", lg: "inline-flex" },
+                width: "content",
+                alignItems: "center",
+                borderBottom: "1px solid currentColor",
+                "&:hover": {
+                  color: palette.gray[30],
+                },
+              })}
+            >
+              Learn more
+              <FontAwesomeIcon
+                icon={faArrowRight}
+                sx={{ ml: 0.5, fontSize: 14 }}
+              />
             </Typography>
           </Box>
         </Container>
