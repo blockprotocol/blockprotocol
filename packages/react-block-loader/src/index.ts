16e22ca43c33e9fe65d1337dd24c5acdaa815b73
Ciaran Morinan <37743469+CiaranMn@users.noreply.github.com>
2022-04-28 20:09:18 +0100

    Introducing react-block-loader (#289)
    Co-authored-by: Alexander Kachkaev <alexander@kachkaev.ru>

---
+++ packages/react-block-loader/src/index.ts
@@ -0,0 +1,9 @@
+export { RemoteBlock } from "./RemoteBlock";
+export type {
+  TextFromUrlRequestMessage,
+  TextFromUrlResponseMessage,
+} from "./shared";
+export {
+  isTextFromUrlRequestMessage,
+  isTextFromUrlResponseMessage,
+} from "./shared";
