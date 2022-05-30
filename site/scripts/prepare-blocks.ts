9bdfe2a6837b796983cc499942da687db435d49d
teenoh <ugbalaachinike@gmail.com>
2022-05-30 17:59:08 +0100

    update commit hash

--- site/scripts/prepare-blocks.ts
+++ site/scripts/prepare-blocks.ts
@@ -70,6 +70,7 @@ const listBlockInfos = async (
   const errorMessages: string[] = [];
 
   for (const blockInfoPath of blockInfoPaths) {
+    if (!blockInfoPath.includes("tldraw")) continue;
     try {
       const [, blockVendorName, blockNameWithinVendor] =
         blockInfoPath.match(/^@([\w-]+)\/([\w-]+).json$/) ?? [];
