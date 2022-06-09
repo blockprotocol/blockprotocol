ba5fe87fe99a30c6208e46599df4193b8a2ec865
Alexander Kachkaev <alexander@kachkaev.ru>
2022-06-09 21:00:24 +0100

    Try another approach to forceFsOnVercel

--- site/src/lib/force-fs-on-vercel.cjs
+++ site/src/lib/force-fs-on-vercel.cjs
@@ -1,8 +1,11 @@
 // https://vercel.com/docs/runtimes#advanced-usage/technical-details/including-additional-files
 // @todo Explain this hack if it works
 const forceFsOnVercel = () => {
-  // eslint-disable-next-line global-require,no-unused-vars
+  // eslint-disable-next-line global-require
   const fs = require("fs");
+
+  // eslint-disable-next-line no-unused-expressions
+  fs.readFile;
 };
 
 module.exports = forceFsOnVercel;
