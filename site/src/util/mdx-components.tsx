2faf6b4f615c75bffb250b564d93aa2b0f102ec4
Alexander Kachkaev <alexander@kachkaev.ru>
2022-06-10 20:06:01 +0100

    Reduce diff (?)

--- site/src/util/mdx-components.tsx
+++ site/src/util/mdx-components.tsx
@@ -271,6 +271,25 @@ export const mdxComponents: Record<string, ReactNode> = {
       </Typography>
     </Box>
   ),
+  // inline code (`)
+  code: (props: HTMLAttributes<HTMLElement>) => {
+    return (
+      <Box
+        component="code"
+        sx={(theme) => ({
+          fontSize: "80%",
+          color: theme.palette.purple[700],
+          background: theme.palette.purple[100],
+          padding: theme.spacing(0.25, 0.5),
+          borderColor: theme.palette.purple[200],
+          borderWidth: 1,
+          borderStyle: "solid",
+          borderRadius: "4px",
+        })}
+        {...props}
+      />
+    );
+  },
   // block code (```) - consists of <pre><code>...</code></pre>
   pre: ({ children, ...rest }: HTMLAttributes<HTMLElement>) => {
     const [child, ...otherChildren] = Children.toArray(children);
@@ -329,24 +348,5 @@ export const mdxComponents: Record<string, ReactNode> = {
 
     // fallback
     return <pre {...rest}>{children}</pre>;
-  },
-  // inline code (`)
-  code: (props: HTMLAttributes<HTMLElement>) => {
-    return (
-      <Box
-        component="code"
-        sx={(theme) => ({
-          fontSize: "80%",
-          color: theme.palette.purple[700],
-          background: theme.palette.purple[100],
-          padding: theme.spacing(0.25, 0.5),
-          borderColor: theme.palette.purple[200],
-          borderWidth: 1,
-          borderStyle: "solid",
-          borderRadius: "4px",
-        })}
-        {...props}
-      />
-    );
   },
 };
