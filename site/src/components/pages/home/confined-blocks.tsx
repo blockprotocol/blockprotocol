068b93fe893d1d27b30e423160fabd56667ac5e6
Maggie Appleton <5599295+MaggieAppleton@users.noreply.github.com>
2022-06-10 15:19:37 +0100

    added 'confined blocks' section

---
+++ site/src/components/pages/home/confined-blocks.tsx
@@ -0,0 +1,110 @@
+import {
+  Box,
+  Container,
+  Typography,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
+
+export const ConfinedBlocks = () => {
+  const theme = useTheme();
+
+  return (
+    <Box
+      sx={{
+        background: "linear-gradient(40.4deg, #161619 63.75%, #3D3D46 82.21%)",
+        py: { xs: 10, md: 12 },
+        px: { xs: "1rem", lg: "0" },
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: { xs: "column", md: "row" },
+          maxWidth: "1100px",
+          margin: "0 auto",
+          gridGap: { xs: "2rem", md: "4rem" },
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: { xs: "center", md: "flex-start" },
+          }}
+        >
+          <Typography
+            sx={{
+              color: ({ palette }) => palette.grey[200],
+              textAlign: { xs: "center", md: "left" },
+              maxWidth: "20ch",
+              margin: "0 auto 1.5rem",
+            }}
+            variant="bpHeading2"
+          >
+            Today, blocks are confined to single apps and websites
+          </Typography>
+          <Box
+            sx={{
+              width: "120px",
+              height: "2px",
+              ml: "0.25rem",
+              my: 2,
+              background:
+                "linear-gradient(to right, rgb(172, 159, 255, 1), rgba(172, 159, 255, 0))",
+            }}
+          />
+          <Typography
+            sx={{
+              color: "#c2cedf",
+              textAlign: { xs: "center", md: "left" },
+              maxWidth: "45ch",
+              margin: "1rem auto",
+              lineHeight: 1.5,
+            }}
+            mb={2}
+            variant="bpBodyCopy"
+          >
+            Every app has to build all of their own blocks. This means
+            developers are rebuilding the same block types over and over.
+          </Typography>
+          <Typography
+            sx={{
+              color: "#c2cedf",
+              textAlign: { xs: "center", md: "left" },
+              maxWidth: "45ch",
+              margin: "1rem auto",
+              lineHeight: 1.5,
+            }}
+            mb={2}
+            variant="bpBodyCopy"
+          >
+            Open source components can save some time, but still need to
+            be individually integrated.
+          </Typography>
+          <Typography
+            sx={{
+              color: "#c2cedf",
+              textAlign: { xs: "center", md: "left" },
+              maxWidth: "45ch",
+              margin: "1rem auto",
+              lineHeight: 1.5,
+            }}
+            mb={2}
+            variant="bpBodyCopy"
+          >
+            This also limits how many blocks users have access to in any single
+            application.
+          </Typography>
+        </Box>
+        <Box
+          sx={{ width: { xs: "90%", md: "55%" } }}
+          component="img"
+          src="/assets/new-home/dark-box.png"
+        />
+      </Box>
+    </Box>
+  );
+};
