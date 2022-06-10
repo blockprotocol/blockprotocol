23c09c7b3c98b9739d7054652c733b2e0c539b12
Maggie Appleton <5599295+MaggieAppleton@users.noreply.github.com>
2022-06-10 15:19:07 +0100

    added composable interfaces section

---
+++ site/src/components/pages/home/composable-interfaces.tsx
@@ -0,0 +1,88 @@
+import {
+  Box,
+  Container,
+  Typography,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
+
+export const ComposableInterfaces = () => {
+  const theme = useTheme();
+
+  return (
+    <Box
+      sx={{
+        background:
+          "radial-gradient(116.02% 95.04% at 50% 100.79%, #F3F0F9 0%, #FFFFFF 70.54%);",
+        mt: 2,
+        pb: 8,
+        borderBottom: `1px solid #eceaf1`,
+      }}
+    >
+      <Box
+        sx={{
+          margin: { xs: "0 auto 2rem", lg: "0 auto" },
+          maxWidth: { xs: "100%", md: "700px" },
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Typography variant="bpHeading2" textAlign="center" my={2}>
+          The Block Protocol makes building composable interfaces easy
+        </Typography>
+        <Box
+          sx={{
+            width: "160px",
+            height: "2px",
+            my: 2,
+            background:
+              "linear-gradient(to right, rgb(149, 135, 239, 1), rgba(172, 159, 255, 0))",
+          }}
+        />
+        <Typography
+          variant="bpBodyCopy"
+          textAlign="center"
+          sx={{
+            color: ({ palette }) => palette.gray[80],
+            maxWidth: { xs: "100%", md: "46ch" },
+            margin: "1rem auto 1rem",
+          }}
+        >
+          Applications that follow the protocol can use any Block Protocol block
+          with zero marginal implementation cost.
+        </Typography>
+        <Typography
+          variant="bpBodyCopy"
+          textAlign="center"
+          sx={{
+            color: ({ palette }) => palette.gray[80],
+            maxWidth: { xs: "100%", md: "46ch" },
+            margin: "0 auto",
+          }}
+        >
+          This means you can give users access to a wide variety of blocks – far
+          more than you could build yourself. Empower users build their own
+          modular interfaces and solutions to complex problems.
+        </Typography>
+      </Box>
+      <Box
+        sx={{
+          alignItems: "center",
+          position: "relative",
+          display: "flex",
+          alignContent: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Box
+          component="img"
+          src="/assets/new-home/composable-full.png"
+          sx={{
+            width: "100%",
+          }}
+        />
+      </Box>
+    </Box>
+  );
+};
