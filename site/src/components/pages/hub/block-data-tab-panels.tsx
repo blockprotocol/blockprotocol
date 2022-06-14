fa46b8af3e5742a30e659f5af204e146f1d5a519
Ugbala Valentino <ugbalaachinike@gmail.com>
2022-06-14 12:12:16 +0100

    Display example-graph data in Block Hub (#328)

--- site/src/components/pages/hub/block-data-tab-panels.tsx
+++ site/src/components/pages/hub/block-data-tab-panels.tsx
@@ -1,4 +1,4 @@
-import { Box } from "@mui/material";
+import { Box, BoxProps, experimental_sx as sx, styled } from "@mui/material";
 import { ChangeEvent, VoidFunctionComponent } from "react";
 
 import { Snippet } from "../../snippet";
@@ -9,51 +9,48 @@ type BlockDataTabPanelProps = {
   modalOpen?: boolean;
   blockDataTab: number;
   schema: BlockSchema;
+  exampleGraph: any;
   text: string;
   setText: (newValue: string) => void;
 };
 
+const SnippetContainer = styled(({ children, ...props }: BoxProps) => (
+  <Box {...props} component="pre" p={4}>
+    {children}
+  </Box>
+))(({ theme }) =>
+  sx({
+    fontSize: 14,
+    backgroundColor: theme.palette.gray[90],
+    borderTopLeftRadius: {
+      xs: 6,
+      md: 0,
+    },
+    borderTopRightRadius: {
+      xs: 6,
+      md: 0,
+    },
+    borderBottomLeftRadius: 6,
+    borderBottomRightRadius: 6,
+    overflow: "auto",
+    width: "100%",
+
+    ".snippet": {
+      fontFamily: `ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace`,
+      overflow: "auto",
+      height: "100%",
+      whiteSpace: "break-spaces",
+    },
+  }),
+);
+
 export const BlockDataTabPanels: VoidFunctionComponent<
   BlockDataTabPanelProps
-> = ({ blockDataTab, schema, text, setText, modalOpen }) => {
+> = ({ blockDataTab, schema, exampleGraph, text, setText, modalOpen }) => {
   const modalHeight = modalOpen ? "60vh" : 450;
 
   return (
     <>
-      <TabPanel value={blockDataTab} index={1}>
-        <Box
-          component="pre"
-          sx={(theme) => ({
-            height: modalHeight,
-            fontSize: 14,
-            backgroundColor: theme.palette.gray[90],
-            borderTopLeftRadius: {
-              xs: 6,
-              md: 0,
-            },
-            borderTopRightRadius: {
-              xs: 6,
-              md: 0,
-            },
-            borderBottomLeftRadius: 6,
-            borderBottomRightRadius: 6,
-            overflow: "auto",
-            width: "100%",
-          })}
-          p={4}
-        >
-          <Snippet
-            sx={{
-              fontFamily: `ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace`,
-              overflow: "auto",
-              height: "100%",
-              whiteSpace: "break-spaces",
-            }}
-            source={JSON.stringify(schema, null, 2)}
-            language="json"
-          />
-        </Box>
-      </TabPanel>
       <TabPanel value={blockDataTab} index={0}>
         <Box sx={{ height: modalHeight, fontSize: 14, width: "100%" }}>
           <Box
@@ -86,6 +83,26 @@ export const BlockDataTabPanels: VoidFunctionComponent<
           />
         </Box>
       </TabPanel>
+      <TabPanel value={blockDataTab} index={1}>
+        <SnippetContainer height={modalHeight}>
+          <Snippet
+            className="snippet"
+            source={JSON.stringify(schema, null, 2)}
+            language="json"
+          />
+        </SnippetContainer>
+      </TabPanel>
+      {!!exampleGraph && (
+        <TabPanel value={blockDataTab} index={2}>
+          <SnippetContainer height={modalHeight}>
+            <Snippet
+              className="snippet"
+              source={JSON.stringify(exampleGraph, null, 2)}
+              language="json"
+            />
+          </SnippetContainer>
+        </TabPanel>
+      )}
     </>
   );
 };
