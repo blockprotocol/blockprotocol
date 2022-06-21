6ca218c49f13a811605a4dd6b00c5b46e3ab2e00
Ciaran Morinan <c@hash.ai>
2022-06-21 09:13:23 +0100

    dont export graph-service.json

--- packages/@blockprotocol/graph/src/index.ts
+++ packages/@blockprotocol/graph/src/index.ts
@@ -1,7 +1,6 @@
 export { BlockElementBase } from "./custom-element";
 export { GraphBlockHandler } from "./graph-block-handler";
 export { GraphEmbedderHandler } from "./graph-embedder-handler";
-export { default as graphServiceJson } from "./graph-service.json";
 export type { BlockComponent } from "./react";
 export { useGraphBlockService, useGraphEmbedderService } from "./react";
 export * from "./types";
