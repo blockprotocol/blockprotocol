ae8ad32f718204489183d4a112ceb3c5d44d6abd
teenoh <ugbalaachinike@gmail.com>
2022-06-22 14:44:27 +0200

    add exampleGraph key to metadataExtra

--- packages/block-scripts/shared/generate-dist-example-graph.js
+++ packages/block-scripts/shared/generate-dist-example-graph.js
@@ -2,13 +2,8 @@ import fs from "fs-extra";
 import path from "node:path";
 
 import { blockDistDirPath, blockRootDirPath } from "./paths.js";
-import { writeFormattedJson } from "./write-formatted-json.js";
 
 export const generateDistExampleGraph = async () => {
-  const blockMetadataInDistPath = path.resolve(
-    blockDistDirPath,
-    "block-metadata.json",
-  );
   const exampleGraphInDistFilePath = path.resolve(
     blockDistDirPath,
     "example-graph.json",
@@ -20,16 +15,5 @@ export const generateDistExampleGraph = async () => {
 
   if (await fs.pathExists(exampleGraphFilePath)) {
     await fs.copy(exampleGraphFilePath, exampleGraphInDistFilePath);
-
-    const blockMetadata = (await fs.pathExists(blockMetadataInDistPath))
-      ? await fs.readJson(blockMetadataInDistPath)
-      : undefined;
-
-    if (blockMetadata) {
-      writeFormattedJson(path.resolve(blockMetadataInDistPath), {
-        ...blockMetadata,
-        exampleGraph: "example-graph.json",
-      });
-    }
   }
 };
