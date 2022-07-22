c5856aa36e0d04026e37d5a078bd045d1883f462
teenoh <ugbalaachinike@gmail.com>
2022-07-22 12:54:24 +0100

    re-order page footer tests

--- site/tests/integration/page-footer.test.ts
+++ site/tests/integration/page-footer.test.ts
@@ -3,6 +3,32 @@ import { expect, test } from "playwright-test-coverage";
 test("page footer navigation works", async ({ page }) => {
   await page.goto("/");
 
+  await page.locator("footer").scrollIntoViewIfNeeded();
+
+  const socialLinksSelector = page.locator(
+    "[data-testid='footer-social-links']",
+  );
+  await expect(socialLinksSelector.locator("a").first()).toHaveAttribute(
+    "href",
+    "https://twitter.com/blockprotocol",
+  );
+  await expect(socialLinksSelector.locator("a").nth(1)).toHaveAttribute(
+    "href",
+    "/discord",
+  );
+  await expect(socialLinksSelector.locator("a").nth(2)).toHaveAttribute(
+    "href",
+    "https://github.com/blockprotocol/blockprotocol",
+  );
+  await expect(
+    socialLinksSelector.locator("text=Star us on Github"),
+  ).toHaveAttribute("href", "https://github.com/blockprotocol/blockprotocol");
+
+  await page.locator("footer >> a:has-text('Contact Us')").click();
+  await expect(page).toHaveURL("/contact");
+
+  await page.locator("footer").scrollIntoViewIfNeeded();
+
   await page.locator("footer >> a:has-text('Block Hub')").click();
   await expect(page).toHaveURL("/hub");
 
@@ -15,34 +41,4 @@ test("page footer navigation works", async ({ page }) => {
 
   //   await page.locator("footer>> a:has-text('Publish a Block')").click();
   //   await page.waitForURL("/docs/developing-blocks#publish");
-
-  await page.locator("footer").scrollIntoViewIfNeeded();
-
-  await page.locator("footer >> a:has-text('Contact Us')").click();
-  await expect(page).toHaveURL("/contact");
-
-  await page.locator("footer").scrollIntoViewIfNeeded();
-
-  const socialLinksSelector = page.locator(
-    "[data-testid='footer-social-links']",
-  );
-
-  await expect(socialLinksSelector.locator("a").first()).toHaveAttribute(
-    "href",
-    "https://twitter.com/blockprotocol",
-  );
-
-  await expect(socialLinksSelector.locator("a").nth(1)).toHaveAttribute(
-    "href",
-    "/discord",
-  );
-
-  await expect(socialLinksSelector.locator("a").nth(2)).toHaveAttribute(
-    "href",
-    "https://github.com/blockprotocol/blockprotocol",
-  );
-
-  await expect(
-    socialLinksSelector.locator("text=Star us on Github"),
-  ).toHaveAttribute("href", "https://github.com/blockprotocol/blockprotocol");
 });
