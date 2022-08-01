a76a44d214d77b3976eb2a86e2c464ac6dd4ede2
Nate Higgins <nate@nateh.uk>
2022-08-01 14:25:37 +0100

    Remove broken export

--- packages/@blockprotocol/hook/src/index.ts
+++ packages/@blockprotocol/hook/src/index.ts
@@ -1,5 +1,4 @@
 export { HookBlockHandler } from "./hook-block-handler";
 export { HookEmbedderHandler } from "./hook-embedder-handler";
-export type { BlockComponent } from "./react";
 export { useHookBlockService, useHookEmbedderService } from "./react";
 export * from "./types";
