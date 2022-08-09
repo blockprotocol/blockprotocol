blockprotocol/blockprotocol

commit 9df537ce3426367127de1526f1530e05bc00274a

Include read-only mode checkbox in `mock-block-dock` debug UI (#488)
* include read-only mode checkbox in debug ui

* remove ids from read-only checkbox input

* add changeset file

packages/mock-block-dock/src/mock-block-dock.tsx
@@ -88,17 +88,23 @@ export const MockBlockDock: FunctionComponent<MockBlockDockProps> = ({
   const [graphService, setGraphService] = useState<GraphEmbedderHandler | null>(
     null,
   );
+  const [debugReadonly, setDebugReadonly] = useState<boolean>(!!readonly);
   const wrapperRef = useRef<HTMLDivElement>(null);
+  const prevReadonly = useRef<boolean>(!!readonly);
 
   const propsToInject: BlockGraphProperties<any> = {
     graph: {
-      readonly,
+      readonly: debugReadonly,
       blockEntity,
       blockGraph,
       entityTypes,
       linkedAggregations,
     },
   };
+
+  useEffect(() => {
+    prevReadonly.current = !!readonly;
+  }, [readonly]);
 
   useEffect(() => {
     if (!wrapperRef.current) {
@@ -113,6 +119,7 @@ export const MockBlockDock: FunctionComponent<MockBlockDockProps> = ({
           linkedAggregations,
           callbacks: graphServiceCallbacks,
           element: wrapperRef.current,
+          readonly: debugReadonly,
         }),
       );
     }
@@ -122,6 +129,7 @@ export const MockBlockDock: FunctionComponent<MockBlockDockProps> = ({
     graphService,
     graphServiceCallbacks,
     linkedAggregations,
+    debugReadonly,
   ]);
 
   useEffect(() => {
@@ -150,9 +158,13 @@ export const MockBlockDock: FunctionComponent<MockBlockDockProps> = ({
 
   useEffect(() => {
     if (graphService) {
-      graphService.readonly({ data: readonly });
+      graphService.readonly({ data: debugReadonly });
     }
-  }, [readonly, graphService]);
+  }, [debugReadonly, graphService]);
+
+  if (readonly !== prevReadonly.current && readonly !== debugReadonly) {
+    setDebugReadonly(!!readonly);
+  }
 
   if (!debug) {
     return (
@@ -181,6 +193,15 @@ export const MockBlockDock: FunctionComponent<MockBlockDockProps> = ({
     <div style={{ fontFamily: "sans-serif" }}>
       <div style={{ marginBottom: 30 }}>
         <h3 style={{ marginTop: 0, marginBottom: 3 }}>Block</h3>
+        {/* eslint-disable-next-line jsx-a11y/label-has-associated-control -- https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/issues/869  */}
+        <label style={{ display: "block", marginBottom: 10 }}>
+          <input
+            type="checkbox"
+            checked={debugReadonly}
+            onChange={(evt) => setDebugReadonly(evt.target.checked)}
+          />
+          Read only mode
+        </label>
         <div style={{ padding: 15, border: "1px dashed black" }}>
           <div ref={wrapperRef}>
             {graphService ? (

packages/mock-block-dock/dev/dev-app.tsx
@@ -32,10 +32,9 @@ const DevApp: FunctionComponent = () => {
         <button type="button" onClick={() => setEntityId(entityId + 1)}>
           Increment Entity ID
         </button>
-
-        <label htmlFor="reaodnly-input" style={{ marginLeft: 20 }}>
+        {/* eslint-disable-next-line jsx-a11y/label-has-associated-control -- https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/issues/869 */}
+        <label style={{ marginLeft: 20 }}>
           <input
-            id="readonly-input"
             type="checkbox"
             onChange={(evt) => setReadonly(evt.target.checked)}
           />

site/tests/universal/blockpage.test.ts
@@ -11,6 +11,7 @@ test("Updating block properties should update block preview", async ({
     browserName === "webkit",
     "https://app.asana.com/0/1202542409311090/1202651551651719 (internal)",
   );
+  test.slow(); // @todo Remove after re-engineering block sandbox
 
   await page.goto("/hub");
 

commit fc7665b0143305c2f9ea46ef03af3257edb516ce

fix multiple re-render bug introduced in #488

packages/mock-block-dock/src/mock-block-dock.tsx
@@ -90,7 +90,7 @@ export const MockBlockDock: FunctionComponent<MockBlockDockProps> = ({
   );
   const [debugReadonly, setDebugReadonly] = useState<boolean>(!!readonly);
   const wrapperRef = useRef<HTMLDivElement>(null);
-  const prevReadonly = useRef<boolean>(!!readonly);
+  const prevReadonly = useRef<boolean | undefined>(readonly);
 
   const propsToInject: BlockGraphProperties<any> = {
     graph: {
@@ -103,7 +103,7 @@ export const MockBlockDock: FunctionComponent<MockBlockDockProps> = ({
   };
 
   useEffect(() => {
-    prevReadonly.current = !!readonly;
+    prevReadonly.current = readonly;
   }, [readonly]);
 
   useEffect(() => {