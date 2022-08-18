6b8a1c301a4571739a38f5a1e7dc81b9409b1a89
teenoh <ugbalaachinike@gmail.com>
2022-08-18 14:21:07 +0100

    add properties view

---
+++ packages/mock-block-dock/src/bottom-view/datastore-view.tsx
@@ -0,0 +1,20 @@
+import { JsonView } from "../json-view";
+
+type Props = {
+  datastore: any; // fix types
+};
+
+export const DataStoreView = ({ datastore }: Props) => {
+  return (
+    <JsonView
+      collapseKeys={["entities", "entityTypes", "links", "linkedAggregations"]}
+      rootName="datastore"
+      src={{
+        entities: datastore.entities,
+        entityTypes: datastore.entityTypes,
+        links: datastore.links,
+        linkedAggregations: datastore.linkedAggregationDefinitions
+      }}
+    />
+  );
+};
---
+++ packages/mock-block-dock/src/bottom-view/properties.tsx
@@ -0,0 +1,45 @@
+import { Box, Grid, Switch } from "@mui/material";
+import { Dispatch, SetStateAction } from "react";
+
+import { JsonView } from "../json-view";
+
+// @todo fix types... Types should be similar to mock-block-dock
+type Props = {
+  properties: {
+    readonly: boolean;
+    blockEntity: any;
+  };
+  setReadonly: Dispatch<SetStateAction<boolean>>;
+};
+
+export const PropertiesView = ({ properties, setReadonly }: Props) => {
+  return (
+    <Grid container spacing={2}>
+      <Grid item xs={3} textAlign="right" alignSelf="center">
+        Read only mode
+      </Grid>
+      <Grid item xs={9}>
+        <Switch
+          checked={properties.readonly}
+          onChange={evt => setReadonly(evt.target.checked)}
+        />
+      </Grid>
+      <Grid item xs={3} textAlign="right">
+        Block Entity Properties
+      </Grid>
+      <Grid item xs={9}>
+        <Box maxWidth={800}>
+          <JsonView
+            collapseKeys={["graph"]}
+            rootName="blockEntity"
+            src={properties.blockEntity}
+            //   onEdit={() => {
+            //     return true;
+            //   }}
+            //   onAdd={() => {}}
+          />
+        </Box>
+      </Grid>
+    </Grid>
+  );
+};
