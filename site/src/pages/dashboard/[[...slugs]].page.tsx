40547d0aedeb654ffb7554f60dc0984c5090cdc3
Yusuf Kinatas <yusufkinatas@gmail.com>
2022-08-19 17:07:51 +0300

    update dashboard content

--- site/src/pages/dashboard/[[...slugs]].page.tsx
+++ site/src/pages/dashboard/[[...slugs]].page.tsx
@@ -1,5 +1,4 @@
-import { faKey, faUser } from "@fortawesome/free-solid-svg-icons";
-import { Box, Container, Typography } from "@mui/material";
+import { Typography } from "@mui/material";
 import Head from "next/head";
 import { useRouter } from "next/router";
 import { useCallback, useEffect, useState } from "react";
@@ -9,81 +8,15 @@ import {
   AuthWallPageContent,
   withAuthWall,
 } from "../../components/pages/auth-wall";
-import {
-  DashboardCard,
-  DashboardCardProps,
-} from "../../components/pages/dashboard/dashboard-card/dashboard-card";
+import { DashboardCard } from "../../components/pages/dashboard/dashboard-card/dashboard-card";
+import { SectionGrid } from "../../components/pages/dashboard/dashboard-section-grid";
+import { SectionTitle } from "../../components/pages/dashboard/dashboard-section-title";
+import { PageContainer } from "../../components/pages/dashboard/page-container";
 import { TopNavigationTabs } from "../../components/pages/dashboard/top-navigation-tabs";
-
-const getDashboardPrimaryCardData = (
-  openCreateSchemaModal: () => void,
-): DashboardCardProps[] => [
-  {
-    title: "Read our quick start guide to building blocks",
-    colorGradient:
-      "linear-gradient(291.34deg, #FFB172 -4.12%, #D082DE 53.49%, #9482FF 91.74%, #84E6FF 151.68%)",
-    colorGradientOnHover:
-      "linear-gradient(292.67deg, #FFB172 44.38%, #D082DE 83.44%, #9482FF 109.38%, #84E6FF 150.02%)",
-    description:
-      "Learn how to build blocks that can integrate with the block protocol and be published to the registry.",
-    link: {
-      title: "Read the guide",
-      href: "/docs/developing-blocks",
-    },
-  },
-  {
-    title: "Test out blocks in the playground",
-    colorGradient:
-      "linear-gradient(310.17deg, #FFB172 -167.67%, #9482FF 13.54%, #84E6FF 126.83%)",
-    colorGradientOnHover:
-      "linear-gradient(304.41deg, #FFB172 -167.57%, #9482FF -22.66%, #84E6FF 53.07%)",
-    description:
-      "Try out our most popular blocks to learn how they consume and render different data structures",
-    link: {
-      title: "Try out blocks",
-      href: "/hub",
-    },
-  },
-  {
-    title: "Create a Schema",
-    colorGradient:
-      "linear-gradient(91.21deg, #FFB172 -84.62%, #9482FF 62.56%, #84E6FF 154.58%)",
-    colorGradientOnHover:
-      "linear-gradient(91.21deg, #FFB172 -84.54%, #9482FF 27.8%, #84E6FF 98.03%)",
-    description:
-      "Schemas are a formal way to describe data – they define the expected properties of things like People, Companies, and Books",
-    link: {
-      title: "Create a schema",
-      onClick: openCreateSchemaModal,
-    },
-  },
-];
-
-const getDashboardSecondaryCardData = (
-  profileLink: string,
-): DashboardCardProps[] => [
-  {
-    title: "Generate your API key",
-    description:
-      "Your API key will allow you to search for blocks by name, author, or compatible data structure.",
-    link: {
-      title: "Generate Key",
-      href: "/settings/api-keys",
-    },
-    icon: faKey,
-  },
-  {
-    title: "View your public profile",
-    description:
-      "See what others see and browse your published blocks and schemas",
-    link: {
-      title: "View Profile",
-      href: profileLink,
-    },
-    icon: faUser,
-    variant: "secondary",
-  },
-];
+import {
+  DashboardSection,
+  getDashboardSectionCards,
+} from "../../components/pages/dashboard/utils";
 
 const Dashboard: AuthWallPageContent = ({ user }) => {
   const { preferredName: userName, shortname } = user ?? {};
@@ -100,6 +33,16 @@ const Dashboard: AuthWallPageContent = ({ user }) => {
     setSchemaModalOpen(true);
   }, [setSchemaModalOpen]);
 
+  const dashboardCards = getDashboardSectionCards({
+    profileLink: `/@${shortname}`,
+    openCreateSchemaModal: openSchemaModal,
+  });
+
+  const renderDashboardCards = (section: DashboardSection) =>
+    dashboardCards[section].map((card) => (
+      <DashboardCard key={card.title} {...card} />
+    ));
+
   return (
     <>
       <Head>
@@ -108,94 +51,33 @@ const Dashboard: AuthWallPageContent = ({ user }) => {
 
       <TopNavigationTabs />
 
-      <Box
-        sx={{
-          background:
-            "linear-gradient(180deg, #FAFBFC 0%, rgba(250, 251, 252, 0) 100%)",
-        }}
-      >
-        <Container
-          sx={{
-            paddingTop: {
-              xs: 5,
-              md: 9,
-            },
-            paddingBottom: {
-              xs: 5,
-              md: 9,
-            },
+      <PageContainer sx={{ display: "flex", flexDirection: "column", gap: 3 }}>
+        <Typography variant="bpHeading2" mb={-2}>
+          Welcome Back, {userName}!
+        </Typography>
+
+        <SectionTitle mb={-1}>Create</SectionTitle>
+
+        <SectionGrid
+          gridTemplateColumns={{
+            xs: "1fr",
+            sm: "1fr 1fr",
+            md: "1fr 1fr 1fr",
           }}
+          columnGap={2}
         >
-          <Typography
-            variant="bpHeading2"
-            sx={{
-              marginBottom: 2,
-            }}
-          >
-            Welcome Back, {userName}!
-          </Typography>
+          {renderDashboardCards("create")}
+        </SectionGrid>
 
-          <Typography
-            variant="bpSmallCaps"
-            maxWidth={750}
-            sx={{
-              mt: {
-                xs: 3,
-                md: 6,
-              },
-              letterSpacing: "0.05em",
-              textTransform: "uppercase",
-              color: ({ palette }) => palette.gray[70],
-              fontWeight: 600,
-              mb: 2,
-            }}
-          >
-            Start Exploring
-          </Typography>
+        <SectionTitle>Manage</SectionTitle>
 
-          <Box
-            display="grid"
-            gridTemplateColumns={{
-              xs: "1fr",
-              sm: "1fr 1fr",
-              md: "1fr 1fr 1fr",
-            }}
-            columnGap={2}
-            rowGap={3}
-            mb={3}
-          >
-            {getDashboardPrimaryCardData(openSchemaModal).map(
-              (dashboardCard) => (
-                <DashboardCard
-                  key={`dashboardCard-${dashboardCard.link.href}`}
-                  {...dashboardCard}
-                />
-              ),
-            )}
-          </Box>
-          <Box
-            display="grid"
-            gridTemplateColumns={{
-              xs: "1fr",
-              md: "1fr 1fr",
-            }}
-            alignItems="flex-start"
-            columnGap={3}
-            rowGap={3}
-            paddingBottom={4}
-          >
-            {getDashboardSecondaryCardData(`/@${shortname}`).map(
-              (dashboardCard) => (
-                <DashboardCard
-                  key={`dashboardCard-${dashboardCard.link.href}`}
-                  {...dashboardCard}
-                  variant="secondary"
-                />
-              ),
-            )}
-          </Box>
-        </Container>
-      </Box>
+        <SectionGrid>{renderDashboardCards("manage")}</SectionGrid>
+
+        <SectionTitle>Explore</SectionTitle>
+
+        <SectionGrid>{renderDashboardCards("explore")}</SectionGrid>
+      </PageContainer>
+
       <CreateSchemaModal
         open={schemaModalOpen}
         onClose={() => setSchemaModalOpen(false)}
