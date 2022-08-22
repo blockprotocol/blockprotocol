697c4115e758f1da409ef713e0c8d9c94174636f
Ciaran Morinan <c@hash.ai>
2022-08-22 10:22:32 +0100

    fix another test

--- site/tests/integration/hubpage.test.ts
+++ site/tests/integration/hubpage.test.ts
@@ -4,7 +4,7 @@ import blocksData from "../../blocks-data.json";
 import type { ExpandedBlockMetadata } from "../../src/lib/blocks";
 
 const codeBlockMetadata = (blocksData as ExpandedBlockMetadata[]).find(
-  ({ name }) => name === "block-code",
+  ({ pathWithNamespace }) => pathWithNamespace === "@hash/code",
 );
 
 if (!codeBlockMetadata) {
