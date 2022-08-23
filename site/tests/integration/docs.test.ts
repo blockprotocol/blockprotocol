7685b312a697d3529e290d6d9cc1e76204a4066e
Alexander Kachkaev <alexander@kachkaev.ru>
2022-08-23 11:56:43 +0100

    Change selector in tests

--- site/tests/integration/docs.test.ts
+++ site/tests/integration/docs.test.ts
@@ -105,13 +105,12 @@ test("Docs page should contain key elements and interactions should work", async
     page.locator("text=PreviousEmbedding Blocks >> a"),
   ).toHaveAttribute("href", "/docs/embedding-blocks");
 
-  await expect(
-    page.locator("text=NextCore Specification 0.2 >> a"),
-  ).toBeVisible();
+  await expect(page.locator("text=NextCore Service >> a")).toBeVisible();
 
-  await expect(
-    page.locator("text=NextCore Specification 0.2 >> a"),
-  ).toHaveAttribute("href", "/docs/spec/core-specification");
+  await expect(page.locator("text=NextCore Service >> a")).toHaveAttribute(
+    "href",
+    "/docs/spec/core-specification",
+  );
 
   if (isMobile) {
     await openMobileNav(page);
