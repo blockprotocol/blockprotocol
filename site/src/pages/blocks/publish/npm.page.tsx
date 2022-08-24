cd3aedb0ac4c4cac21468399acb3281bc8711ee1
Yusuf Kinatas <yusufkinatas@gmail.com>
2022-08-24 08:20:19 +0300

    remove `alert` theme, make a component instead

--- site/src/pages/blocks/publish/npm.page.tsx
+++ site/src/pages/blocks/publish/npm.page.tsx
@@ -85,6 +85,70 @@ const FieldInfoWrapper = ({
   );
 };
 
+const PublishBlockInfo = () => {
+  return (
+    <Alert
+      icon={<FontAwesomeIcon icon={faInfoCircle} />}
+      severity="info"
+      sx={theme => ({
+        width: "100%",
+        alignItems: "center",
+        padding: theme.spacing(3, 4),
+        background: theme.palette.purple[100],
+
+        [`.${alertClasses.icon}`]: {
+          marginRight: theme.spacing(3),
+          svg: {
+            color: theme.palette.purple[200],
+            fontSize: theme.spacing(4)
+          }
+        },
+
+        [`.${alertClasses.message}`]: {
+          flex: 1,
+          display: "flex",
+          flexWrap: "wrap",
+          gap: 2,
+          overflow: "visible",
+          padding: 0
+        }
+      })}
+    >
+      <Box flexGrow={1}>
+        <Typography
+          variant="bpSmallCaps"
+          fontWeight={500}
+          mb={1.5}
+          color={({ palette }) => palette.gray[90]}
+        >
+          Adding a new block will make it public on the hub
+        </Typography>
+        <Typography
+          variant="bpSmallCopy"
+          color={({ palette }) => palette.gray[80]}
+        >
+          Your package must expose a valid{" "}
+          <Typography variant="bpCode">block-metadata.json</Typography> file.
+        </Typography>
+      </Box>
+
+      <Link
+        variant="bpSmallCopy"
+        alignSelf="flex-end"
+        href="/docs/developing-blocks#lifecycle-of-a-block"
+        target="_blank"
+        sx={{
+          display: "flex",
+          alignItems: "center"
+        }}
+      >
+        Learn more in the docs{" "}
+        <FontAwesomeIcon sx={{ ml: 1 }} icon={faExternalLink} />
+      </Link>
+    </Alert>
+  );
+};
+
 const PublishFromNPMPage = () => {
   const { register, handleSubmit, formState } = useForm<FormValues>({
     mode: "onChange"
@@ -118,48 +182,7 @@ const PublishFromNPMPage = () => {
               alignItems: "flex-start"
             }}
           >
-            <Alert
-              icon={<FontAwesomeIcon icon={faInfoCircle} />}
-              severity="info"
-              sx={{
-                width: "100%",
-                [`.${alertClasses.message}`]: {
-                  flex: 1,
-                  display: "flex",
-                  flexWrap: "wrap",
-                  // justifyContent: "flex-end",
-                  gap: 2
-                }
-              }}
-            >
-              <Box flexGrow={1}>
-                <Typography
-                  variant="bpSmallCaps"
-                  fontWeight={500}
-                  mb={1.5}
-                  color={({ palette }) => palette.gray[90]}
-                >
-                  Adding a new block will make it public on the hub
-                </Typography>
-                <Typography
-                  variant="bpSmallCopy"
-                  color={({ palette }) => palette.gray[80]}
-                >
-                  Your package must expose a valid{" "}
-                  <Typography variant="bpCode">block-metadata.json</Typography>{" "}
-                  file.
-                </Typography>
-              </Box>
-
-              <Link
-                variant="bpSmallCopy"
-                alignSelf="flex-end"
-                href="/docs/developing-blocks#lifecycle-of-a-block"
-                target="_blank"
-              >
-                Learn more in the docs <FontAwesomeIcon icon={faExternalLink} />
-              </Link>
-            </Alert>
+            <PublishBlockInfo />
 
             <BlockFormSection title="Where is your block located?">
               <FieldInfoWrapper
