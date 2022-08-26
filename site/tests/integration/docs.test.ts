06715dd191d9088d5d6a84665992c8d6a05c85e1
Alexander Kachkaev <alexander@kachkaev.ru>
2022-08-26 17:00:51 +0100

    Fix test

--- site/tests/integration/docs.test.ts
+++ site/tests/integration/docs.test.ts
@@ -105,11 +105,12 @@ test("Docs page should contain key elements and interactions should work", async
     page.locator("text=PreviousEmbedding Blocks >> a"),
   ).toHaveAttribute("href", "/docs/embedding-blocks");
 
-  await expect(page.locator("text=NextCore Specification >> a")).toBeVisible();
+  await expect(page.locator("text=NextCore >> a")).toBeVisible();
 
-  await expect(
-    page.locator("text=NextCore Specification >> a"),
-  ).toHaveAttribute("href", "/docs/spec/core-specification");
+  await expect(page.locator("text=NextCore >> a")).toHaveAttribute(
+    "href",
+    "/docs/spec/core-specification",
+  );
 
   if (isMobile) {
     await openMobileNav(page);
