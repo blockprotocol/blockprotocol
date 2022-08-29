e2268c62087504af08ae83f837c35ce86b344bba
Yusuf Kinatas <yusufkinatas@gmail.com>
2022-08-29 09:10:13 +0300

    move `Alert` styles to mui theme options

--- site/src/components/alert.tsx
+++ site/src/components/alert.tsx
@@ -8,74 +8,22 @@ import {
 import {
   // eslint-disable-next-line no-restricted-imports
   Alert as MuiAlert,
-  alertClasses,
   AlertColor,
   AlertProps as MuiAlertProps,
   Box,
-  Palette,
   Typography,
   useTheme,
 } from "@mui/material";
-import { ReactNode, useMemo } from "react";
+import { ReactNode } from "react";
 
 import { FontAwesomeIcon } from "./icons";
 
-const getTypeProps = (
-  palette: Palette,
-): Record<
-  AlertColor,
-  {
-    icon: IconDefinition;
-    colors: {
-      icon: string;
-      border: string;
-      bg: string;
-      codeBg: string;
-      codeBorder: string;
-    };
-  }
-> => ({
-  error: {
-    icon: faXmarkCircle,
-    colors: {
-      icon: palette.red[600],
-      bg: palette.red[100],
-      border: palette.red[300],
-      codeBg: palette.red[200],
-      codeBorder: palette.red[300],
-    },
-  },
-  info: {
-    icon: faInfoCircle,
-    colors: {
-      icon: palette.purple[200],
-      bg: palette.purple[100],
-      border: palette.purple[300],
-      codeBg: palette.purple[20],
-      codeBorder: palette.purple[30],
-    },
-  },
-  success: {
-    icon: faCheckCircle,
-    colors: {
-      icon: palette.green[60],
-      bg: palette.green[10],
-      border: palette.green[30],
-      codeBg: palette.green[20],
-      codeBorder: palette.green[40],
-    },
-  },
-  warning: {
-    icon: faExclamationCircle,
-    colors: {
-      icon: palette.orange[300],
-      bg: palette.orange[100],
-      border: palette.orange[300],
-      codeBg: palette.orange[200],
-      codeBorder: palette.orange[300],
-    },
-  },
-});
+const icons: Record<AlertColor, IconDefinition> = {
+  error: faXmarkCircle,
+  info: faInfoCircle,
+  success: faCheckCircle,
+  warning: faExclamationCircle,
+};
 
 interface AlertProps {
   title: string;
@@ -90,50 +38,14 @@ export const Alert = ({
   description,
   type = "info",
   extraContent,
-  sx = [],
+  sx,
 }: AlertProps) => {
-  const { palette, spacing } = useTheme();
+  const { palette } = useTheme();
 
-  const typeProps = useMemo(() => getTypeProps(palette)[type], [type, palette]);
-  const { icon, colors } = typeProps;
+  const icon = icons[type];
 
   return (
-    <MuiAlert
-      icon={<FontAwesomeIcon icon={icon} />}
-      severity={type}
-      sx={[
-        {
-          border: `1px solid ${colors.border}`,
-          width: "100%",
-          alignItems: "center",
-          padding: spacing(3, 4),
-          background: colors.bg,
-
-          [`.${alertClasses.icon}`]: {
-            marginRight: spacing(3),
-            svg: {
-              color: colors.icon,
-              fontSize: spacing(4),
-            },
-          },
-
-          [`.${alertClasses.message}`]: {
-            flex: 1,
-            display: "flex",
-            flexWrap: "wrap",
-            gap: 2,
-            overflow: "visible",
-            padding: 0,
-          },
-
-          code: {
-            background: colors.codeBg,
-            borderColor: colors.codeBorder,
-          },
-        },
-        ...(Array.isArray(sx) ? sx : [sx]),
-      ]}
-    >
+    <MuiAlert icon={<FontAwesomeIcon icon={icon} />} severity={type} sx={sx}>
       <Box flexGrow={1}>
         <Typography
           variant="bpSmallCaps"
