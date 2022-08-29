d11e6f60a7e33172255cae55b7a4153aa04378e6
Alfred Mountfield <alfie.mountfield@gmail.com>
2022-08-29 15:45:14 +0100

    TryFrom for OneOf

--- crates/type-system/src/ontology/shared/one_of.rs
+++ crates/type-system/src/ontology/shared/one_of.rs
@@ -3,47 +3,39 @@ pub(in crate::ontology) mod repr {
     #[cfg(target_arch = "wasm32")]
     use {tsify::Tsify, wasm_bindgen::prelude::*};
 
-    use crate::ontology::shared::validate::ValidationError;
-
     #[cfg_attr(target_arch = "wasm32", derive(Tsify))]
     #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
     #[serde(rename_all = "camelCase", deny_unknown_fields)]
-    pub(crate) struct OneOf<T> {
+    pub struct OneOf<T> {
         // TODO: tsify doesn't seem to let us override the type of this without losing the generic
         //  see https://github.com/madonoharu/tsify/issues/4
         //  we want something like #[cfg_attr(target_arch = "wasm32", tsify(type = "[T, ...T[]]"))]
-        pub(super) one_of: Vec<T>,
+        pub one_of: Vec<T>,
     }
 
-    impl<T> OneOf<T> {
-        /// Creates a new `OneOf` without validating.
-        pub fn new_unchecked<U: Into<Vec<T>>>(one_of: U) -> Self {
+    impl<T, R> TryFrom<OneOf<R>> for super::OneOf<T>
+    where
+        T: TryFrom<R>,
+    {
+        // TODO
+        type Error = ();
+
+        fn try_from(one_of_repr: OneOf<R>) -> Result<Self, Self::Error> {
+            let inner = one_of_repr
+                .one_of
+                .into_iter()
+                .map(|ele| ele.try_into().map_err(|err| ()))
+                .collect::<Result<Vec<_>, Self::Error>>()?;
+
+            Ok(Self::new(inner).map_err(|err| ())?)
+        }
+    }
+
+    impl<T> From<super::OneOf<T>> for OneOf<T> {
+        fn from(one_of: super::OneOf<T>) -> Self {
             Self {
-                one_of: one_of.into(),
+                one_of: one_of.one_of,
             }
-        }
-
-        /// Creates a new `OneOf` from the given vector.
-        ///
-        /// # Errors
-        ///
-        /// - [`ValidationError`] if the object is not in a valid state.
-        pub fn new<U: Into<Vec<T>>>(one_of: U) -> Result<Self, ValidationError> {
-            let one_of = Self::new_unchecked(one_of);
-            one_of.validate()?;
-            Ok(one_of)
-        }
-
-        #[must_use]
-        pub fn one_of(&self) -> &[T] {
-            &self.one_of
-        }
-
-        fn validate(&self) -> Result<(), ValidationError> {
-            if self.one_of().is_empty() {
-                return Err(ValidationError::EmptyOneOf);
-            }
-            Ok(())
         }
     }
 }
@@ -84,14 +76,6 @@ impl<T> OneOf<T> {
             return Err(ValidationError::EmptyOneOf);
         }
         Ok(())
-    }
-}
-
-impl<T> TryFrom<repr::OneOf<T>> for OneOf<T> {
-    type Error = ValidationError;
-
-    fn try_from(one_of: repr::OneOf<T>) -> Result<Self, Self::Error> {
-        Self::new(one_of.one_of)
     }
 }
 
