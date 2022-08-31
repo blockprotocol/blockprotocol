c0b3bfc992834f1f3e80215b175564a386ba534c
Alexander Kachkaev <alexander@kachkaev.ru>
2022-08-31 14:15:15 +0100

    Prevent direct access to sandbox

--- site/src/middleware.page.ts
+++ site/src/middleware.page.ts
@@ -5,18 +5,33 @@ import { NextResponse } from "next/server";
 
 import { SESSION_COOKIE_NAME } from "./lib/api/middleware/constants";
 
+const productionHost = "blockprotocol.org";
+const productionSandboxHost = "www.blocksandbox.org";
+
 export async function middleware(request: NextRequest) {
-  const { cookies } = request;
+  const url = new URL(request.url);
+  const openingBlockSandboxPage = url.pathname.endsWith("/sandboxed-demo");
 
-  // destroying session at `logout.api.ts` removes this cookie
-  const isLoggedIn = !!cookies.get(SESSION_COOKIE_NAME);
+  if (url.host === productionHost && openingBlockSandboxPage) {
+    const newUrl = new URL(url);
+    newUrl.host = productionSandboxHost;
+    return NextResponse.redirect(newUrl);
+  }
 
-  if (isLoggedIn) {
-    return NextResponse.redirect(new URL("/dashboard", request.url));
+  if (url.host === productionSandboxHost && !openingBlockSandboxPage) {
+    const newUrl = new URL(url);
+    newUrl.host = productionHost;
+    return NextResponse.redirect(newUrl);
+  }
+
+  if (url.pathname === "/") {
+    const { cookies } = request;
+
+    // destroying session at `logout.api.ts` removes this cookie
+    const isLoggedIn = !!cookies.get(SESSION_COOKIE_NAME);
+
+    if (isLoggedIn) {
+      return NextResponse.redirect(new URL("/dashboard", request.url));
+    }
   }
 }
-
-export const config = {
-  // only redirect on homepage
-  matcher: "/",
-};
