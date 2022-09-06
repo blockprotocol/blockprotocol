6ca50aaf85446fc13c248004439d1929ff9261a0
Ciaran Morinan <c@hash.ai>
2022-09-06 16:16:05 +0100

    correct return type

--- packages/blockprotocol/cli/commands/publish/shared/find-project-root.js
+++ packages/blockprotocol/cli/commands/publish/shared/find-project-root.js
@@ -3,7 +3,7 @@ import path from "node:path";
 
 /**
  * Find the path to the project root, by searching upwards for the nearest package.json
- * @returns {Promise<string>}
+ * @returns {Promise<string|undefined>}
  */
 export const findProjectRoot = async () => {
   const packageJson = await findUp("package.json");
