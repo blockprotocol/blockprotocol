f7acca9021a3c8c1e769b1b44b8a37ad6a291067
Ciaran Morinan <37743469+CiaranMn@users.noreply.github.com>
2022-09-07 11:34:26 +0100

    Introduce blockprotocol CLI (#575)

---
+++ packages/blockprotocol/cli/commands.js
@@ -0,0 +1,7 @@
+import * as help from "./commands/help.js";
+import * as publish from "./commands/publish.js";
+
+export const commandLookup = {
+  help,
+  publish,
+};
--- packages/blockprotocol/core.js
+++
@@ -1,2 +0,0 @@
-// Current version of this package only provides TypeScript types.
-// Watch https://github.com/blockprotocol/blockprotocol for the updates!
