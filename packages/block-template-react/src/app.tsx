dfde40f382ad206d9094abeada0b9b8c6bda3899
Ciaran Morinan <c@hash.ai>
2022-09-09 16:33:30 +0100

    wip

--- packages/block-template-react/src/app.tsx
+++ packages/block-template-react/src/app.tsx
@@ -2,14 +2,39 @@ import {
   BlockComponent,
   useGraphBlockService,
 } from "@blockprotocol/graph/react";
-import { useCallback, useRef } from "react";
+import { useRef } from "react";
 
+/**
+ * This file provides some base styling for your block.
+ * You can delete it and write your own, but we encourage you to keep the styling scoped, e.g.
+ * - use CSS modules (this approach)
+ * - use a CSS-in-JS solution
+ * - use a shadow DOM
+ * all of these ensure that your styling does not affect anything outside your block
+ */
 import styles from "./base.module.scss";
 
+/**
+ * This defines the properties of the entity your block expects to be sent.
+ * This entity is available to your block on props.graph.blockEntity.
+ * To change the structure of the entity your block expects, change this type.
+ */
 type BlockEntityProperties = {
   name: string;
 };
 
+/**
+ * This function is to help illustrate a property being change when the button is pressed.
+ * You can remove it when you remove the button below.
+ */
+const supplyRandomName = () => {
+  const names = ["Alice", "Bob", "Carol", "Dave", "Erin", "Frank"];
+  return names[Math.floor(Math.random() * names.length)];
+};
+
+/**
+ * This is the entry point for your block – it is the component w
+ */
 export const App: BlockComponent<BlockEntityProperties> = ({
   graph: {
     blockEntity: { entityId, properties },
@@ -17,14 +42,6 @@ export const App: BlockComponent<BlockEntityProperties> = ({
 }) => {
   const blockRootRef = useRef<HTMLDivElement>(null);
   const { graphService } = useGraphBlockService(blockRootRef);
-
-  const updateSelf = useCallback(
-    (newProperties: Partial<BlockEntityProperties>) =>
-      graphService?.updateEntity({
-        data: { properties: newProperties, entityId },
-      }),
-    [entityId, graphService],
-  );
 
   const { name } = properties;
 
@@ -36,7 +53,14 @@ export const App: BlockComponent<BlockEntityProperties> = ({
         e.g. by calling <code>updateEntity</code>.
       </p>
       <button
-        onClick={() => updateSelf({ name: Math.random().toString() })}
+        onClick={() =>
+          graphService?.updateEntity({
+            data: {
+              entityId,
+              properties: { name: supplyRandomName() },
+            },
+          })
+        }
         type="button"
       >
         Update Name
