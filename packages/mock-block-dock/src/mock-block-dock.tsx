6a8b3dabd1dd54badfa7612e199d0c5911b28206
Ciaran Morinan <37743469+CiaranMn@users.noreply.github.com>
2022-09-13 09:46:20 +0100

    Mock Block Dock debug UI: improve clarity (#593)
    * make docs link open in new tab
    
    * only send graph messages to block if value has changed
    
    * add missing block message to type
    
    * add box around block content in debug mode. make debug panel resizeable handle target bigger
    
    * make entire log row clickable
    
    * make log view into table with header
    
    * add changesets
    
    * lint fixes
    
    * fix DOM nesting and missing value issue
    
    * Update tab-panel.tsx
    
    * Update packages/mock-block-dock/src/debug-view/dev-tools/logs-view.tsx
    
    Co-authored-by: Nate Higgins <nate@nateh.uk>
    
    * Update packages/mock-block-dock/src/use-send-graph-value.ts
    
    Co-authored-by: Nate Higgins <nate@nateh.uk>
    
    * Update packages/mock-block-dock/src/debug-view/dev-tools/logs-view.tsx
    
    Co-authored-by: Nate Higgins <nate@nateh.uk>
    
    * use correct dom elements. use correct sx pass through
    
    * add comment to resizeh andle
    
    * Update use-send-graph-value.ts
    
    Co-authored-by: Nate Higgins <nate@nateh.uk>

--- packages/mock-block-dock/src/mock-block-dock.tsx
+++ packages/mock-block-dock/src/mock-block-dock.tsx
@@ -20,6 +20,7 @@ import { DebugView } from "./debug-view";
 import { OffSwitch } from "./debug-view/icons";
 import { MockBlockDockProvider } from "./mock-block-dock-context";
 import { useMockBlockProps } from "./use-mock-block-props";
+import { useSendGraphValue } from "./use-send-graph-value";
 
 type BlockDefinition =
   | { ReactComponent: ComponentType<any> }
@@ -136,6 +137,7 @@ export const MockBlockDock: FunctionComponent<MockBlockDockProps> = ({
         new GraphEmbedderHandler({
           blockGraph,
           blockEntity,
+          entityTypes,
           linkedAggregations,
           callbacks: graphServiceCallbacks,
           element: wrapperRef.current,
@@ -146,41 +148,38 @@ export const MockBlockDock: FunctionComponent<MockBlockDockProps> = ({
   }, [
     blockEntity,
     blockGraph,
+    entityTypes,
     graphService,
     graphServiceCallbacks,
     linkedAggregations,
     readonly,
   ]);
 
-  useEffect(() => {
-    if (graphService) {
-      graphService.blockEntity({ data: blockEntity });
-    }
-  }, [blockEntity, graphService]);
-
-  useEffect(() => {
-    if (graphService) {
-      graphService.blockGraph({ data: blockGraph });
-    }
-  }, [blockGraph, graphService]);
-
-  useEffect(() => {
-    if (graphService) {
-      graphService.entityTypes({ data: entityTypes });
-    }
-  }, [entityTypes, graphService]);
-
-  useEffect(() => {
-    if (graphService) {
-      graphService.linkedAggregations({ data: linkedAggregations });
-    }
-  }, [linkedAggregations, graphService]);
-
-  useEffect(() => {
-    if (graphService) {
-      graphService.readonly({ data: readonly });
-    }
-  }, [readonly, graphService]);
+  useSendGraphValue({
+    graphService,
+    value: blockEntity,
+    valueName: "blockEntity",
+  });
+  useSendGraphValue({
+    graphService,
+    value: blockGraph,
+    valueName: "blockGraph",
+  });
+  useSendGraphValue({
+    graphService,
+    value: entityTypes,
+    valueName: "entityTypes",
+  });
+  useSendGraphValue({
+    graphService,
+    value: linkedAggregations,
+    valueName: "linkedAggregations",
+  });
+  useSendGraphValue({
+    graphService,
+    value: readonly,
+    valueName: "readonly",
+  });
 
   useEffect(() => {
     if (graphService) {
@@ -189,7 +188,17 @@ export const MockBlockDock: FunctionComponent<MockBlockDockProps> = ({
   }, [graphService, graphServiceCallbacks]);
 
   const Component = (
-    <div ref={wrapperRef}>
+    <div
+      ref={wrapperRef}
+      style={
+        debugMode
+          ? {
+              border: "1px dashed rgb(0,0,0,0.1)",
+              marginTop: 30,
+            }
+          : {}
+      }
+    >
       {graphService ? (
         <BlockRenderer
           customElement={
