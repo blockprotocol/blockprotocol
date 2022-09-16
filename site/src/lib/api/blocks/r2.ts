3943da8965ac88ff312eb36811ea44e466165170
Yusuf Kinatas <yusufkinatas@gmail.com>
2022-09-16 21:50:25 +0300

    sanitize image & icon urls

--- site/src/lib/api/blocks/r2.ts
+++ site/src/lib/api/blocks/r2.ts
@@ -262,7 +262,7 @@ export const validateExpandAndUploadBlockFiles = async ({
 
   const now = new Date().toISOString();
 
-  const expandedMetadata = expandBlockMetadata({
+  const expandedMetadata = await expandBlockMetadata({
     metadata: metadataJson,
     source: sourceInformation,
     timestamps: { createdAt: createdAt ?? now, lastUpdated: now },
--- site/src/lib/blocks.ts
+++ site/src/lib/blocks.ts
@@ -3,14 +3,46 @@ import {
   BlockMetadataRepository,
   JsonObject,
 } from "@blockprotocol/core";
+import axios from "axios";
 import fs from "fs-extra";
 import { globby } from "globby";
 import hostedGitInfo from "hosted-git-info";
 import path from "node:path";
 import sanitize from "sanitize-html";
 
+import { publicBaseR2Url } from "./api/blocks/r2";
 import { FRONTEND_URL } from "./config";
 
+/** @todo move this to a utils file */
+const isSvgSafe = (svgContent: string) => {
+  const hasUnsafeContent = svgContent.match(
+    /(script|entity|onerror|onload|onmouseover|onclick|onfocus|foreignObject)/i,
+  );
+
+  return !hasUnsafeContent;
+};
+
+/** @todo move this to a utils file */
+const sanitizeImageUrl = async (url: string) => {
+  if (!url.includes("svg")) return url;
+
+  let svgContent = "";
+
+  try {
+    const { data } = await axios.get(url);
+    svgContent = data;
+  } catch (error) {
+    //
+  }
+
+  if (svgContent && isSvgSafe(svgContent)) {
+    return url;
+  }
+
+  return "";
+};
+
+/** @todo move this to a utils file */
 const sanitizeUrl = (url: string) => {
   const results = sanitize(`<a href="${url}" />`, {
     allowedAttributes: {
@@ -116,7 +148,7 @@ export const getRepositoryUrl = (
   return undefined;
 };
 
-export const expandBlockMetadata = ({
+export const expandBlockMetadata = async ({
   timestamps,
   includesExampleGraph,
   metadata,
@@ -133,7 +165,7 @@ export const expandBlockMetadata = ({
   };
   timestamps: { createdAt?: string; lastUpdated?: string };
   includesExampleGraph: boolean;
-}): ExpandedBlockMetadata => {
+}): Promise<ExpandedBlockMetadata> => {
   const {
     blockDistributionFolderUrl,
     npmPackageName,
@@ -159,6 +191,22 @@ export const expandBlockMetadata = ({
     throw new Error(`Malformed pathWithNamespace ${pathWithNamespace}`);
   }
 
+  /**
+   * check if "image" or "icon" has "publicBaseR2Url" in it, if it has, then download image,
+   * @todo I did not checked if this is actually working from CLI,
+   * just tested sanitizeImageUrl on local images & confirmed that it's working
+   * we should make sure this works as expected on a real case
+   */
+  let image = generateBlockFileUrl(metadata.image, blockDistributionFolderUrl);
+  if (image?.includes(publicBaseR2Url)) {
+    image = await sanitizeImageUrl(image);
+  }
+
+  let icon = generateBlockFileUrl(metadata.icon, blockDistributionFolderUrl);
+  if (icon?.includes(publicBaseR2Url)) {
+    icon = await sanitizeImageUrl(icon);
+  }
+
   // eslint-disable-next-line no-param-reassign -- could make a new object, but would need to update for any new metadata fields
   delete metadata.devReloadEndpoint;
 
@@ -172,8 +220,8 @@ export const expandBlockMetadata = ({
     componentId: blockDistributionFolderUrl,
     createdAt: timestamps.createdAt,
     displayName: metadata.displayName ?? name,
-    icon: generateBlockFileUrl(metadata.icon, blockDistributionFolderUrl),
-    image: generateBlockFileUrl(metadata.image, blockDistributionFolderUrl),
+    icon,
+    image,
     lastUpdated: timestamps.lastUpdated,
     name,
     npmPackageName,
