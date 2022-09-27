a7c89be3da422059f4f2aaa25b44b038d6004ae6
Alexander Kachkaev <alexander@kachkaev.ru>
2022-09-27 14:52:24 +0100

    Revert "Add tmpdir api endpoint"
    This reverts commit cafa76b060637733e330a2c2f067ea20e328f716.

--- site/src/pages/api/tmpdir.api.ts
+++
@@ -1,9 +0,0 @@
-import { NextApiHandler } from "next";
-import os from "node:os";
-
-const handler: NextApiHandler = async (req, res) => {
-  res.write(os.tmpdir());
-  res.end();
-};
-
-export default handler;
