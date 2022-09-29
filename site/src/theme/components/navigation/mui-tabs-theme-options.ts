fb1af9f270ecbb9c6a8faaa29ea59a5d26427d98
Alexander Kachkaev <alexander@kachkaev.ru>
2022-09-29 13:30:31 +0100

    Fix several markup issues (#642)

--- site/src/theme/components/navigation/mui-tabs-theme-options.ts
+++ site/src/theme/components/navigation/mui-tabs-theme-options.ts
@@ -1,10 +1,6 @@
 import { Components } from "@mui/material";
 
 export const MuiTabsThemeOptions: Components["MuiTabs"] = {
-  defaultProps: {
-    disableRipple: true,
-    disableTouchRipple: true,
-  },
   styleOverrides: {
     indicator: {
       height: 5,
