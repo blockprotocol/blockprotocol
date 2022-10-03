f10e3fd1350c8f6a56b899b2b74c8dc3beca4495
Alexander Kachkaev <alexander@kachkaev.ru>
2022-10-03 17:09:40 +0100

    Fix test contents

--- site/tests/e2e/api.test.ts
+++ site/tests/e2e/api.test.ts
@@ -1,8 +1,9 @@
 import { expect, test } from "playwright-test-coverage";
 
-test("/api/me should respond with HTTP 401", async ({ request }) => {
+test("/api/me should be healthy", async ({ request }) => {
   // This test helps check DB connection. If the endpoint returns 500,
   // either DB is down or environment variables are misconfigured.
   const response = await request.get(`/api/me`);
-  expect(response.status()).toBe(401);
+  expect(response.status()).toBe(200);
+  expect(await response.json()).toStrictEqual({ guest: true });
 });
