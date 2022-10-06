328d592da88f4ba33eed8c0b573ddb8cc4cf4e23
Alexander Kachkaev <alexander@kachkaev.ru>
2022-10-06 23:13:18 +0100

    Another coverage hack

---
+++ apps/test-coverage-hack.js
@@ -0,0 +1,4 @@
+// This file was created in https://github.com/blockprotocol/blockprotocol/pull/609
+// to fix path resolution in nyc reports. Without this file, paths are reported as
+// site/x/y/z.js, but with this file, they need to be apps/site/x/y/z.js.
+// We can remove this file after adding more apps or investigating the root cause.
