08c14534e4ad74c41196c2910557d50791f4423b
Alexander Kachkaev <alexander@kachkaev.ru>
2022-10-18 19:02:09 +0100

    Configure Sentry Replay

--- apps/site/sentry.client.config.ts
+++ apps/site/sentry.client.config.ts
@@ -2,6 +2,7 @@
 
 import * as Sentry from "@sentry/nextjs";
 import type { NextjsOptions } from "@sentry/nextjs/types/utils/nextjsOptions";
+import { Replay } from "@sentry/replay";
 
 const dsn = process.env.NEXT_PUBLIC_SENTRY_DSN;
 
@@ -9,6 +10,15 @@ export const sentryConfig: NextjsOptions = {
   dsn,
   enabled: !!dsn,
   environment: process.env.NEXT_PUBLIC_VERCEL_ENV ?? "unset",
+  integrations: [
+    new Replay({
+      captureOnlyOnError: true,
+      // @todo Introduce sampling in production after initial testing
+      // replaysSamplingRate:
+      //   process.env.NEXT_PUBLIC_VERCEL_ENV === "production" ? 0.1 : 1,
+      stickySession: true,
+    }),
+  ],
 };
 
 Sentry.init(sentryConfig);
