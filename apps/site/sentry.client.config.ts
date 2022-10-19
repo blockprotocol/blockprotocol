8522d6edd0bc3bda9999f6d501b8fb04fa5bf911
Alexander Kachkaev <alexander@kachkaev.ru>
2022-10-19 21:21:30 +0100

    Reduce diff

--- apps/site/sentry.client.config.ts
+++ apps/site/sentry.client.config.ts
@@ -1,1 +1,24 @@
-import "./sentry.server.config";
+// Context: https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+import { Replay } from "@sentry/replay";
+
+const dsn = process.env.NEXT_PUBLIC_SENTRY_DSN;
+
+Sentry.init({
+  dsn,
+  enabled: !!dsn,
+  environment: process.env.NEXT_PUBLIC_VERCEL_ENV ?? "unset",
+  integrations:
+    typeof window === "undefined"
+      ? []
+      : [
+          new Replay({
+            captureOnlyOnError: true,
+            // @todo Introduce sampling in production after initial testing
+            // replaysSamplingRate:
+            //   process.env.NEXT_PUBLIC_VERCEL_ENV === "production" ? 0.1 : 1,
+            stickySession: true,
+          }),
+        ],
+});
--- apps/site/src/lib/api/blocks/get.ts
+++ apps/site/src/lib/api/blocks/get.ts
@@ -6,7 +6,7 @@ import { ExpandedBlockMetadata } from "../../blocks";
 import { getDbBlock, getDbBlocks } from "./db";
 
 const localBlocks = fs.readJsonSync(
-  path.resolve(process.cwd(), "blocks-data.json"),
+  path.resolve("blocks-data.json"),
 ) as ExpandedBlockMetadata[];
 
 export const getAllBlocks = async (): Promise<ExpandedBlockMetadata[]> => {
