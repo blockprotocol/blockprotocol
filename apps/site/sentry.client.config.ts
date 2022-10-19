50dc03cd568663ac8884ef061aece2e7a29cbf5a
Alexander Kachkaev <alexander@kachkaev.ru>
2022-10-19 13:40:00 +0100

    Simplify config

--- apps/site/sentry.client.config.ts
+++ apps/site/sentry.client.config.ts
@@ -3,19 +3,22 @@
 import * as Sentry from "@sentry/nextjs";
 import { Replay } from "@sentry/replay";
 
-import { sharedSentryConfig } from "./src/lib/shared-sentry-config";
+const dsn = process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 Sentry.init({
-  ...sharedSentryConfig,
-  integrations: [
-    // @ts-expect-error -- refactor after upgrading to TypeScript 4.3, use `satisfies` in sharedSentryConfig
-    ...sharedSentryConfig.integrations,
-    new Replay({
-      captureOnlyOnError: true,
-      // @todo Introduce sampling in production after initial testing
-      // replaysSamplingRate:
-      //   process.env.NEXT_PUBLIC_VERCEL_ENV === "production" ? 0.1 : 1,
-      stickySession: true,
-    }),
-  ],
+  dsn,
+  enabled: !!dsn,
+  environment: process.env.NEXT_PUBLIC_VERCEL_ENV ?? "unset",
+  integrations:
+    typeof window === "undefined"
+      ? []
+      : [
+          new Replay({
+            captureOnlyOnError: true,
+            // @todo Introduce sampling in production after initial testing
+            // replaysSamplingRate:
+            //   process.env.NEXT_PUBLIC_VERCEL_ENV === "production" ? 0.1 : 1,
+            stickySession: true,
+          }),
+        ],
 });
--- apps/site/sentry.server.config.ts
+++ apps/site/sentry.server.config.ts
@@ -1,5 +1,1 @@
-import * as Sentry from "@sentry/nextjs";
-
-import { sharedSentryConfig } from "./src/lib/shared-sentry-config";
-
-Sentry.init(sharedSentryConfig);
+import "./sentry.client.config";
