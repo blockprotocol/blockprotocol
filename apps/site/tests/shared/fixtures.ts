642d6448e79b3bc5080f94453fc296e2ae2f7a47
Alexander Kachkaev <alexander@kachkaev.ru>
2022-10-24 18:22:27 +0100

    Fix tests?

--- apps/site/tests/shared/fixtures.ts
+++ apps/site/tests/shared/fixtures.ts
@@ -1,5 +1,6 @@
-import { execa } from "execa";
-
 export const resetDb = async () => {
+  // @todo Replace with static import after upgrading tests to ESM
+  const { execa } = await import("execa");
+
   await execa("yarn", ["exe", "scripts/seed-db.ts"]);
 };
