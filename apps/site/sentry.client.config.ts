4952c3219a5875b8fcdae7a7c81bd9b17d080074
Alexander Kachkaev <alexander@kachkaev.ru>
2022-10-26 16:56:41 +0100

    Improve `@sentry/replay` setup (#698)

--- apps/site/sentry.client.config.ts
+++ apps/site/sentry.client.config.ts
@@ -5,26 +5,19 @@ import { Replay } from "@sentry/replay";
 
 const dsn = process.env.NEXT_PUBLIC_SENTRY_DSN;
 
-const replaysSamplingRate = Number.parseInt(
-  process.env.NEXT_PUBLIC_SENTRY_REPLAYS_SAMPLING_RATE || "0",
-  10,
-);
-
 Sentry.init({
   dsn,
   enabled: !!dsn,
   environment: process.env.NEXT_PUBLIC_VERCEL_ENV ?? "unset",
-  integrations:
-    replaysSamplingRate > 0 &&
-    replaysSamplingRate <= 1 &&
-    // @todo Remove when https://github.com/getsentry/sentry-replay/issues/246#issuecomment-1284472286 is resolved
-    typeof window !== "undefined"
-      ? [
-          new Replay({
-            captureOnlyOnError: true,
-            replaysSamplingRate,
-            stickySession: true,
-          }),
-        ]
-      : [],
+  integrations: process.env.NEXT_PUBLIC_SENTRY_REPLAYS_SAMPLING_RATE
+    ? [
+        new Replay({
+          captureOnlyOnError: true,
+          replaysSamplingRate: parseFloat(
+            process.env.NEXT_PUBLIC_SENTRY_REPLAYS_SAMPLING_RATE,
+          ),
+          stickySession: true,
+        }),
+      ]
+    : [],
 });
