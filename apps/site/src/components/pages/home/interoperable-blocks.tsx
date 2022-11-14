d77d87cbd230c9171e9ecfdc29a88b53a178f89a
Alexander Kachkaev <alexander@kachkaev.ru>
2022-11-14 11:17:50 +0000

    Update Next to v13 (#692)

--- apps/site/src/components/pages/home/interoperable-blocks.tsx
+++ apps/site/src/components/pages/home/interoperable-blocks.tsx
@@ -1,5 +1,5 @@
 import { Box, Typography, useMediaQuery, useTheme } from "@mui/material";
-import Image from "next/image";
+import Image from "next/legacy/image";
 
 import transparentBlocksWithCircleImage from "../../../../public/assets/new-home/transparent-blocks-with-circle-min.png";
 
