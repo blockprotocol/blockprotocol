a722b8512520288d1630c1a48685a55a6bac2dc6
Alexander Kachkaev <alexander@kachkaev.ru>
2022-11-29 09:56:20 +0000

    Update Sentry (#796)

--- apps/site/sentry.client.config.ts
+++ apps/site/sentry.client.config.ts
@@ -9,12 +9,12 @@ Sentry.init({
   dsn,
   enabled: !!dsn,
   environment: process.env.NEXT_PUBLIC_VERCEL_ENV ?? "unset",
-  integrations: process.env.NEXT_PUBLIC_SENTRY_REPLAYS_SAMPLING_RATE
+  integrations: process.env.NEXT_PUBLIC_SENTRY_REPLAY_SESSION_SAMPLE_RATE
     ? [
         new Replay({
-          captureOnlyOnError: true,
-          replaysSamplingRate: parseFloat(
-            process.env.NEXT_PUBLIC_SENTRY_REPLAYS_SAMPLING_RATE,
+          errorSampleRate: 1,
+          sessionSampleRate: parseFloat(
+            process.env.NEXT_PUBLIC_SENTRY_REPLAY_SESSION_SAMPLE_RATE,
           ),
           stickySession: true,
         }),
