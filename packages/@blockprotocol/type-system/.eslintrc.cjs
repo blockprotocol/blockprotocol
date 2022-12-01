e82093213bbbf9ca61168f31a9e174ec7ce8ecbe
Alfred Mountfield <alfie.mountfield@gmail.com>
2022-12-01 11:25:54 +0000

    Fix eslint running

--- packages/@blockprotocol/type-system/.eslintrc.cjs
+++ packages/@blockprotocol/type-system/.eslintrc.cjs
@@ -1,5 +1,4 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   ...require("@local/eslint-config/generate-workspace-config.cjs")(__dirname),
-  ignorePatterns: ["src/wasm/*", "dist/*"]
 };
