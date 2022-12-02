cd85a39a2a9ccef07ac93cf0c7e5f98b00a7094f
Alfred Mountfield <alfie.mountfield@gmail.com>
2022-12-02 15:31:58 +0000

    Improve error reasoning

--- packages/@blockprotocol/type-system/src/index.ts
+++ packages/@blockprotocol/type-system/src/index.ts
@@ -2,8 +2,7 @@ import wasm from "../wasm/type-system_bg.wasm";
 import { setWasmInit } from "./common";
 
 export * from "../wasm/type-system";
-export { setWasmInit, TypeSystemInitializer } from "./common";
+export { TypeSystemInitializer } from "./common";
 
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-expect-error
+// @ts-expect-error -- The cause of this error is unknown, perhaps growing pains for the WASM ecosystem, or we need to do some custom TS declaration
 setWasmInit(() => wasm());
