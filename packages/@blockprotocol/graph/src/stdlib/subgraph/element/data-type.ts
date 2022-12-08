7ea064896140fe7bfa97e1a3a06f23277001e3e2
Alfred Mountfield <alfie.mountfield@gmail.com>
2022-12-08 16:44:24 +0000

    Add `data-type` stdlib functions

---
+++ packages/@blockprotocol/graph/src/stdlib/subgraph/element/data-type.ts
@@ -0,0 +1,106 @@
+import {
+  BaseUri,
+  extractBaseUri,
+  extractVersion,
+  VersionedUri,
+} from "@blockprotocol/type-system";
+
+import { OntologyTypeEditionId } from "../../../types/ontology";
+import { DataTypeWithMetadata } from "../../../types/ontology/data-type";
+import { Subgraph } from "../../../types/subgraph";
+import { isDataTypeVertex } from "../../../types/subgraph/vertices";
+
+/**
+ * Returns all `DataTypeWithMetadata`s within the vertices of the subgraph
+ *
+ * @param subgraph
+ */
+export const getDataTypes = (subgraph: Subgraph): DataTypeWithMetadata[] => {
+  return Object.values(
+    Object.values(subgraph.vertices).flatMap((versionObject) =>
+      Object.values(versionObject)
+        .filter(isDataTypeVertex)
+        .map((vertex) => vertex.inner),
+    ),
+  );
+};
+
+/**
+ * Gets a `DataTypeWithMetadata` by its `VersionedUri` from within the vertices of the subgraph. Returns `undefined` if
+ * the data type couldn't be found.
+ *
+ * @param subgraph
+ * @param dataTypeId
+ * @throws if the vertex isn't a `DataTypeVertex`
+ */
+export const getDataTypeById = (
+  subgraph: Subgraph,
+  dataTypeId: VersionedUri,
+): DataTypeWithMetadata | undefined => {
+  const [baseUri, version] = [
+    extractBaseUri(dataTypeId),
+    extractVersion(dataTypeId),
+  ];
+  const vertex = subgraph.vertices[baseUri]?.[version];
+
+  if (!vertex) {
+    return undefined;
+  }
+
+  if (!isDataTypeVertex(vertex)) {
+    throw new Error(`expected data type vertex but got: ${vertex.kind}`);
+  }
+
+  return vertex.inner;
+};
+
+/**
+ * Gets a `DataTypeWithMetadata` by its `OntologyTypeEditionId` from within the vertices of the subgraph. Returns
+ * `undefined` if the data type couldn't be found.
+ *
+ * @param subgraph
+ * @param editionId
+ * @throws if the vertex isn't a `DataTypeVertex`
+ */
+export const getDataTypeByEditionId = (
+  subgraph: Subgraph,
+  editionId: OntologyTypeEditionId,
+): DataTypeWithMetadata | undefined => {
+  const vertex = subgraph.vertices[editionId.baseId]?.[editionId.versionId];
+
+  if (!vertex) {
+    return undefined;
+  }
+
+  if (!isDataTypeVertex(vertex)) {
+    throw new Error(`expected data type vertex but got: ${vertex.kind}`);
+  }
+
+  return vertex.inner;
+};
+
+/**
+ * Returns all `DataTypeWithMetadata`s within the vertices of the subgraph that match a given `BaseUri`
+ *
+ * @param subgraph
+ * @param baseUri
+ */
+export const getDataTypesByBaseUri = (
+  subgraph: Subgraph,
+  baseUri: BaseUri,
+): DataTypeWithMetadata[] => {
+  const versionObject = subgraph.vertices[baseUri];
+
+  if (!versionObject) {
+    return [];
+  }
+  const dataTypeVertices = Object.values(versionObject);
+
+  return dataTypeVertices.map((vertex) => {
+    if (!isDataTypeVertex(vertex)) {
+      throw new Error(`expected data type vertex but got: ${vertex.kind}`);
+    }
+
+    return vertex.inner;
+  });
+};
