78ff0f05b71d4200595ae4b8e06617b881af72c7
Alfred Mountfield <alfie.mountfield@gmail.com>
2022-12-08 10:53:38 +0000

    Add `OntologyTypeEditionId`

--- packages/@blockprotocol/graph/src/types/ontology.ts
+++ packages/@blockprotocol/graph/src/types/ontology.ts
@@ -1,1 +1,14 @@
+import { BaseUri } from "@blockprotocol/type-system/dist/cjs-slim/index-slim";
+
 export * from "./ontology/entity-type";
+/** @todo - Add documentation */
+/**
+ *  @todo - Do we want to introduce "ontology" into code? Alternatives:
+ *    * `TypeEditionId` - "type" is so ambiguous in languages and tends to clash with protected keywords
+ *    * `TypeSystemElementId` - This is about as wordy as below, and is an element of the ontology the same as an element
+ *      of the type system? Not sure the type system == ontology, it's more like the type system describes the ontology.
+ */
+export type OntologyTypeEditionId = {
+  baseId: BaseUri;
+  versionId: number;
+};
