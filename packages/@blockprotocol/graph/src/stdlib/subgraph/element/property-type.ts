3209409b9104cef7876e1697a4c57dd654785461
Alfred Mountfield <alfie.mountfield@gmail.com>
2022-12-08 16:45:26 +0000

    Add `property-type` stdlib functions

---
+++ packages/@blockprotocol/graph/src/stdlib/subgraph/element/property-type.ts
@@ -0,0 +1,108 @@
+import {
+  BaseUri,
+  extractBaseUri,
+  extractVersion,
+  VersionedUri,
+} from "@blockprotocol/type-system";
+
+import { OntologyTypeEditionId } from "../../../types/ontology";
+import { PropertyTypeWithMetadata } from "../../../types/ontology/property-type";
+import { Subgraph } from "../../../types/subgraph";
+import { isPropertyTypeVertex } from "../../../types/subgraph/vertices";
+
+/**
+ * Returns all `PropertyTypeWithMetadata`s within the vertices of the subgraph
+ *
+ * @param subgraph
+ */
+export const getPropertyTypes = (
+  subgraph: Subgraph,
+): PropertyTypeWithMetadata[] => {
+  return Object.values(
+    Object.values(subgraph.vertices).flatMap((versionObject) =>
+      Object.values(versionObject)
+        .filter(isPropertyTypeVertex)
+        .map((vertex) => vertex.inner),
+    ),
+  );
+};
+
+/**
+ * Gets a `PropertyTypeWithMetadata` by its `VersionedUri` from within the vertices of the subgraph. Returns `undefined`
+ * if the property type couldn't be found.
+ *
+ * @param subgraph
+ * @param propertyTypeId
+ * @throws if the vertex isn't a `PropertyTypeVertex`
+ */
+export const getPropertyTypeById = (
+  subgraph: Subgraph,
+  propertyTypeId: VersionedUri,
+): PropertyTypeWithMetadata | undefined => {
+  const [baseUri, version] = [
+    extractBaseUri(propertyTypeId),
+    extractVersion(propertyTypeId),
+  ];
+  const vertex = subgraph.vertices[baseUri]?.[version];
+
+  if (!vertex) {
+    return undefined;
+  }
+
+  if (!isPropertyTypeVertex(vertex)) {
+    throw new Error(`expected property type vertex but got: ${vertex.kind}`);
+  }
+
+  return vertex.inner;
+};
+
+/**
+ * Gets a `PropertyTypeWithMetadata` by its `OntologyTypeEditionId` from within the vertices of the subgraph. Returns
+ * `undefined` if the property type couldn't be found.
+ *
+ * @param subgraph
+ * @param editionId
+ * @throws if the vertex isn't a `PropertyTypeVertex`
+ */
+export const getPropertyTypeByEditionId = (
+  subgraph: Subgraph,
+  editionId: OntologyTypeEditionId,
+): PropertyTypeWithMetadata | undefined => {
+  const vertex = subgraph.vertices[editionId.baseId]?.[editionId.versionId];
+
+  if (!vertex) {
+    return undefined;
+  }
+
+  if (!isPropertyTypeVertex(vertex)) {
+    throw new Error(`expected property type vertex but got: ${vertex.kind}`);
+  }
+
+  return vertex.inner;
+};
+
+/**
+ * Returns all `PropertyTypeWithMetadata`s within the vertices of the subgraph that match a given `BaseUri`
+ *
+ * @param subgraph
+ * @param baseUri
+ */
+export const getPropertyTypesByBaseUri = (
+  subgraph: Subgraph,
+  baseUri: BaseUri,
+): PropertyTypeWithMetadata[] => {
+  const versionObject = subgraph.vertices[baseUri];
+
+  if (!versionObject) {
+    return [];
+  }
+  const propertyTypeVertices = Object.values(versionObject);
+
+  return propertyTypeVertices.map((vertex) => {
+    if (!isPropertyTypeVertex(vertex)) {
+      throw new Error(`expected property type vertex but got: ${vertex.kind}`);
+    }
+
+    return vertex.inner;
+  });
+};
