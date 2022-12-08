29de9ae36d74ec736d24884a5d24e1225483165c
Alfred Mountfield <alfie.mountfield@gmail.com>
2022-12-08 12:30:06 +0000

    Add `DataTypeWithMetadata` and `PropertyTypeWithMetadata`

---
+++ packages/@blockprotocol/graph/src/types/ontology/property-type.ts
@@ -0,0 +1,14 @@
+import { PropertyType } from "@blockprotocol/type-system/slim";
+
+import { OntologyElementMetadata } from "./metadata";
+
+/**
+ * @todo - Should we re-export this? Should the type-system package be an implementation detail of the graph service?
+ *   Or should consumers import it directly? Also raises the question of if we should be re-exporting the functions.
+ */
+export type { PropertyType };
+
+export type PropertyTypeWithMetadata = {
+  schema: PropertyType;
+  metadata: OntologyElementMetadata;
+};
