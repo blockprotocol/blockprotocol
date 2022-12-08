0355a972045fbd145a78e3c245406578d53a9fc6
Alfred Mountfield <alfie.mountfield@gmail.com>
2022-12-08 17:08:03 +0000

    Update `subgraph` generic parameters

--- packages/@blockprotocol/graph/src/types/subgraph.ts
+++ packages/@blockprotocol/graph/src/types/subgraph.ts
@@ -1,11 +1,21 @@
 import { Entity, EntityEditionId } from "./entity";
 import { OntologyTypeEditionId } from "./ontology";
+import { DataTypeWithMetadata } from "./ontology/data-type";
 import { EntityTypeWithMetadata } from "./ontology/entity-type";
+import { PropertyTypeWithMetadata } from "./ontology/property-type";
 import { Edges } from "./subgraph/edges";
 import { GraphResolveDepths } from "./subgraph/graph-resolve-depths";
 import { Vertices } from "./subgraph/vertices";
 
 export type SubgraphRootTypes = {
+  dataType: {
+    editionId: OntologyTypeEditionId;
+    element: DataTypeWithMetadata;
+  };
+  propertyType: {
+    editionId: OntologyTypeEditionId;
+    element: PropertyTypeWithMetadata;
+  };
   entityType: {
     editionId: OntologyTypeEditionId;
     element: EntityTypeWithMetadata;
