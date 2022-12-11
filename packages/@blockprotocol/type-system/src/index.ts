b770aaa67a0dbe15ef4ffa3d13a0ca7bd2841c22
Alfred Mountfield <alfie.mountfield@gmail.com>
2022-12-11 01:44:12 +0000

    Reimplement some of the Type System package in TypeScript

--- packages/@blockprotocol/type-system/src/index.ts
+++ packages/@blockprotocol/type-system/src/index.ts
@@ -3,6 +3,7 @@ import { setWasmInit } from "./common";
 
 export * from "../wasm/type-system";
 export { TypeSystemInitializer } from "./common";
+export * from "./native";
 
 // @ts-expect-error -- The cause of this error is unknown, perhaps growing pains for the WASM ecosystem, or we need to do some custom TS declaration
 setWasmInit(() => wasm());
