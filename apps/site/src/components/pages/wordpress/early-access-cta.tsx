441a777ca3ef477adae9a1d1390aa11cc83cd53b
Luís Bettencourt <lmn.bettencourt@gmail.com>
2022-12-16 13:07:26 +0000

    Revert "remove input maxWidth"
    This reverts commit 631e6a90022bd6c8e6652d3e7d651faab9a52ad9.

--- apps/site/src/components/pages/wordpress/early-access-cta.tsx
+++ apps/site/src/components/pages/wordpress/early-access-cta.tsx
@@ -76,6 +76,7 @@ export const EarlyAccessCTA = () => {
       disabled={loading || !!submittedEmail}
       sx={{
         marginBottom: 2,
+        maxWidth: 480,
       }}
       required
       type="email"
