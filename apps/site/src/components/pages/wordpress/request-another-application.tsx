fb1ca391d8827ea5ea88fc6ff979a0de020f8f8f
David Wilkinson <6226576+nonparibus@users.noreply.github.com>
2022-12-16 19:50:33 +0000

    Tweak link to cache-bust Twitter Open Graph

--- apps/site/src/components/pages/wordpress/request-another-application.tsx
+++ apps/site/src/components/pages/wordpress/request-another-application.tsx
@@ -129,7 +129,7 @@ const otherApplication = {
   name: "Other",
   icon: <CommentDotsIcon />,
   twitterHref:
-    "https://twitter.com/intent/tweet?text=The%20%40BlockProtocol%20is%20an%20open%20standard%20for%20developing%20blocks%20which%20work%20across%20multiple%20apps%20(including%20WordPress).%20What%20app%20would%20you%20like%20to%20see%20support%20in%20next%3F%20https%3A%2F%2Fblockprotocol.org%2Fwordpress",
+    "https://twitter.com/intent/tweet?text=The%20%40BlockProtocol%20is%20an%20open%20standard%20for%20developing%20blocks%20which%20work%20across%20multiple%20apps%20(including%20WordPress).%20What%20app%20would%20you%20like%20to%20see%20support%20in%20next%3F%20blockprotocol.org%2Fwordpress",
 };
 
 export const applications: Application[] = [
@@ -138,28 +138,28 @@ export const applications: Application[] = [
     name: "Sanity",
     icon: <SanityIcon />,
     twitterHref:
-      "https://twitter.com/intent/tweet?text=The%20%40BlockProtocol%20is%20an%20open%20standard%20for%20building%20blocks%20which%20work%20across%20multiple%20apps%20(including%20WordPress).%20I%27ve%20voted%20for%20%40sanity_io%20support%20next%20at%20https%3A%2F%2Fblockprotocol.org%2Fwordpress",
+      "https://twitter.com/intent/tweet?text=The%20%40BlockProtocol%20is%20an%20open%20standard%20for%20building%20blocks%20which%20work%20across%20multiple%20apps%20(including%20WordPress).%20I%27ve%20voted%20for%20%40sanity_io%20support%20next%20at%20blockprotocol.org%2Fwordpress",
   },
   {
     id: ApplicationIds.Strapi,
     name: "Strapi",
     icon: <StrapiIcon />,
     twitterHref:
-      "https://twitter.com/intent/tweet?text=The%20%40BlockProtocol%20is%20an%20open%20standard%20for%20building%20blocks%20which%20work%20across%20multiple%20apps%20(including%20WordPress).%20I%27ve%20voted%20for%20%40strapijs%20support%20next%20at%20https%3A%2F%2Fblockprotocol.org%2Fwordpress",
+      "https://twitter.com/intent/tweet?text=The%20%40BlockProtocol%20is%20an%20open%20standard%20for%20building%20blocks%20which%20work%20across%20multiple%20apps%20(including%20WordPress).%20I%27ve%20voted%20for%20%40strapijs%20support%20next%20at%20blockprotocol.org%2Fwordpress",
   },
   {
     id: ApplicationIds.Contentful,
     name: "Contentful",
     icon: <ContentfulIcon />,
     twitterHref:
-      "https://twitter.com/intent/tweet?text=The%20%40BlockProtocol%20is%20an%20open%20standard%20for%20building%20blocks%20which%20work%20across%20multiple%20apps%20(including%20WordPress).%20I%27ve%20voted%20for%20%40contentful%20support%20next%20at%20https%3A%2F%2Fblockprotocol.org%2Fwordpress",
+      "https://twitter.com/intent/tweet?text=The%20%40BlockProtocol%20is%20an%20open%20standard%20for%20building%20blocks%20which%20work%20across%20multiple%20apps%20(including%20WordPress).%20I%27ve%20voted%20for%20%40contentful%20support%20next%20at%20blockprotocol.org%2Fwordpress",
   },
   {
     id: ApplicationIds.Github,
     name: "GitHub",
     icon: <GithubIcon />,
     twitterHref:
-      "https://twitter.com/intent/tweet?text=The%20%40BlockProtocol%20is%20an%20open%20standard%20for%20building%20blocks%20which%20work%20across%20multiple%20apps%20(including%20WordPress).%20Join%20me%20in%20voting%20for%20%40GitHubNext%20support%20at%20https%3A%2F%2Fblockprotocol.org%2Fwordpress",
+      "https://twitter.com/intent/tweet?text=The%20%40BlockProtocol%20is%20an%20open%20standard%20for%20building%20blocks%20which%20work%20across%20multiple%20apps%20(including%20WordPress).%20Join%20me%20in%20voting%20for%20%40GitHubNext%20support%20at%20blockprotocol.org%2Fwordpress",
   },
   otherApplication,
 ];
