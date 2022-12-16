62a2c362af915e8d7184312e8c11265cac089c51
Luís Bettencourt <lmn.bettencourt@gmail.com>
2022-12-16 12:51:13 +0000

    seo social card

--- apps/site/src/pages/wordpress.page.tsx
+++ apps/site/src/pages/wordpress.page.tsx
@@ -1,3 +1,4 @@
+import { NextSeo } from "next-seo";
 import { FunctionComponent } from "react";
 
 import { Availability } from "../components/pages/wordpress/availability";
@@ -7,12 +8,30 @@ import { WhyDevelop } from "../components/pages/wordpress/why-develop";
 import { WhyInstall } from "../components/pages/wordpress/why-install";
 
 const WordPressPage: FunctionComponent = () => (
-  <EmailSubmittedProvider>
-    <Header />
-    <WhyInstall />
-    <WhyDevelop />
-    <Availability />
-  </EmailSubmittedProvider>
+  <>
+    <NextSeo
+      title="Block Protocol WordPress Plugin"
+      openGraph={{
+        title: "Þ plugin for WordPress",
+        description:
+          "The Block Protocol is a standard for building web blocks that work across applications. Use them in WordPress, or vote for another app",
+        images: [
+          { url: "https://blockprotocol.org/assets/wp_bp_og_cover.png" },
+        ],
+        site_name: "Block Protocol",
+        type: "website",
+        url: "https://blockprotocol.org/wordpress",
+      }}
+    />
+    <meta title="og:social" content="public.jpg" />
+
+    <EmailSubmittedProvider>
+      <Header />
+      <WhyInstall />
+      <WhyDevelop />
+      <Availability />
+    </EmailSubmittedProvider>
+  </>
 );
 
 export default WordPressPage;
