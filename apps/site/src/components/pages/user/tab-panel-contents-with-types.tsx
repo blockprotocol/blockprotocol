2203e7742938902a44322ab85e7b0ae21bbb7632
David @ HASH <6226576+nonparibus@users.noreply.github.com>
2022-12-29 01:48:39 +0000

    Update and rename tab-panel-contents-with-schemas.tsx to tab-panel-contents-with-types.tsx

--- apps/site/src/components/pages/user/tab-panel-contents-with-types.tsx
+++ apps/site/src/components/pages/user/tab-panel-contents-with-types.tsx
@@ -5,26 +5,26 @@ import { EntityType } from "../../../lib/api/model/entity-type.model";
 import { SerializedUser } from "../../../lib/api/model/user.model";
 import { formatUpdatedAt } from "../../../util/html-utils";
 import { Button } from "../../button";
-import { CreateSchemaModal } from "../../modal/create-schema-modal";
+import { CreateTypeModal } from "../../modal/create-type-modal";
 import { ListViewCard } from "./list-view-card";
 import { Placeholder } from "./placeholder";
-import { BrowseHubButton, CreateSchemaButton } from "./placeholder-buttons";
+import { BrowseHubButton, CreateTypeButton } from "./placeholder-buttons";
 import { useUserStatus } from "./use-user-status";
 
-export interface TabPanelContentsWithSchemasProps {
+export interface TabPanelContentsWithTypesProps {
   entityTypes: EntityType[];
   user: SerializedUser;
 }
 
-export const TabPanelContentsWithSchemas: FunctionComponent<
-  TabPanelContentsWithSchemasProps
+export const TabPanelContentsWithTypes: FunctionComponent<
+  TabPanelContentsWithTypesProps
 > = ({ user, entityTypes }) => {
-  const [schemaModalOpen, setSchemaModalOpen] = useState(false);
+  const [typeModalOpen, setTypeModalOpen] = useState(false);
   const userStatus = useUserStatus(user);
-  const createSchemaModal = (
-    <CreateSchemaModal
-      open={schemaModalOpen}
-      onClose={() => setSchemaModalOpen(false)}
+  const createTypeModal = (
+    <CreateTypeModal
+      open={typeModalOpen}
+      onClose={() => setTypeModalOpen(false)}
     />
   );
 
@@ -36,18 +36,18 @@ export const TabPanelContentsWithSchemas: FunctionComponent<
     return userStatus === "current" ? (
       <>
         <Placeholder
-          header="You haven’t created any schemas yet"
+          header="You haven’t created any types yet"
           tip="Start building to see your creations show up here."
           actions={
-            <CreateSchemaButton onClick={() => setSchemaModalOpen(true)} />
+            <CreateTypeButton onClick={() => setTypeModalOpen(true)} />
           }
         />
-        {createSchemaModal}
+        {createTypeModal}
       </>
     ) : (
       <Placeholder
-        header={`@${user.shortname} hasn’t published any schemas yet`}
-        tip="You can browse existing schemas on the Hub."
+        header={`@${user.shortname} hasn’t published any types yet`}
+        tip="You can browse existing types on the Hub."
         actions={<BrowseHubButton />}
       />
     );
@@ -62,21 +62,21 @@ export const TabPanelContentsWithSchemas: FunctionComponent<
             justifyContent: { xs: "flex-start", md: "flex-end" },
           }}
         >
-          <Button squared size="small" onClick={() => setSchemaModalOpen(true)}>
-            Create New Schema
+          <Button squared size="small" onClick={() => setTypeModalOpen(true)}>
+            Create New Type
           </Button>
         </Box>
       )}
-      {entityTypes.map(({ entityTypeId, schema, updatedAt }) => (
+      {entityTypes.map(({ entityTypeId, type, updatedAt }) => (
         <ListViewCard
           key={entityTypeId}
-          title={schema.title}
-          description={schema.description as string}
+          title={type.title}
+          description={type.description as string}
           extraContent={formatUpdatedAt(updatedAt as unknown as string)}
-          url={schema.$id}
+          url={type.$id}
         />
       ))}
-      {createSchemaModal}
+      {createTypeModal}
     </>
   );
 };
