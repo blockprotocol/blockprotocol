ff4c584a508362d30c5252042cce3fee88859eb6
Luís Bettencourt <lmn.bettencourt@gmail.com>
2023-01-09 21:54:07 +0000

    feedback

--- apps/site/src/pages/api/vote-application.api.ts
+++ apps/site/src/pages/api/vote-application.api.ts
@@ -1,10 +1,41 @@
+import Ajv from "ajv";
 import { createBaseHandler } from "../../lib/api/handler/base-handler";
 import { getMember, subscribeToMailchimp } from "../../lib/api/mailchimp";
 
 export type VoteApplicationRequestBody = {
   email: string;
-  merge_fields?: { [key: string]: any };
+  merge_fields: {
+    ECO_SANITY?: string;
+    ECO_STRAPI?: string;
+    ECO_CONFUL?: string;
+    ECO_GITHUB?: string;
+    ECO_OTHER?: string;
+    WISH_EA?: string;
+  };
 };
+
+const ajv = new Ajv();
+
+const validate = ajv.compile<VoteApplicationRequestBody>({
+  type: "object",
+  properties: {
+    email: { type: "string" },
+    merge_fields: {
+      type: "object",
+      properties: {
+        ECO_SANITY: { type: "string" },
+        ECO_STRAPI: { type: "string" },
+        ECO_CONFUL: { type: "string" },
+        ECO_GITHUB: { type: "string" },
+        ECO_OTHER: { type: "string" },
+        WISH_EA: { type: "string" },
+      },
+      additionalProperties: false,
+    },
+  },
+  required: ["email"],
+  additionalProperties: false,
+});
 
 export type VoteApplicationResponse = {
   success?: boolean;
@@ -15,23 +46,21 @@ export default createBaseHandler<
   VoteApplicationRequestBody,
   VoteApplicationResponse
 >().put(async (req, res) => {
-  try {
-    const member = await getMember({ email: req.body.email });
+  const member = await getMember({ email: req.body.email });
 
+  try {
     const payload = {
-      email: req.body.email,
-      merge_fields: {
-        ECO_SANITY: req.body.merge_fields?.ECO_SANITY,
-        ECO_STRAPI: req.body.merge_fields?.ECO_STRAPI,
-        ECO_CONFUL: req.body.merge_fields?.ECO_CONFUL,
-        ECO_GITHUB: req.body.merge_fields?.ECO_GITHUB,
-        ECO_OTHER: req.body.merge_fields?.ECO_OTHER,
-        WISH_EA: req.body.merge_fields?.WISH_EA,
-      },
+      ...req.body,
     };
 
     if (member?.merge_fields?.WISH_EA && payload?.merge_fields?.WISH_EA) {
       payload.merge_fields.WISH_EA = `${member?.merge_fields?.WISH_EA}, ${payload.merge_fields.WISH_EA}`;
+    }
+
+    const valid = validate(payload);
+
+    if (!valid) {
+      throw new Error();
     }
 
     await subscribeToMailchimp(payload);
