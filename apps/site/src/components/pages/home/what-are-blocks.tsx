59ccab14111215165f7e356c09753ba00bd09564
luisbettencourt <lmn.bettencourt@gmail.com>
2023-01-20 13:31:25 +0000

    what are blocks section

--- apps/site/src/components/pages/home/what-are-blocks.tsx
+++ apps/site/src/components/pages/home/what-are-blocks.tsx
@@ -5,8 +5,7 @@ export const WhatAreBlocks = () => {
     <Box
       sx={{
         zIndex: 1,
-        background:
-          "radial-gradient(116.02% 95.04% at 50% 100.79%, #f5f3fa 0%, #FFFFFF 80.54%)",
+        background: ({ palette }) => palette.common.white,
         borderBottom: "1px solid #eceaf1",
       }}
     >
@@ -20,98 +19,145 @@ export const WhatAreBlocks = () => {
           borderRadius: "8px",
           boxShadow:
             "0px 2.8px 2.2px rgba(166, 142, 187, 0.15), 0px 6.7px 5.3px rgba(166, 142, 187, 0.08), 0px 12.5px 10px rgba(166, 142, 187, 0.05), 0px 22.3px 17.9px rgba(166, 142, 187, 0.09), 0px 41.8px 33.4px rgba(166, 142, 187, 0.1), 0px 100px 80px rgba(166, 142, 187, 0.1)",
-          pt: { xs: 1, md: 3 },
-          px: { xs: 2, md: 9 },
-          pb: 10,
-          margin: { xs: "-6rem 1rem 0", lg: "-8rem auto 0" },
+          margin: { xs: "-8rem 1rem 0", lg: "-12rem auto 0" },
+          overflow: "hidden",
         }}
       >
         <Box
           sx={{
-            display: "grid",
-            gridTemplateColumns: { xs: "1fr", md: "1fr 1fr" },
-            alignItems: { xs: "center", md: "start" },
-            justifyContent: { xs: "center", md: "start" },
-            py: "1rem",
-            gridGap: "1rem",
+            background: ({ palette }) => palette.common.white,
+            boxShadow: "inset 0px 2px 5px rgba(0, 0, 0, 0.04)",
+            pt: { xs: 1, md: 3 },
+            px: { xs: 2, md: 9 },
+            pb: 10,
           }}
         >
-          <Box>
-            <Typography
-              variant="bpHeading2"
+          <Box
+            sx={{
+              display: "grid",
+              gridTemplateColumns: { xs: "1fr", md: "1fr 1fr" },
+              justifyContent: { xs: "center", md: "start" },
+              py: "1rem",
+              gridGap: "1rem",
+            }}
+          >
+            <Box
               sx={{
-                lineHeight: 1,
-                color: ({ palette }) => palette.gray[90],
-                margin: "2rem auto 1rem",
-                maxWidth: "40ch",
-                textAlign: { xs: "center", md: "left" },
+                display: "flex",
+                flexDirection: "column",
+                alignItems: { xs: "center", md: "start" },
               }}
             >
-              What are blocks?
-            </Typography>
-            <Typography
-              variant="bpBodyCopy"
+              <Box sx={{ mb: 3 }}>
+                <Typography
+                  variant="bpHeading3"
+                  sx={{
+                    fontWeight: 500,
+                    lineHeight: 1,
+                    color: ({ palette }) => palette.gray[90],
+                    margin: "2rem auto 6px",
+                    maxWidth: "40ch",
+                    textAlign: { xs: "center", md: "left" },
+                  }}
+                >
+                  What are{" "}
+                  <Box
+                    component="span"
+                    sx={{ color: ({ palette }) => palette.purple[800] }}
+                  >
+                    blocks?
+                  </Box>
+                </Typography>
+                <Box
+                  sx={{
+                    width: 74,
+                    height: 3,
+                    background:
+                      "linear-gradient(90deg, rgba(117, 86, 220, 0.8) 0%, rgba(117, 86, 220, 0) 100%)",
+                    borderRadius: 6,
+                  }}
+                />
+              </Box>
+              <Typography
+                variant="bpSmallCopy"
+                sx={{
+                  textAlign: { xs: "center", md: "left" },
+                  margin: "0 auto 0.75rem",
+                  lineHeight: 1.7,
+                  fontWeight: 400,
+                }}
+                maxWidth="52ch"
+              >
+                Blocks are individual front-end components that display data.
+                They allow regular users to edit their contents without needing
+                to write code.
+              </Typography>
+              <Typography
+                variant="bpSmallCopy"
+                sx={{
+                  textAlign: { xs: "center", md: "left" },
+                  margin: "0 auto",
+                  lineHeight: 1.7,
+                  fontWeight: 400,
+                }}
+                maxWidth="52ch"
+              >
+                Blocks can be simple and static, like an image or a text block,
+                or they can be more complex, like a checklist or chart block.
+              </Typography>
+            </Box>
+            <Box
+              component="img"
+              src="/assets/new-home/checklist-block.svg"
               sx={{
-                textAlign: { xs: "center", md: "left" },
-                margin: "0 auto 1rem",
+                borderRadius: "6px",
+                boxShadow:
+                  "0px 1px 0.629px 0px rgba(175, 155, 193, 0.081), 0px 4.237px 8.10px 0px rgba(175, 155, 193, 0.123), 0px 8px 20px 0px rgba(175, 155, 193, 0.083)",
+                margin: { xs: 1, md: "2rem 4rem 2.5rem 4rem" },
+                maxWidth: "500px",
+                alignSelf: "center",
+                justifySelf: "center",
               }}
-              maxWidth="52ch"
-            >
-              Blocks are individual front-end components that display data. They
-              allow regular users to edit their contents without needing to
-              write code.
-            </Typography>
-            <Typography
-              variant="bpBodyCopy"
+            />
+          </Box>
+          <Box
+            sx={{
+              display: "grid",
+              gridTemplateColumns: { xs: "1fr", md: "5.5fr 6.5fr" },
+              alignItems: { xs: "center", md: "start" },
+              justifyContent: { xs: "center", md: "start" },
+              pb: 3,
+              gridGap: "1.5rem",
+            }}
+          >
+            <Box
               sx={{
-                textAlign: { xs: "center", md: "left" },
-                margin: "0 auto",
+                maxWidth: "500px",
+                alignSelf: "center",
+                justifySelf: "center",
               }}
-              maxWidth="52ch"
-            >
-              Blocks can be simple and static, like an image or a text block, or
-              they can be more complex, like a checklist or chart block.
-            </Typography>
+              component="img"
+              src="/assets/new-home/image-block.svg"
+            />
+            <Box
+              component="img"
+              src="/assets/new-home/table-block.svg"
+              sx={{
+                borderRadius: "6px",
+                maxWidth: "600px",
+                alignSelf: "center",
+                justifySelf: "center",
+                boxShadow:
+                  "0px 1px 0.629px 0px rgba(175, 155, 193, 0.081), 0px 4.237px 8.10px 0px rgba(175, 155, 193, 0.123), 0px 8px 20px 0px rgba(175, 155, 193, 0.083)",
+              }}
+            />
           </Box>
           <Box
             component="img"
-            src="/assets/new-home/checklist-block.svg"
+            src="/assets/new-home/kanban-block.svg"
             sx={{
+              width: "100%",
               borderRadius: "6px",
-              boxShadow:
-                "0px 1px 0.629px 0px rgba(175, 155, 193, 0.081), 0px 4.237px 8.10px 0px rgba(175, 155, 193, 0.123), 0px 8px 20px 0px rgba(175, 155, 193, 0.083)",
-              margin: { xs: 1, md: "2rem 4rem 2.5rem 4rem" },
-              maxWidth: "500px",
-              alignSelf: "center",
-              justifySelf: "center",
-            }}
-          />
-        </Box>
-        <Box
-          sx={{
-            display: "grid",
-            gridTemplateColumns: { xs: "1fr", md: "5.5fr 6.5fr" },
-            alignItems: { xs: "center", md: "start" },
-            justifyContent: { xs: "center", md: "start" },
-            pb: 3,
-            gridGap: "1.5rem",
-          }}
-        >
-          <Box
-            sx={{
-              maxWidth: "500px",
-              alignSelf: "center",
-              justifySelf: "center",
-            }}
-            component="img"
-            src="/assets/new-home/image-block.svg"
-          />
-          <Box
-            component="img"
-            src="/assets/new-home/table-block.svg"
-            sx={{
-              borderRadius: "6px",
-              maxWidth: "600px",
               alignSelf: "center",
               justifySelf: "center",
               boxShadow:
@@ -119,18 +165,6 @@ export const WhatAreBlocks = () => {
             }}
           />
         </Box>
-        <Box
-          component="img"
-          src="/assets/new-home/kanban-block.svg"
-          sx={{
-            width: "100%",
-            borderRadius: "6px",
-            alignSelf: "center",
-            justifySelf: "center",
-            boxShadow:
-              "0px 1px 0.629px 0px rgba(175, 155, 193, 0.081), 0px 4.237px 8.10px 0px rgba(175, 155, 193, 0.123), 0px 8px 20px 0px rgba(175, 155, 193, 0.083)",
-          }}
-        />
       </Box>
 
       {/* Second section */}
@@ -154,32 +188,37 @@ export const WhatAreBlocks = () => {
             px: { xs: "1rem", md: "0" },
           }}
         >
+          <Box sx={{ mb: 3 }}>
+            <Typography
+              variant="bpHeading3"
+              sx={{
+                color: ({ palette }) => palette.gray[90],
+                fontWeight: 500,
+                textAlign: { xs: "center", md: "left" },
+                maxWidth: { xs: "20ch", md: "100%" },
+                mb: 0.75,
+              }}
+            >
+              Users typically select blocks from a list
+            </Typography>
+            <Box
+              sx={{
+                width: 74,
+                height: 3,
+                background:
+                  "linear-gradient(90deg, rgba(117, 86, 220, 0.8) 0%, rgba(117, 86, 220, 0) 100%)",
+                borderRadius: 6,
+              }}
+            />
+          </Box>
           <Typography
-            variant="bpHeading2"
+            variant="bpSmallCopy"
             sx={{
-              textAlign: { xs: "center", md: "left" },
-              maxWidth: { xs: "20ch", md: "100%" },
-            }}
-          >
-            Users typically select blocks from a list
-          </Typography>
-          <Box
-            sx={{
-              width: "120px",
-              height: "2px",
-              ml: "0.25rem",
-              my: 3,
-              background:
-                "linear-gradient(to right, rgb(149, 135, 239, 1), rgba(172, 159, 255, 0))",
-            }}
-          />
-          <Typography
-            variant="bpBodyCopy"
-            sx={{
+              fontWeight: 400,
               textAlign: { xs: "center", md: "left" },
               maxWidth: { xs: "45ch", md: "100%" },
             }}
-            mb={{ xs: 3, md: 6 }}
+            mb={{ xs: 3, md: 5 }}
           >
             Users select the type of block they want, then add it to their page,
             dashboard, or canvas. They can add content to the block, edit it, or
