741d2838697539dfd13829ba6769ff37fba4c55e
Ben Werner <ben.werner01@gmail.com>
2023-01-25 16:05:11 +0000

    chore: re-generate internal-api-client

--- apps/site/src/lib/internal-api-client.ts
+++ apps/site/src/lib/internal-api-client.ts
@@ -2,9 +2,23 @@ import {
   Configuration,
   DefaultApi as InternalApiClient,
 } from "@local/internal-api-client";
+import type { Stripe } from "stripe";
 
 const basePath = "/api/internal";
 
 const config = new Configuration({ basePath });
 
 export const internalApi = new InternalApiClient(config, basePath);
+
+declare module "@local/internal-api-client" {
+  interface StripeSubscription extends Stripe.Subscription {}
+  interface StripePrice extends Stripe.Price {}
+}
+
+export const getDateFromStripeDate = (stripeDate: number): Date => {
+  const date = new Date(0);
+
+  date.setUTCSeconds(stripeDate);
+
+  return date;
+};
