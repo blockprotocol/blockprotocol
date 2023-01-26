f718bad77c86dfa12e00b702e3e895af61ccbe77
luisbettencourt <lmn.bettencourt@gmail.com>
2023-01-26 10:54:56 +0000

    registry section responsiveness

--- apps/site/src/components/pages/home/registry-section.tsx
+++ apps/site/src/components/pages/home/registry-section.tsx
@@ -1,5 +1,12 @@
 import SearchIcon from "@mui/icons-material/Search";
-import { Box, Container, Stack, Typography } from "@mui/material";
+import {
+  Box,
+  Container,
+  Stack,
+  Typography,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
 import { FunctionComponent } from "react";
 
 import { ExpandedBlockMetadata as BlockMetadata } from "../../../lib/blocks";
@@ -8,6 +15,59 @@ import { BlockProtocolIcon } from "../../icons";
 import { Link } from "../../link";
 import { LinkButton } from "../../link-button";
 
+const BrowseBlocksSection = () => {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        alignItems: { xs: "center", lg: "flex-end" },
+      }}
+    >
+      <Box mb={1.5}>
+        <LinkButton
+          href="/hub"
+          variant="secondary"
+          sx={{
+            background: "transparent",
+          }}
+          startIcon={
+            <SearchIcon
+              sx={{
+                fontSize: 18,
+              }}
+            />
+          }
+        >
+          Browse all blocks
+        </LinkButton>
+      </Box>
+
+      <Box display="flex">
+        <Typography
+          variant="bpSmallCopy"
+          sx={{
+            textAlign: { xs: "center", lg: "right" },
+            lineHeight: "26px",
+          }}
+        >
+          <strong>Anyone can publish a block.</strong> If you can’t see one you
+          want,{" "}
+          <Link
+            href="/docs/developing-blocks"
+            sx={{
+              color: ({ palette }) => `${palette.purple[700]} !important`,
+            }}
+          >
+            build it!
+          </Link>
+        </Typography>
+      </Box>
+    </Box>
+  );
+};
+
 type RegistrySectionProps = {
   catalog: BlockMetadata[];
 };
@@ -15,6 +75,9 @@ type RegistrySectionProps = {
 export const RegistrySection: FunctionComponent<RegistrySectionProps> = ({
   catalog,
 }) => {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("lg"));
+
   return (
     <Box
       sx={{
@@ -94,59 +157,18 @@ export const RegistrySection: FunctionComponent<RegistrySectionProps> = ({
           </Stack>
         </Box>
 
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "space-between",
-            alignItems: { xs: "center", lg: "flex-end" },
-          }}
-        >
-          <Box mb={1.5}>
-            <LinkButton
-              href="/hub"
-              variant="secondary"
-              sx={{
-                background: "transparent",
-              }}
-              startIcon={
-                <SearchIcon
-                  sx={{
-                    fontSize: 18,
-                  }}
-                />
-              }
-            >
-              Browse all blocks
-            </LinkButton>
-          </Box>
-
-          <Box display="flex">
-            <Typography
-              variant="bpSmallCopy"
-              sx={{
-                textAlign: { xs: "center", lg: "right" },
-                lineHeight: "26px",
-              }}
-            >
-              <strong>Anyone can publish a block.</strong> If you can’t see one
-              you want,{" "}
-              <Link
-                href="/docs/developing-blocks"
-                sx={{
-                  color: ({ palette }) => `${palette.purple[700]} !important`,
-                }}
-              >
-                build it!
-              </Link>
-            </Typography>
-          </Box>
-        </Box>
+        {isMobile ? null : <BrowseBlocksSection />}
       </Container>
 
       <Box sx={{ width: "100%" }}>
         <BlocksSlider catalog={catalog} />
       </Box>
+
+      {isMobile ? (
+        <Box mb={5}>
+          <BrowseBlocksSection />
+        </Box>
+      ) : null}
     </Box>
   );
 };
