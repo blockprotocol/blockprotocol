5e5f0f1c03cdccacfc6f7a3bd059db3ff60b1f6a
Alfred Mountfield <alfie.mountfield@gmail.com>
2023-02-01 12:39:26 +0000

    Reformat typedEntries file

--- libs/@blockprotocol/graph/src/shared.ts
+++ libs/@blockprotocol/graph/src/shared.ts
@@ -5,6 +5,7 @@ type TupleEntry<
 > = T extends readonly [infer Head, ...infer Tail]
   ? TupleEntry<Tail, [...I, unknown], R | [`${I["length"]}`, Head]>
   : R;
+
 type ObjectEntry<T extends {}> = T extends object
   ? { [K in keyof T]: [K, Required<T>[K]] }[keyof T] extends infer E
     ? E extends [infer K, infer V]
@@ -14,7 +15,7 @@ type ObjectEntry<T extends {}> = T extends object
       : never
     : never
   : never;
-// Source: https://dev.to/harry0000/a-bit-convenient-typescript-type-definitions-for-objectentries-d6g
+
 type Entry<T extends {}> = T extends readonly [unknown, ...unknown[]]
   ? TupleEntry<T>
   : T extends ReadonlyArray<infer U>
@@ -22,7 +23,10 @@ type Entry<T extends {}> = T extends readonly [unknown, ...unknown[]]
   : ObjectEntry<T>;
 
 // @todo deduplicate this and packages/mock-block-dock/src/util.ts
-/** `Object.entries` analogue which returns a well-typed array */
+/** `Object.entries` analogue which returns a well-typed array
+ *
+ * Source: https://dev.to/harry0000/a-bit-convenient-typescript-type-definitions-for-objectentries-d6g
+ */
 export function typedEntries<T extends {}>(object: T): ReadonlyArray<Entry<T>> {
   return Object.entries(object) as unknown as ReadonlyArray<Entry<T>>;
 }
