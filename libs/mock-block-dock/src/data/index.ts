9c0b46f06a56213388be631168a2ca2843eba39e
Alfred Mountfield <alfie.mountfield@gmail.com>
2023-02-02 13:11:20 +0000

    Add temporal metadata to mock-data

--- libs/mock-block-dock/src/data/index.ts
+++ libs/mock-block-dock/src/data/index.ts
@@ -1,7 +1,13 @@
 import { MockData } from "../datastore/use-mock-datastore";
-import { entities } from "./entities";
+import { createEntities } from "./entities";
+import { mockDataSubgraphTemporalAxes } from "./temporal-axes";
 
-export const mockData: MockData = {
-  entities,
-  // linkedAggregationDefinitions,
-};
+export const mockData: MockData = (() => {
+  const subgraphTemporalAxes = mockDataSubgraphTemporalAxes();
+
+  return {
+    subgraphTemporalAxes,
+    entities: createEntities(subgraphTemporalAxes.resolved),
+    // linkedAggregationDefinitions,
+  };
+})();
