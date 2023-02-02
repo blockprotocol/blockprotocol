3620a972a330dff0464d2b2b529d6531b3d9a899
Alfred Mountfield <alfie.mountfield@gmail.com>
2023-02-02 13:17:49 +0000

    Add `mustBeDefined` util

--- libs/mock-block-dock/src/util.ts
+++ libs/mock-block-dock/src/util.ts
@@ -13,6 +13,14 @@ import {
   Subgraph,
 } from "@blockprotocol/graph";
 
+export const mustBeDefined = <T>(x: T | undefined, message?: string): T => {
+  if (x === undefined) {
+    throw new Error(`invariant was broken: ${message ?? ""}`);
+  }
+
+  return x;
+};
+
 type TupleEntry<
   T extends readonly unknown[],
   I extends unknown[] = [],
