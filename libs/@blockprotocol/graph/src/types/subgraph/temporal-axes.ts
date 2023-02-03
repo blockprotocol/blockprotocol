5a6b5105a4c2bef14c68c2691de3500770841273
Alfred Mountfield <alfie.mountfield@gmail.com>
2023-02-03 11:40:25 +0000

    Rename `Axis` types

--- libs/@blockprotocol/graph/src/types/subgraph/temporal-axes.ts
+++ libs/@blockprotocol/graph/src/types/subgraph/temporal-axes.ts
@@ -1,59 +1,41 @@
 import {
-  LimitedTemporalBound,
   PinnedTemporalAxis,
-  TemporalBound,
-  Timestamp,
+  PinnedTemporalAxisUnresolved,
   VariableTemporalAxis,
+  VariableTemporalAxisUnresolved,
 } from "../temporal-versioning.js";
 
 /**
  * Defines the two possible combinations of pinned/variable temporal axes that are used in queries that return
  * {@link Subgraph}`s.
  *
- * The {@link VariableTemporalAxis} is optionally bounded, in the absence of provided bounds an inclusive bound at the
- * timestamp at point of resolving is assumed.
+ * The {@link VariableTemporalAxisUnresolved} is optionally bounded, in the absence of provided bounds an inclusive
+ * bound at the timestamp at point of resolving is assumed.
  */
 export type UnresolvedQueryTemporalAxes =
   | {
-      variable: VariableTemporalAxis<
-        "decisionTime",
-        TemporalBound | null,
-        LimitedTemporalBound | null
-      >;
-      pinned: PinnedTemporalAxis<"transactionTime", Timestamp | null>;
+      variable: VariableTemporalAxisUnresolved<"decisionTime">;
+      pinned: PinnedTemporalAxisUnresolved<"transactionTime">;
     }
   | {
-      variable: VariableTemporalAxis<
-        "transactionTime",
-        TemporalBound | null,
-        LimitedTemporalBound | null
-      >;
-      pinned: PinnedTemporalAxis<"decisionTime", Timestamp | null>;
+      variable: VariableTemporalAxisUnresolved<"transactionTime">;
+      pinned: PinnedTemporalAxisUnresolved<"decisionTime">;
     };
 
 /**
  * Defines the two possible combinations of pinned/variable temporal axes that are used in responses to queries that
  * return {@link Subgraph}`s.
  *
- * The {@link VariableTemporalAxis} is bounded according to the input of the query (where absent bounds in the input are
- * interpreted as described in the docs for {@link UnresolvedQueryTemporalAxes})
+ * The {@link VariableTemporalAxis} is bounded according to the input of the query.
  */
 export type ResolvedQueryTemporalAxes =
   | {
-      variable: VariableTemporalAxis<
-        "decisionTime",
-        TemporalBound,
-        LimitedTemporalBound
-      >;
-      pinned: PinnedTemporalAxis<"transactionTime", Timestamp>;
+      variable: VariableTemporalAxisUnresolved<"decisionTime">;
+      pinned: PinnedTemporalAxis<"transactionTime">;
     }
   | {
-      variable: VariableTemporalAxis<
-        "transactionTime",
-        TemporalBound,
-        LimitedTemporalBound
-      >;
-      pinned: PinnedTemporalAxis<"decisionTime", Timestamp>;
+      variable: VariableTemporalAxis<"transactionTime">;
+      pinned: PinnedTemporalAxis<"decisionTime">;
     };
 
 /**
--- libs/@blockprotocol/graph/src/types/temporal-versioning.ts
+++ libs/@blockprotocol/graph/src/types/temporal-versioning.ts
@@ -80,18 +80,36 @@ export type BoundedTimeInterval = TimeInterval<
 >;
 
 /**
- * A representation of a "variable" temporal axis, which is optionally bounded to a given {@link TimeIntervalUnresolved}.
+ * A representation of a "variable" temporal axis, which is optionally bounded to a given interval where some of the
+ * bounds may have been omitted for later processing (see {@link TimeIntervalUnresolved}), whereby `null` values are
+ * replaced with inclusive bounds referring the current {@link Timestamp}.
+ *
+ * In a bitemporal system, a {@link VariableTemporalAxisUnresolved} should almost always be accompanied by a
+ * {@link PinnedTemporalAxisUnresolved}.
+ */
+export type VariableTemporalAxisUnresolved<
+  Axis extends TemporalAxes,
+  StartBound extends TemporalBound | null = TemporalBound | null,
+  EndBound extends LimitedTemporalBound | null = LimitedTemporalBound | null,
+> = {
+  axis: Axis;
+  interval: TimeIntervalUnresolved<StartBound, EndBound>;
+};
+
+/**
+ * A representation of a "variable" temporal axis, which bounded to a given {@link TimeInterval} where the end of the
+ * interval must be limited by a {@link Timestamp}
  *
  * In a bitemporal system, a {@link VariableTemporalAxis} should almost always be accompanied by a
  * {@link PinnedTemporalAxis}.
  */
 export type VariableTemporalAxis<
   Axis extends TemporalAxes,
-  StartBound extends TemporalBound | null,
-  EndBound extends LimitedTemporalBound | null,
+  StartBound extends TemporalBound = TemporalBound,
+  EndBound extends LimitedTemporalBound = LimitedTemporalBound,
 > = {
   axis: Axis;
-  interval: TimeIntervalUnresolved<StartBound, EndBound>;
+  interval: TimeInterval<StartBound, EndBound>;
 };
 
 /**
@@ -99,12 +117,27 @@ export type VariableTemporalAxis<
  * {@link Timestamp}. If the `timestamp` is set to `null`, then it will be filled in with the current time _when a query
  * is being resolved._
  *
+ * In a bitemporal system, a {@link PinnedTemporalAxisUnresolved} should almost always be accompanied by a
+ * {@link VariableTemporalAxisUnresolved}.
+ */
+export type PinnedTemporalAxisUnresolved<
+  Axis extends TemporalAxes,
+  PinnedTime extends Timestamp | null = Timestamp | null,
+> = {
+  axis: Axis;
+  timestamp: PinnedTime;
+};
+
+/**
+ * A representation of a "pinned" temporal axis, used to project another temporal axis along the given
+ * {@link Timestamp}.
+ *
  * In a bitemporal system, a {@link PinnedTemporalAxis} should almost always be accompanied by a
  * {@link VariableTemporalAxis}.
  */
 export type PinnedTemporalAxis<
   Axis extends TemporalAxes,
-  PinnedTime extends Timestamp | null,
+  PinnedTime extends Timestamp = Timestamp,
 > = {
   axis: Axis;
   timestamp: PinnedTime;
