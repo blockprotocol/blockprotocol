4d85e5af18b4f98a303502cbbaf784f7dcfa7256
Ciaran Morinan <37743469+CiaranMn@users.noreply.github.com>
2023-02-09 08:50:49 +0000

    Revert "remove exports paths from core (#959)"
    This reverts commit 8f92311d397746ca5698150b445b4f076f9725d2.

--- libs/@blockprotocol/hook/src/react.ts
+++ libs/@blockprotocol/hook/src/react.ts
@@ -1,4 +1,4 @@
-import { useServiceConstructor } from "@blockprotocol/core/dist/esm/react.js";
+import { useServiceConstructor } from "@blockprotocol/core/react";
 import { EntityId } from "@blockprotocol/graph";
 import { RefObject, useLayoutEffect, useRef, useState } from "react";
 
