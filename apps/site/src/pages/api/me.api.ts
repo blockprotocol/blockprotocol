fbacdf474574ae840630cc13bc6a2c0385d68650
Ciaran Morinan <c@hash.ai>
2023-02-15 20:58:20 +0000

    set cookie domain

--- apps/site/src/pages/api/me.api.ts
+++ apps/site/src/pages/api/me.api.ts
@@ -1,6 +1,7 @@
 import { createBaseHandler } from "../../lib/api/handler/base-handler";
 import { SESSION_COOKIE_NAME } from "../../lib/api/middleware/constants";
 import { SerializedUser } from "../../lib/api/model/user.model";
+import { isProduction } from "../../lib/config";
 
 export type ApiMeResponse = { user: SerializedUser } | { guest: true };
 
@@ -10,7 +11,15 @@ export default createBaseHandler<undefined, ApiMeResponse>().get((req, res) => {
   if (user) {
     res.status(200).json({ user: user.serialize(true) });
   } else {
-    res.setHeader("Set-Cookie", `${SESSION_COOKIE_NAME}=; Max-Age=-1; Path=/`);
+    const cookieDomain = isProduction
+      ? "blockprotocol.org"
+      : process.env.VERCEL
+      ? ".stage.hash.ai"
+      : "localhost";
+    res.setHeader(
+      "Set-Cookie",
+      `${SESSION_COOKIE_NAME}=; Domain=${cookieDomain}; Max-Age=-1; Path=/`,
+    );
 
     res.status(200).json({ guest: true });
   }
