3d320df3278565bbb688bfa8e2e5bf739684342a
Alfred Mountfield <alfie.mountfield@gmail.com>
2023-02-15 18:00:37 +0000

    Move `stdlib.ts` and `types.ts` into `./shared`

--- libs/@blockprotocol/graph/src/shared/stdlib.ts
+++ libs/@blockprotocol/graph/src/shared/stdlib.ts
@@ -1,7 +1,7 @@
 /**
  * The extended standard library of functions for interacting with a {@link Subgraph}.
  */
-export { compareBounds } from "./shared/stdlib/bound.js";
+export { compareBounds } from "./stdlib/bound.js";
 export {
   intervalCompareWithInterval,
   intervalContainsInterval,
@@ -16,45 +16,45 @@ export {
   intervalUnionWithInterval,
   sortIntervals,
   unionOfIntervals,
-} from "./shared/stdlib/interval.js";
-export { buildSubgraph } from "./shared/stdlib/subgraph/builder.js";
-export { getPropertyTypesReferencedByEntityType } from "./shared/stdlib/subgraph/edge/entity-type.js";
+} from "./stdlib/interval.js";
+export { buildSubgraph } from "./stdlib/subgraph/builder.js";
+export { getPropertyTypesReferencedByEntityType } from "./stdlib/subgraph/edge/entity-type.js";
 export {
   getIncomingLinksForEntity,
   getLeftEntityForLinkEntity,
   getOutgoingLinkAndTargetEntities,
   getOutgoingLinksForEntity,
   getRightEntityForLinkEntity,
-} from "./shared/stdlib/subgraph/edge/link-entity.js";
+} from "./stdlib/subgraph/edge/link-entity.js";
 export {
   getDataTypeById,
   getDataTypeByVertexId,
   getDataTypes,
   getDataTypesByBaseUri,
-} from "./shared/stdlib/subgraph/element/data-type.js";
+} from "./stdlib/subgraph/element/data-type.js";
 export {
   getEntities,
   getEntityRevision,
   getEntityRevisionsByEntityId,
-} from "./shared/stdlib/subgraph/element/entity.js";
+} from "./stdlib/subgraph/element/entity.js";
 export {
   getEntityTypeById,
   getEntityTypeByVertexId,
   getEntityTypes,
   getEntityTypesByBaseUri,
-} from "./shared/stdlib/subgraph/element/entity-type.js";
-export { mapElementsIntoRevisions } from "./shared/stdlib/subgraph/element/map-revisions.js";
+} from "./stdlib/subgraph/element/entity-type.js";
+export { mapElementsIntoRevisions } from "./stdlib/subgraph/element/map-revisions.js";
 export {
   getPropertyTypeById,
   getPropertyTypeByVertexId,
   getPropertyTypes,
   getPropertyTypesByBaseUri,
-} from "./shared/stdlib/subgraph/element/property-type.js";
+} from "./stdlib/subgraph/element/property-type.js";
 export {
   getRoots,
   isDataTypeRootedSubgraph,
   isEntityRootedSubgraph,
   isEntityTypeRootedSubgraph,
   isPropertyTypeRootedSubgraph,
-} from "./shared/stdlib/subgraph/roots.js";
-export { getLatestInstantIntervalForSubgraph } from "./shared/stdlib/subgraph/temporal-axes.js";
+} from "./stdlib/subgraph/roots.js";
+export { getLatestInstantIntervalForSubgraph } from "./stdlib/subgraph/temporal-axes.js";
--- libs/@blockprotocol/graph/src/types.ts
+++
@@ -1,7 +0,0 @@
-export * from "./shared/types/block-graph.js";
-export * from "./shared/types/entity.js";
-export * from "./shared/types/file.js";
-export * from "./shared/types/linked-aggregation.js";
-export * from "./shared/types/ontology.js";
-export * from "./shared/types/subgraph.js";
-export * from "./shared/types/temporal-versioning.js";
