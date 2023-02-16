b7455c934aaeb1676cf4b7dd2184b2e22659ca90
Alfred Mountfield <alfie.mountfield@gmail.com>
2023-02-16 11:38:13 +0000

    more wip

--- libs/@blockprotocol/eslint-plugin-custom/src/main.ts
+++ libs/@blockprotocol/eslint-plugin-custom/src/main.ts
@@ -7,26 +7,88 @@ import RuleContext = TSESLint.RuleContext;
 
 const createRule = ESLintUtils.RuleCreator((name) => `https://example.com/rule/${name}`)
 
-function collectExportNames(exportedNames: string[], node: TSESTree.ProgramStatement, context: Readonly<RuleContext<never, never[]>>) {
+function collectExportNames(
+  exportedNames: { kind: string, name: string, node: Partial<TSESTree.Node>, debug?: string }[],
+  node: TSESTree.ProgramStatement,
+  context: Readonly<RuleContext<never, never[]>>
+) {
   if (node.type === AST_NODE_TYPES.ExportDefaultDeclaration) {
-    exportedNames.push('default')
-  } else if (node.type === AST_NODE_TYPES.ExportNamedDeclaration) {
-    node.specifiers.forEach((specifier) => {
-      if (specifier.exported) {
-        exportedNames.push(specifier.exported.name);
-      }
+    exportedNames.push({
+      kind: 'default',
+      name: 'default',
+      node: {type: node.type, exportKind: node.exportKind, declaration: node.declaration}
     });
-    if (node.declaration) {
-      if (node.declaration.type === AST_NODE_TYPES.VariableDeclaration) {
-        node.declaration.declarations.forEach((decl) => {
-          if (decl.id.type === 'Identifier') {
-            exportedNames.push(decl.id.name);
-          }
-        });
-      } else if (node.declaration.type === AST_NODE_TYPES.FunctionDeclaration && node.declaration.id) {
-        exportedNames.push(node.declaration.id.name);
+  } else if (node.type === AST_NODE_TYPES.ExportNamedDeclaration) {
+    const declaration = node.declaration;
+    let keyword = '';
+
+    if (node.exportKind === 'type') {
+      keyword = 'type';
+      if (declaration) {
+        switch (declaration.type) {
+          case AST_NODE_TYPES.TSInterfaceDeclaration:
+            keyword = 'interface';
+            break;
+          case AST_NODE_TYPES.TSTypeAliasDeclaration:
+            keyword = 'type';
+            break;
+          default:
+            break;
+        }
+      }
+    } else if (node.exportKind === 'value') {
+      if (declaration) {
+        switch (declaration.type) {
+          case AST_NODE_TYPES.VariableDeclaration:
+            keyword = declaration.declare ? 'declare const' : 'const';
+            break;
+          case AST_NODE_TYPES.FunctionDeclaration:
+            keyword = declaration.declare ? 'declare function' : 'function';
+            break;
+          case AST_NODE_TYPES.ClassDeclaration:
+            keyword = declaration.declare ? 'declare class' : 'class';
+            break;
+          default:
+            break;
+        }
       }
     }
+    if (declaration && declaration.type === AST_NODE_TYPES.VariableDeclaration) {
+      declaration.declarations.forEach((decl) => {
+        if (decl.id.type === 'Identifier' && decl.id.name !== 'undefined') {
+          exportedNames.push({
+            kind: keyword,
+            name: decl.id.name,
+            node: {declaration: node.declaration, exportKind: node.exportKind, type: node.type},
+            debug: "VariableDeclaration.declarations"
+          });
+        }
+      });
+    } else if (declaration && declaration.type === AST_NODE_TYPES.FunctionDeclaration && declaration.id) {
+      exportedNames.push({
+        kind: keyword, name: declaration.id.name, node: {
+          declaration: node.declaration,
+          exportKind: node.exportKind,
+          type: node.type,
+          source: node.source
+        }, debug: "FunctionDeclaration"
+      });
+    }
+
+    node.specifiers.forEach((specifier) => {
+      if (specifier.exported) {
+        exportedNames.push({
+          kind: keyword,
+          name: specifier.exported.name,
+          node: {
+            declaration: node.declaration,
+            exportKind: node.exportKind,
+            type: node.type,
+          },
+          debug: "ExportNamedDeclaration.specifiers"
+        });
+      }
+    });
   } else if (node.type === AST_NODE_TYPES.ExportAllDeclaration) {
     const modulePath = node.source.value;
     let requirePath = modulePath;
@@ -69,7 +131,7 @@ module.exports = {
       },
       defaultOptions: [],
       create(context) {
-        const exportedNames: string[] = [];
+        const exportedNames: { kind: string, name: string, node: Partial<TSESTree.Node>, debug?: string }[] = [];
         return {
           'Program:exit': (node: TSESTree.Program) => {
             node.body.forEach((node: TSESTree.Node) => {
