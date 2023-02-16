0ebe31f6f394df9c2dfd9535eda1136ea57ce02c
Alfred Mountfield <alfie.mountfield@gmail.com>
2023-02-16 16:03:49 +0000

    Run it on the graph

--- libs/@blockprotocol/graph/.eslintrc.cjs
+++ libs/@blockprotocol/graph/.eslintrc.cjs
@@ -1,4 +1,13 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   ...require("@local/eslint-config/generate-workspace-config.cjs")(__dirname),
+  plugins: ["custom"],
+  overrides: [
+    {
+      "files": ["src/index.ts"],
+      rules: {
+        "custom/enforce-reexport": "error",
+      }
+    },
+  ]
 };
