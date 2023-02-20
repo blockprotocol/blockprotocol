e751b000f1c9fac5b7b2b4cbff6966f0e60c2965
Alfred Mountfield <alfie.mountfield@gmail.com>
2023-02-20 11:40:32 +0000

    Migrate MBD to support temporal and non-temporal versioning

--- libs/mock-block-dock/src/debug-view/dev-tools/datastore-view.tsx
+++ libs/mock-block-dock/src/debug-view/dev-tools/datastore-view.tsx
@@ -1,11 +1,14 @@
 import { Box } from "@mui/material";
 
-import { useMockBlockDockContext } from "../../mock-block-dock-context";
+import {
+  useMockBlockDockNonTemporalContext,
+  useMockBlockDockTemporalContext,
+} from "../../mock-block-dock-context";
 import { DatastoreGraphVisualization } from "./datastore-graph-visualization";
 import { JsonView } from "./json-view";
 
-export const DataStoreView = () => {
-  const { graph } = useMockBlockDockContext();
+const DataStoreViewNonTemporal = () => {
+  const { graph } = useMockBlockDockNonTemporalContext();
   return (
     <Box
       sx={{
@@ -25,7 +28,37 @@ export const DataStoreView = () => {
           edges: graph.edges,
         }}
       />
-      <DatastoreGraphVisualization />
+      <DatastoreGraphVisualization temporal={false} />
     </Box>
   );
+};
+
+const DataStoreViewTemporal = () => {
+  const { graph } = useMockBlockDockTemporalContext();
+  return (
+    <Box
+      sx={{
+        minHeight: "100%",
+        display: "flex",
+        flexDirection: {
+          xs: "column-reverse",
+          lg: "row",
+        },
+      }}
+    >
+      <JsonView
+        collapseKeys={["vertices", "edges"]}
+        rootName="datastore"
+        src={{
+          vertices: graph.vertices,
+          edges: graph.edges,
+        }}
+      />
+      <DatastoreGraphVisualization temporal />
+    </Box>
+  );
+};
+
+export const DataStoreView = ({ temporal }: { temporal: boolean }) => {
+  return temporal ? <DataStoreViewTemporal /> : <DataStoreViewNonTemporal />;
 };
