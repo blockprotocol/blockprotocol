b7e8dd6e6258714ac73212f63bf73756dcfb5168
Alfred Mountfield <alfie.mountfield@gmail.com>
2023-02-21 11:10:46 +0000

    Fix HTML block

--- libs/mock-block-dock/dev/test-html-block/block.js
+++ libs/mock-block-dock/dev/test-html-block/block.js
@@ -3,7 +3,15 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck -- unable to typecheck GraphBlockHandler because of URL import
 
-import { GraphBlockHandler } from "https://esm.sh/@blockprotocol/graph@0.0.11";
+import {
+  extractBaseUri,
+  GraphBlockHandler,
+} from "https://esm.sh/@blockprotocol/graph@0.1.0-canary-20230223103409";
+import { getRoots } from "https://esm.sh/@blockprotocol/graph@0.1.0-canary-20230223103409/stdlib";
+
+const propertyTypeIds = {
+  name: "http://example.com/types/property-type/name/v/1",
+};
 
 const element = blockprotocol.getBlockContainer(import.meta.url);
 
@@ -12,17 +20,24 @@ const paragraph = element.querySelector("[data-paragraph]");
 const input = element.querySelector("[data-input]");
 const readonlyParagraph = element.querySelector("[data-readonly]");
 
-let entityId;
+let entity;
 
 const handler = new GraphBlockHandler({
   element,
   callbacks: {
-    blockEntity: (entity) => {
-      entityId = entity.data.entityId;
+    blockEntitySubgraph: ({ data: entitySubgraph }) => {
+      entity = getRoots(entitySubgraph)[0];
+      if (!entity) {
+        throw new Error(
+          "Couldn't find block entity in the roots of the subgraph",
+        );
+      }
 
-      title.innerText = `Hello, ${entity.data.properties.name}`;
-      paragraph.innerText = `The entityId of this block is ${entity.data.entityId}. Use it to update its data when calling updateEntity`;
-      input.value = entity.data.properties.name;
+      title.innerText = `Hello, ${
+        entity.properties[extractBaseUri(propertyTypeIds.name)]
+      }`;
+      paragraph.innerText = `The entityId of this block is ${entity.metadata.recordId.entityId}. Use it to update its data when calling updateEntity`;
+      input.value = entity.properties[extractBaseUri(propertyTypeIds.name)];
       readonlyParagraph.innerText = input.value;
     },
     readonly: ({ data: readonly }) => {
@@ -40,12 +55,15 @@ const handler = new GraphBlockHandler({
 });
 
 input.addEventListener("change", (event) => {
-  if (entityId) {
+  if (entity) {
     handler
       .updateEntity({
         data: {
-          entityId,
-          properties: { name: event.target.value },
+          entityId: entity.metadata.recordId.entityId,
+          entityTypeId: entity.metadata.entityTypeId,
+          properties: {
+            [extractBaseUri(propertyTypeIds.name)]: event.target.value,
+          },
         },
       })
       .catch((err) => {
