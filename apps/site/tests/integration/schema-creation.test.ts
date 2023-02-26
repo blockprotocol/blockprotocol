5ff4d148ba70a7d26f76f23c1657d0385465f490
Ciaran Morinan <c@hash.ai>
2023-02-26 17:04:14 +0000

    fix test?

--- apps/site/tests/integration/schema-creation.test.ts
+++ apps/site/tests/integration/schema-creation.test.ts
@@ -53,7 +53,7 @@ test("user should be able to create an Entity Type", async ({ page }) => {
 
   await expect(
     page.locator("[data-testid='create-schema-modal']", {
-      hasText: `Invalid entity type: User already has an entity type with id ${existingSchemaWithMetadata.schema.$id}`,
+      hasText: `User already has an entity type with id ${existingSchemaWithMetadata.schema.$id}`,
     }),
   ).toBeVisible();
 
