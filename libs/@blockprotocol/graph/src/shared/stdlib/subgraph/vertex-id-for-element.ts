7f05656e2633ea9c32dcd950926261c4ff2e2059
Ciaran Morinan <37743469+CiaranMn@users.noreply.github.com>
2023-02-27 19:25:14 +0000

    Fix recordId comparison in getVertexIdForRecordId (#1059)
    * fix recordId comparison in getVertexIdForRecordId
    
    * unused import

--- libs/@blockprotocol/graph/src/shared/stdlib/subgraph/vertex-id-for-element.ts
+++ libs/@blockprotocol/graph/src/shared/stdlib/subgraph/vertex-id-for-element.ts
@@ -1,6 +1,8 @@
 import {
   EntityRecordId,
   GraphElementVertexId,
+  isEntityRecordId,
+  isOntologyTypeRecordId,
   OntologyTypeRecordId,
   Subgraph,
 } from "../../types.js";
@@ -20,7 +22,17 @@ export const getVertexIdForRecordId = (
 ): GraphElementVertexId => {
   for (const [baseId, revisionObject] of typedEntries(subgraph.vertices)) {
     for (const [revisionId, vertex] of typedEntries(revisionObject)) {
-      if (vertex.inner.metadata.recordId === recordId) {
+      const { recordId: vertexRecordId } = vertex.inner.metadata;
+      if (
+        (isOntologyTypeRecordId(recordId) &&
+          isOntologyTypeRecordId(vertexRecordId) &&
+          recordId.baseUrl === vertexRecordId.baseUrl &&
+          recordId.version === vertexRecordId.version) ||
+        (isEntityRecordId(recordId) &&
+          isEntityRecordId(vertexRecordId) &&
+          recordId.entityId === vertexRecordId.entityId &&
+          recordId.editionId === vertexRecordId.editionId)
+      ) {
         return {
           baseId,
           revisionId,
