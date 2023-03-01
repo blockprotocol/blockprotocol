47779b65c7d6b28023164cebfb24ff4b1e59969c
David @ HASH <6226576+nonparibus@users.noreply.github.com>
2023-03-01 02:13:43 +0000

    Update header.tsx

--- apps/site/src/components/pages/wordpress/header.tsx
+++ apps/site/src/components/pages/wordpress/header.tsx
@@ -43,7 +43,7 @@ export const Header = () => {
           textAlign="center"
           variant="bpSmallCaps"
         >
-          Early access
+          Available Now
         </Typography>
 
         <Box
