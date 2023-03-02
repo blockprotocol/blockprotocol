ea8787e7e47fc53048428cfc51d4b8faca490623
luisbettencourt <lmn.bettencourt@gmail.com>
2023-03-02 00:11:56 +0000

    Add Inter font to block demo

--- apps/site/src/pages/api/rewrites/sandboxed-block-demo.api.ts
+++ apps/site/src/pages/api/rewrites/sandboxed-block-demo.api.ts
@@ -101,6 +101,10 @@ const handler: NextApiHandler = async (req, res) => {
 <html>
   <head>
     <meta charSet="utf-8" />
+    <link
+    href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap"
+    rel="stylesheet"
+    />
     <script type="module">
       const handleMessage = ({ data }) => {
         if (typeof data !== "string") {
