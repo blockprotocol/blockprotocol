3aea7bfe410668ef34c1ce8c922521d3f563d1e3
Alfred Mountfield <alfie.mountfield@gmail.com>
2023-03-08 14:39:29 +0000

    Delete old `codegen` implementation

--- libs/@blockprotocol/graph/src/shared/codegen/shared.ts
+++
@@ -1,8 +0,0 @@
-import fetch from "node-fetch";
-
-export const fetchTypeAsJson = (versionedUrl: string) =>
-  fetch(versionedUrl, {
-    headers: {
-      accept: "application/json",
-    },
-  }).then((resp) => resp.json());
