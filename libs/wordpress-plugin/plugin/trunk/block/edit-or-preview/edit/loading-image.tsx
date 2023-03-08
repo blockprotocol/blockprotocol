cc428658576b2a7c335f7ed06708daeb515d1b9e
Ciaran Morinan <37743469+CiaranMn@users.noreply.github.com>
2023-03-08 17:17:31 +0000

    Remove inappropriate controls from WP block, set API key input to password, add loading skeleton (#1142)
    * update block supports to remove inappropriate controls. set api key input type 'password'
    
    * replace loading gif with skeleton

--- libs/wordpress-plugin/plugin/trunk/block/edit-or-preview/edit/loading-image.tsx
+++ libs/wordpress-plugin/plugin/trunk/block/edit-or-preview/edit/loading-image.tsx
@@ -1,11 +1,45 @@
-export const LoadingImage = () => {
-  const { plugin_url } = window.block_protocol_data;
+export const LoadingImage = ({ height }: { height: string }) => {
   return (
-    <img
-      alt="Loading"
-      src={`${plugin_url}images/blocks-loading.gif`}
-      height={42}
-      width={42}
-    />
+    <>
+      <style>{`
+        .bp-loading-skeleton {
+          display: block;
+          background-color: rgba(0, 0, 0, 0.06);
+          border-radius: 8;
+          opacity: 0.4;
+          -webkit-animation: bp-loading-pulse 1.5s ease-in-out 0.5s infinite;
+          animation: bp-loading-pulse 1.5s ease-in-out 0.5s infinite;
+        }
+        
+        @-webkit-keyframes bp-loading-pulse {
+          0% {
+              opacity: 0.4;
+          }
+      
+          50% {
+              opacity: 1;
+          }
+      
+          100% {
+              opacity: 0.4;
+          }
+        }
+
+        @keyframes bp-loading-pulse {
+          0% {
+              opacity: 0.4;
+          }
+      
+          50% {
+              opacity: 1;
+          }
+      
+          100% {
+              opacity: 0.4;
+          }
+        }
+      `}</style>
+      <div className="bp-loading-skeleton" style={{ height }} />
+    </>
   );
 };
