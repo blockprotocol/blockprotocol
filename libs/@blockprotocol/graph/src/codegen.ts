7fac8e6f38a5be1aa5519edb2de83e6233bb70de
Alfred Mountfield <alfie.mountfield@gmail.com>
2023-03-08 16:54:04 +0000

    Add codegen entrypoint

---
+++ libs/@blockprotocol/graph/src/codegen.ts
@@ -0,0 +1,10 @@
+/**
+ * Defines utilities for generating TypeScript types from elements of the Block Protocol Type System.
+ */
+import { CodegenParameters } from "./codegen/parameters.js";
+import { LogLevel } from "./codegen/shared.js";
+
+export const codegen = async (
+  params: CodegenParameters,
+  logLevel: LogLevel = "info",
+) => {};
---
+++ libs/@blockprotocol/graph/src/codegen/shared.ts
@@ -0,0 +1,1 @@
+export type LogLevel = "silent" | "warn" | "info" | "debug" | "trace";
