e51719ad59aa59c8ce524c67b3605c539cec191d
Ahmad Sattar <thehabbos007@gmail.com>
2023-03-09 16:53:15 +0100

    Refactor shared functionality for reference filter

--- libs/@blockprotocol/graph/src/shared/stdlib/subgraph/edge/entity-type.ts
+++ libs/@blockprotocol/graph/src/shared/stdlib/subgraph/edge/entity-type.ts
@@ -10,20 +10,18 @@ import {
   OntologyOutwardEdge,
   OntologyTypeRevisionId,
   OntologyTypeVertexId,
+  OutwardEdge,
+  StrictOntologyOutwardEdge,
   Subgraph,
 } from "../../../types/subgraph.js";
 
-/**
- * Gets identifiers for all `PropertyType`s referenced within a given `EntityType` schema by searching for
- * "ConstrainsPropertiesOn" `Edge`s from the respective `Vertex` within a `Subgraph`.
- *
- * @param subgraph {Subgraph} - The `Subgraph` containing the type tree of the `EntityType`
- * @param entityTypeId {OntologyTypeVertexId | VersionedUrl} - The identifier of the `EntityType` to search for
- * @returns {OntologyTypeVertexId[]} - The identifiers of the `PropertyType`s referenced from the `EntityType`
- */
-export const getPropertyTypesReferencedByEntityType = (
+// Helper higher-order function to simplify edge extraction
+const getXTypesReferencedByYType = (
   subgraph: Subgraph<boolean>,
   entityTypeId: OntologyTypeVertexId | VersionedUrl,
+  outwardEdgePredicate: (
+    outwardEdge: OutwardEdge<boolean>,
+  ) => outwardEdge is StrictOntologyOutwardEdge,
 ): OntologyTypeVertexId[] => {
   let baseUrl: BaseUrl;
   let revisionId: OntologyTypeRevisionId;
@@ -45,6 +43,24 @@ export const getPropertyTypesReferencedByEntityType = (
   }
 
   return outwardEdges
-    .filter(isConstrainsPropertiesOnEdge)
+    .filter(outwardEdgePredicate)
     .map((outwardEdge) => outwardEdge.rightEndpoint);
 };
+
+/**
+ * Gets identifiers for all `PropertyType`s referenced within a given `EntityType` schema by searching for
+ * "ConstrainsPropertiesOn" `Edge`s from the respective `Vertex` within a `Subgraph`.
+ *
+ * @param subgraph {Subgraph} - The `Subgraph` containing the type tree of the `EntityType`
+ * @param entityTypeId {OntologyTypeVertexId | VersionedUrl} - The identifier of the `EntityType` to search for
+ * @returns {OntologyTypeVertexId[]} - The identifiers of the `PropertyType`s referenced from the `EntityType`
+ */
+export const getPropertyTypesReferencedByEntityType = (
+  subgraph: Subgraph<boolean>,
+  entityTypeId: OntologyTypeVertexId | VersionedUrl,
+): OntologyTypeVertexId[] =>
+  getXTypesReferencedByYType(
+    subgraph,
+    entityTypeId,
+    isConstrainsPropertiesOnEdge,
+  );
