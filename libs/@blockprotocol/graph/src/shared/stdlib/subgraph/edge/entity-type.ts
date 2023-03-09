d167bfc8418939aaeaa8325b77c934f3838bfe05
Ahmad Sattar <thehabbos007@gmail.com>
2023-03-09 17:05:09 +0100

    Move shared function out of `entity-type`

--- libs/@blockprotocol/graph/src/shared/stdlib/subgraph/edge/entity-type.ts
+++ libs/@blockprotocol/graph/src/shared/stdlib/subgraph/edge/entity-type.ts
@@ -1,52 +1,12 @@
-import {
-  BaseUrl,
-  extractBaseUrl,
-  extractVersion,
-  VersionedUrl,
-} from "@blockprotocol/type-system/slim";
+import { VersionedUrl } from "@blockprotocol/type-system/slim";
 
 import {
   isConstrainsPropertiesOnEdge,
   isInheritsFromEdge,
-  OntologyOutwardEdge,
-  OntologyTypeRevisionId,
   OntologyTypeVertexId,
-  OutwardEdge,
-  StrictOntologyOutwardEdge,
   Subgraph,
 } from "../../../types/subgraph.js";
-
-// Helper higher-order function to simplify edge extraction
-const getXTypesReferencedByYType = (
-  subgraph: Subgraph<boolean>,
-  entityTypeId: OntologyTypeVertexId | VersionedUrl,
-  outwardEdgePredicate: (
-    outwardEdge: OutwardEdge<boolean>,
-  ) => outwardEdge is StrictOntologyOutwardEdge,
-): OntologyTypeVertexId[] => {
-  let baseUrl: BaseUrl;
-  let revisionId: OntologyTypeRevisionId;
-
-  if (typeof entityTypeId === "string") {
-    baseUrl = extractBaseUrl(entityTypeId);
-    revisionId = extractVersion(entityTypeId).toString();
-  } else {
-    baseUrl = entityTypeId.baseId;
-    revisionId = entityTypeId.revisionId;
-  }
-
-  const outwardEdges = subgraph.edges[baseUrl]?.[
-    revisionId
-  ] as OntologyOutwardEdge<boolean>[];
-
-  if (outwardEdges === undefined) {
-    return [];
-  }
-
-  return outwardEdges
-    .filter(outwardEdgePredicate)
-    .map((outwardEdge) => outwardEdge.rightEndpoint);
-};
+import { getXTypesReferencedByYType } from "./shared.js";
 
 /**
  * Gets identifiers for all `PropertyType`s referenced within a given `EntityType` schema by searching for
---
+++ libs/@blockprotocol/graph/src/shared/stdlib/subgraph/edge/shared.ts
@@ -0,0 +1,55 @@
+import {
+  BaseUrl,
+  extractBaseUrl,
+  extractVersion,
+  VersionedUrl,
+} from "@blockprotocol/type-system/slim";
+
+import {
+  OntologyOutwardEdge,
+  OntologyTypeRevisionId,
+  OntologyTypeVertexId,
+  OutwardEdge,
+  StrictOntologyOutwardEdge,
+  Subgraph,
+} from "../../../types/subgraph.js";
+
+/**
+ * This is a helper function to extract ontology edges given an `outwardEdgePredicate` function.
+ *
+ * @internal
+ * @param subgraph {Subgraph} - The `Subgraph` containing the underlying ontology types
+ * @param ontologyTypeId {OntologyTypeVertexId | VersionedUrl} - The identifier of the `EntityType` to search for
+ * @param outwardEdgePredicate {(outwardEdge: OutwardEdge<boolean>) => outwardEdge is StrictOntologyOutwardEdge} - The predicate to filter edges by
+ * @returns {OntologyTypeVertexId[]} - The resulting endpoints of the filtered edges
+ */
+export const getXTypesReferencedByYType = (
+  subgraph: Subgraph<boolean>,
+  ontologyTypeId: OntologyTypeVertexId | VersionedUrl,
+  outwardEdgePredicate: (
+    outwardEdge: OutwardEdge<boolean>,
+  ) => outwardEdge is StrictOntologyOutwardEdge,
+): OntologyTypeVertexId[] => {
+  let baseUrl: BaseUrl;
+  let revisionId: OntologyTypeRevisionId;
+
+  if (typeof ontologyTypeId === "string") {
+    baseUrl = extractBaseUrl(ontologyTypeId);
+    revisionId = extractVersion(ontologyTypeId).toString();
+  } else {
+    baseUrl = ontologyTypeId.baseId;
+    revisionId = ontologyTypeId.revisionId;
+  }
+
+  const outwardEdges = subgraph.edges[baseUrl]?.[
+    revisionId
+  ] as OntologyOutwardEdge<boolean>[];
+
+  if (outwardEdges === undefined) {
+    return [];
+  }
+
+  return outwardEdges
+    .filter(outwardEdgePredicate)
+    .map((outwardEdge) => outwardEdge.rightEndpoint);
+};
