a04180b363f204798bf453d8aad1c4b9d7c61d8b
Alfred Mountfield <alfie.mountfield@gmail.com>
2023-03-13 12:37:28 +0000

    Make processing step filenames consistent

--- libs/@blockprotocol/graph/src/codegen/initialize.ts
+++ libs/@blockprotocol/graph/src/codegen/initialize.ts
@@ -1,6 +1,6 @@
 import { InitializeContext } from "./context/initialize.js";
 import { ensureTargetDirExists } from "./initialize/ensure-target-dir-exists.js";
-import { traverseAndCollateSchemas } from "./initialize/traverse.js";
+import { traverseAndCollateSchemas } from "./initialize/traverse-and-collate-schemas.js";
 
 export const initialize = async (context: InitializeContext) => {
   await ensureTargetDirExists(context);
--- libs/@blockprotocol/graph/src/codegen/preprocess.ts
+++ libs/@blockprotocol/graph/src/codegen/preprocess.ts
@@ -1,7 +1,7 @@
 import { PreprocessContext } from "./context.js";
 import { identifyLinkEntityTypes } from "./preprocess/identify-link-entity-types.js";
 import { removeEmptyAllOfs } from "./preprocess/remove-empty-all-ofs.js";
-import { rewriteTypeTitles } from "./preprocess/transform-type-names.js";
+import { rewriteTypeTitles } from "./preprocess/transform-type-titles.js";
 
 export const preprocess = (context: PreprocessContext) => {
   rewriteTypeTitles(context);
