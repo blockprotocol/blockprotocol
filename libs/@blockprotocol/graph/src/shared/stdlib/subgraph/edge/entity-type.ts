a06fe52456256fe458aeee1f6b71cb5d2a1ecd5d
Ahmad Sattar <thehabbos007@gmail.com>
2023-03-13 18:20:13 +0100

    Refactor return type of `getEntityTypesReferencedByEntityType`

--- libs/@blockprotocol/graph/src/shared/stdlib/subgraph/edge/entity-type.ts
+++ libs/@blockprotocol/graph/src/shared/stdlib/subgraph/edge/entity-type.ts
@@ -1,11 +1,17 @@
-import { VersionedUrl } from "@blockprotocol/type-system/slim";
+import {
+  BaseUrl,
+  extractBaseUrl,
+  extractVersion,
+  VersionedUrl,
+} from "@blockprotocol/type-system/slim";
 
 import {
   isConstrainsLinkDestinationsOnEdge,
   isConstrainsLinksOnEdge,
   isConstrainsPropertiesOnEdge,
   isInheritsFromEdge,
-  OntologyToOntologyOutwardEdge,
+  OntologyOutwardEdge,
+  OntologyTypeRevisionId,
   OntologyTypeVertexId,
   Subgraph,
 } from "../../../types/subgraph.js";
@@ -29,23 +35,57 @@ export const getPropertyTypesReferencedByEntityType = (
     isConstrainsPropertiesOnEdge,
   );
 
+type EntityTypeReferences = {
+  inheritsFrom: OntologyTypeVertexId[];
+  constrainsLinksOn: OntologyTypeVertexId[];
+  constrainsLinkDestinationsOn: OntologyTypeVertexId[];
+};
+
 /**
  * Gets identifiers for all `EntityType`s referenced within a given `EntityType` schema by searching for
  * "InheritsFrom", "ConstrainsLinksOn" and "ConstrainsLinkDestinationsOn" `Edge`s from the respective `Vertex` within a `Subgraph`.
  *
  * @param subgraph {Subgraph} - The `Subgraph` containing the type tree of the `EntityType`
  * @param entityTypeId {OntologyTypeVertexId | VersionedUrl} - The identifier of the `EntityType` to search for
- * @returns {OntologyTypeVertexId[]} - The identifiers of the `EntityType`s referenced from the `EntityType`
+ * @returns {EntityTypeReferences} - The identifiers of the `EntityType`s referenced from the `EntityType` grouped by their edge kind
  */
 export const getEntityTypesReferencedByEntityType = (
   subgraph: Subgraph<boolean>,
   entityTypeId: OntologyTypeVertexId | VersionedUrl,
-): OntologyTypeVertexId[] =>
-  getOntologyEndpointsForOntologyOutwardEdge(
-    subgraph,
-    entityTypeId,
-    (outwardEdge): outwardEdge is OntologyToOntologyOutwardEdge =>
-      isInheritsFromEdge(outwardEdge) ||
-      isConstrainsLinksOnEdge(outwardEdge) ||
-      isConstrainsLinkDestinationsOnEdge(outwardEdge),
-  );
+): EntityTypeReferences => {
+  let baseUrl: BaseUrl;
+  let revisionId: OntologyTypeRevisionId;
+
+  if (typeof entityTypeId === "string") {
+    baseUrl = extractBaseUrl(entityTypeId);
+    revisionId = extractVersion(entityTypeId).toString();
+  } else {
+    baseUrl = entityTypeId.baseId;
+    revisionId = entityTypeId.revisionId;
+  }
+
+  const outwardEdges = subgraph.edges[baseUrl]?.[
+    revisionId
+  ] as OntologyOutwardEdge<boolean>[];
+
+  const ontologyVertexIds = {
+    inheritsFrom: [],
+    constrainsLinksOn: [],
+    constrainsLinkDestinationsOn: [],
+  };
+
+  if (outwardEdges === undefined) {
+    return ontologyVertexIds;
+  }
+
+  return outwardEdges.reduce<EntityTypeReferences>((acc, outwardEdge) => {
+    if (isInheritsFromEdge(outwardEdge)) {
+      acc.inheritsFrom.push(outwardEdge.rightEndpoint);
+    } else if (isConstrainsLinksOnEdge(outwardEdge)) {
+      acc.constrainsLinksOn.push(outwardEdge.rightEndpoint);
+    } else if (isConstrainsLinkDestinationsOnEdge(outwardEdge)) {
+      acc.constrainsLinkDestinationsOn.push(outwardEdge.rightEndpoint);
+    }
+    return acc;
+  }, ontologyVertexIds);
+};
