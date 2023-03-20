61a9e1ba7f81f6d6025fab4cf9d915958a690f1d
gitstart-blockprotocol <blockprotocol@gitstart.com>
2023-03-20 19:32:50 +0000

    fix: Linting

--- libs/@blockprotocol/design-system/.storybook/preview.js
+++ libs/@blockprotocol/design-system/.storybook/preview.js
@@ -1,9 +1,11 @@
-export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
+module.exports = {
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
     },
-  },
+  }
 };
