41f2d2957af333f824fe53beac24979d04c23e11
Yusuf Kinatas <yusufkinatas@gmail.com>
2023-03-21 21:08:05 +0300

    add fade-in to the homepage first element

--- apps/site/src/components/pages/home/header.tsx
+++ apps/site/src/components/pages/home/header.tsx
@@ -8,6 +8,7 @@ import {
 import Image from "next/legacy/image";
 
 import helixBoxes from "../../../../public/assets/new-home/helix-boxes.webp";
+import { FadeInOnViewport } from "../../fade-in-on-viewport";
 import { DESKTOP_NAVBAR_HEIGHT, MOBILE_NAVBAR_HEIGHT } from "../../navbar";
 
 export const Header = () => {
@@ -27,82 +28,84 @@ export const Header = () => {
         position: "relative",
       }}
     >
-      <Container
-        sx={{
-          position: "relative",
-          zIndex: 3,
-          pt: {
-            xs: `calc(128px - ${height}px)`,
-            md: `calc(160px - ${height}px)`,
-          },
-          mb: { xs: 6, md: 10 },
-          maxWidth: { xs: "95%", md: "75%", lg: "60%" },
-        }}
-      >
-        <Typography
+      <FadeInOnViewport>
+        <Container
           sx={{
-            textTransform: "uppercase",
-            color: ({ palette }) => palette.purple[800],
-            mb: 2.5,
-            fontWeight: 500,
-            mx: "auto",
-          }}
-          textAlign="center"
-          variant="bpSmallCaps"
-        >
-          The Block Protocol
-        </Typography>
-        <Typography
-          variant="bpHeading1"
-          textAlign="center"
-          sx={{
-            lineHeight: 1,
-            color: ({ palette }) => palette.gray[90],
-            mb: 2.5,
-            // @todo font-size should match design system
-            fontSize: "clamp(3rem, 5vw, 7rem)",
-            fontStyle: "italic",
-            letterSpacing: "-0.02em",
+            position: "relative",
+            zIndex: 3,
+            pt: {
+              xs: `calc(128px - ${height}px)`,
+              md: `calc(160px - ${height}px)`,
+            },
+            mb: { xs: 6, md: 10 },
+            maxWidth: { xs: "95%", md: "75%", lg: "60%" },
           }}
         >
-          The open standard for {sm ? <br /> : null}
-          block-based apps
-        </Typography>
-        <Typography
-          variant="bpBodyCopy"
-          lineHeight={1.4}
-          color={theme.palette.gray[70]}
-          textAlign="center"
-          sx={{
-            color: ({ palette }) => palette.gray[90],
-            margin: "0 auto",
-            fontSize: { xs: "1.25rem", md: "1.45rem" },
-          }}
-        >
-          The{" "}
-          <strong>
-            <Box
-              component="span"
-              sx={{ color: ({ palette }) => palette.purple[70] }}
-            >
-              Þ
-            </Box>{" "}
-            Block Protocol
-          </strong>{" "}
-          enables applications to make their interfaces infinitely extensible
-          with interoperable components known as blocks
-        </Typography>
-      </Container>
-      <Image
-        layout="responsive"
-        src={helixBoxes}
-        style={{ zIndex: 999 }}
-        /**
-         * not using placeholder="blur" on this image, since it's transparent, making it blur looks weird
-         * `priority` makes sure this image is loaded ASAP, because this is the first image in the homepage
-         */
-        priority
-      />
+          <Typography
+            sx={{
+              textTransform: "uppercase",
+              color: ({ palette }) => palette.purple[800],
+              mb: 2.5,
+              fontWeight: 500,
+              mx: "auto",
+            }}
+            textAlign="center"
+            variant="bpSmallCaps"
+          >
+            The Block Protocol
+          </Typography>
+          <Typography
+            variant="bpHeading1"
+            textAlign="center"
+            sx={{
+              lineHeight: 1,
+              color: ({ palette }) => palette.gray[90],
+              mb: 2.5,
+              // @todo font-size should match design system
+              fontSize: "clamp(3rem, 5vw, 7rem)",
+              fontStyle: "italic",
+              letterSpacing: "-0.02em",
+            }}
+          >
+            The open standard for {sm ? <br /> : null}
+            block-based apps
+          </Typography>
+          <Typography
+            variant="bpBodyCopy"
+            lineHeight={1.4}
+            color={theme.palette.gray[70]}
+            textAlign="center"
+            sx={{
+              color: ({ palette }) => palette.gray[90],
+              margin: "0 auto",
+              fontSize: { xs: "1.25rem", md: "1.45rem" },
+            }}
+          >
+            The{" "}
+            <strong>
+              <Box
+                component="span"
+                sx={{ color: ({ palette }) => palette.purple[70] }}
+              >
+                Þ
+              </Box>{" "}
+              Block Protocol
+            </strong>{" "}
+            enables applications to make their interfaces infinitely extensible
+            with interoperable components known as blocks
+          </Typography>
+        </Container>
+        <Image
+          layout="responsive"
+          src={helixBoxes}
+          style={{ zIndex: 999 }}
+          /**
+           * not using placeholder="blur" on this image, since it's transparent, making it blur looks weird
+           * `priority` makes sure this image is loaded ASAP, because this is the first image in the homepage
+           */
+          priority
+        />
+      </FadeInOnViewport>
     </Box>
   );
 };
