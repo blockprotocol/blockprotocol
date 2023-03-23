3fc435ac6927a3a17ddf0b77a4f09c0aa851e66f
Ciaran Morinan <c@hash.ai>
2023-03-23 18:50:05 +0000

    handle incompatibilites with gutenberg's editor iframe

--- libs/wordpress-plugin/plugin/trunk/block/shared/block-loader/block-renderer/custom-element.tsx
+++ libs/wordpress-plugin/plugin/trunk/block/shared/block-loader/block-renderer/custom-element.tsx
@@ -2,6 +2,8 @@ import { createComponent } from "@lit-labs/react";
 // eslint-disable-next-line unicorn/import-style -- needed to pass to createComponent
 import React, { FunctionComponent, useMemo } from "react";
 
+import { getContainingIframe } from "../get-containing-iframe";
+
 type CustomElementLoaderProps = {
   elementClass: typeof HTMLElement;
   properties: Record<string, unknown>;
@@ -48,6 +50,32 @@ export const CustomElementLoader: FunctionComponent<
     () => createComponent({ react: React, tagName, elementClass }),
     [elementClass, tagName],
   );
+
+  const iframeContainingBlock = getContainingIframe();
+  if (iframeContainingBlock) {
+    /**
+     * If the block editor is iFramed, blocks are portalled into the iFrame – see docs on {@link getContainingIframe}
+     * Code is evaluated in one window and rendered into another, leading to some mismatches between globals
+     * One such mismatch is on the custom elements registry – 'customElements' below will refer to the top window registry,
+     * but the element needs to be available in the iFrame's own custom element registry (iframeContainingBlock.contentWindow.customElements)
+     *
+     * Current difficulties:
+     * 1. If the element is registered only in the top window's registry, the tag appears in the iFrame's DOM but with no content
+     * 2. If the element is registered only in the iFrame's registry, there is an "Illegal constructor" error (i.e. the element is not registered where it needs to be)
+     * 3. If the element is registered in both registries, this error: "Failed to construct 'CustomElement': The result must be in the same document"
+     *   -- this error possibly related to https://github.com/WICG/webcomponents/issues/907
+     *
+     * @todo see if there's a way to have the custom element registered and loaded in the iFrame without issue
+     *   this may partly involve overwriting 'customElements' in load-remote-block in the same way we do for 'document'
+     */
+    return (
+      <p style={{ border: "1px solid red", padding: 20 }}>
+        This block cannot be loaded in the editor, probably due to
+        incompatibilities with the Gutenberg plugin. It should still display
+        correctly in the final page.
+      </p>
+    );
+  }
 
   return <CustomElement {...properties} />;
 };
--- libs/wordpress-plugin/plugin/trunk/block/shared/block-loader/load-remote-block.tsx
+++ libs/wordpress-plugin/plugin/trunk/block/shared/block-loader/load-remote-block.tsx
@@ -1,6 +1,7 @@
 import { ReactElement } from "react";
 
 import { memoizeFetchFunction } from "../util";
+import { getContainingIframe } from "./get-containing-iframe";
 
 export type UnknownBlock =
   | string
@@ -59,9 +60,25 @@ export const parseBlockSource = (
    */
   const exports: Record<string, UnknownBlock> = {};
   const module = { exports };
+
+  /**
+   * If the block has been portalled into an iFrame, we need to overwrite its document, because:
+   * - within a block, 'document' will refer to the _parent_ document, not the document of the iFrame it is rendered in
+   * - styling libraries which work by appending styles to document.head are going to add styles to the parent document
+   *   - e.g. webpack style-loader (for imported CSS files), or emotion
+   * - styles are therefore not present in the iFrame
+   *
+   * We therefore check if the Gutenberg iFrame is present, and if so overwrite the value of 'document' to the iFrame's document.
+   * Styles still fail to load on some blocks, e.g. TLDraw's styles in the 'drawing' block
+   * @todo investigate how TLDraw's styles are loaded and make them load
+   */
+  const iframe = getContainingIframe();
+  const documentBlockRenderedInside = iframe?.contentDocument ?? document;
+
   // eslint-disable-next-line no-new-func
-  const func = new Function("require", "module", "exports", source);
-  func(requires, module, exports);
+  const func = new Function("require", "module", "exports", "document", source);
+
+  func(requires, module, exports, documentBlockRenderedInside);
 
   const exported =
     module.exports.default ??
