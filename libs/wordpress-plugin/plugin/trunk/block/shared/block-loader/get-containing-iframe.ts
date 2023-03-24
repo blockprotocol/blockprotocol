20e13ef1768f76f02bb8f3a28ee6e427508c1ae6
Ciaran Morinan <c@hash.ai>
2023-03-24 10:28:19 +0000

    tweak another comment

--- libs/wordpress-plugin/plugin/trunk/block/shared/block-loader/get-containing-iframe.ts
+++ libs/wordpress-plugin/plugin/trunk/block/shared/block-loader/get-containing-iframe.ts
@@ -2,6 +2,9 @@
  * The Gutenberg plugin loads blocks in the editor inside an iFrame. Blocks are rendered via a React portal.
  * This function returns the iFrame element, if the plugin is active.
  *
+ * The iFrame is likely to be introduced to WP Core at some point, but the stated plan (as of March 2023)
+ * is to only use it when all blocks are API v3+.
+ *
  * Note: the current check depends on 'editor' appearing in the iFrame's title, and is therefore fragile.
  *
  * The Pull Request explaining that WP core will only iFrame editors where all blocks are API v3+
