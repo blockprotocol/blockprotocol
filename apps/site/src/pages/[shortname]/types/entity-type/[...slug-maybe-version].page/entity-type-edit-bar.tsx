0b033b9a0836e35d3af5f4b4e4bbc7c24c961c80
luisbettencourt <37453800+luisbettencourt@users.noreply.github.com>
2023-03-30 14:28:50 +0100

    Add description editing to the entity type editor (#1198)
    * fix scrolling to #plans and add skeletons to prices
    
    * WIP
    
    * Use react hoof form for setting the value
    
    * add animation to the edit bar
    
    * update package version
    
    * replace RHF useController with register
    
    ---------
    
    Co-authored-by: David @ HASH <6226576+nonparibus@users.noreply.github.com>

--- apps/site/src/pages/[shortname]/types/entity-type/[...slug-maybe-version].page/entity-type-edit-bar.tsx
+++ apps/site/src/pages/[shortname]/types/entity-type/[...slug-maybe-version].page/entity-type-edit-bar.tsx
@@ -2,7 +2,7 @@ import {
   EntityTypeEditorFormData,
   useEntityTypeFormState,
 } from "@hashintel/type-editor";
-import { Box, Stack, Typography } from "@mui/material";
+import { Box, Collapse, Stack, Typography } from "@mui/material";
 import { Container } from "@mui/system";
 import { useRouter } from "next/router";
 import { useState } from "react";
@@ -39,63 +39,61 @@ export const EntityTypeEditBar = ({
 
   const frozenSubmitting = useFrozenValue(isSubmitting);
 
-  if (!isDirty && !isDraft) {
-    return null;
-  }
-
   return (
-    <Box
-      sx={({ palette, zIndex, shadows }) => ({
-        backgroundColor: palette.purple[60],
-        color: "white",
-        position: "sticky",
-        top: "var(--navbar-height)",
-        zIndex: zIndex.drawer + 2,
-        boxShadow: shadows[1],
-      })}
-    >
-      <Container>
-        <Box
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            py: "18px",
-          }}
-        >
-          <PencilSimpleLineIcon sx={{ height: 16, width: 16 }} />
-          <Typography sx={{ ml: 1, color: "white" }}>
-            <Box component="span" sx={{ fontWeight: "bold" }}>
-              Currently editing
-            </Box>{" "}
-            {isDraft ? (
-              <>– this type has not yet been created</>
-            ) : (
-              `Version ${frozenVersion} -> ${frozenVersion + 1}`
-            )}
-          </Typography>
-          <Stack spacing={1.25} sx={{ marginLeft: "auto" }} direction="row">
-            <Button
-              disabled={frozenSubmitting}
-              type="button"
-              variant="secondary"
-              size="small"
-              {...(isDraft
-                ? { href: `/${router.query.shortname}/all-types` }
-                : { onClick: () => reset() })}
-            >
-              Discard changes
-            </Button>
-            <Button
-              disabled={frozenSubmitting}
-              variant="primary"
-              size="small"
-              type="submit"
-            >
-              {isDraft ? <>Create</> : <>Publish update</>}
-            </Button>
-          </Stack>
-        </Box>
-      </Container>
-    </Box>
+    <Collapse in={isDirty || isDraft}>
+      <Box
+        sx={({ palette, zIndex, shadows }) => ({
+          backgroundColor: palette.purple[60],
+          color: "white",
+          position: "sticky",
+          top: "var(--navbar-height)",
+          zIndex: zIndex.drawer + 2,
+          boxShadow: shadows[1],
+        })}
+      >
+        <Container>
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              py: "18px",
+            }}
+          >
+            <PencilSimpleLineIcon sx={{ height: 16, width: 16 }} />
+            <Typography sx={{ ml: 1, color: "white" }}>
+              <Box component="span" sx={{ fontWeight: "bold" }}>
+                Currently editing
+              </Box>{" "}
+              {isDraft ? (
+                <>– this type has not yet been created</>
+              ) : (
+                `Version ${frozenVersion} -> ${frozenVersion + 1}`
+              )}
+            </Typography>
+            <Stack spacing={1.25} sx={{ marginLeft: "auto" }} direction="row">
+              <Button
+                disabled={frozenSubmitting}
+                type="button"
+                variant="secondary"
+                size="small"
+                {...(isDraft
+                  ? { href: `/${router.query.shortname}/all-types` }
+                  : { onClick: () => reset() })}
+              >
+                Discard changes
+              </Button>
+              <Button
+                disabled={frozenSubmitting}
+                variant="primary"
+                size="small"
+                type="submit"
+              >
+                {isDraft ? <>Create</> : <>Publish update</>}
+              </Button>
+            </Stack>
+          </Box>
+        </Container>
+      </Box>
+    </Collapse>
   );
 };
