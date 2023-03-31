b8c7fdf77c0129da0ac0d43ab7f15ae6a0883193
Ahmad Sattar <thehabbos007@gmail.com>
2023-03-31 15:25:25 +0200

    Enable creating multiple API keys (#1239)
    * Allow creating multiple API keys
    
    * UI to create multiple API keys
    
    * Revert "UI to create multiple API keys"
    
    This reverts commit cad3b9605e4c42cb398fe8aeb19f978a04888d34.
    
    * Add API key revoke endpoint
    
    * Use revoke endpoint in UI temporarily
    
    * Simplify model class indirection
    
    * Add API key update name endpoint
    
    * Use timing safe equality for key check
    
    * Update README with new API routes
    
    * Typo
    
    * Fix copy-paste mistake
    
    * Ensure user is signed up for API key CRUD
    
    * Allow renaming revoked keys
    
    * Change Revoke/Update response to "SUCCESS"

--- apps/site/src/pages/api/me/api-keys.api.ts
+++ apps/site/src/pages/api/me/api-keys.api.ts
@@ -5,12 +5,12 @@ export type ApiKeysResponse = {
   apiKeysMetadata: UserFacingApiKeyProperties[];
 };
 
-export default createAuthenticatedHandler<undefined, ApiKeysResponse>(
-  false,
-).get(async (req, res) => {
-  const { db, user } = req;
+export default createAuthenticatedHandler<undefined, ApiKeysResponse>().get(
+  async (req, res) => {
+    const { db, user } = req;
 
-  const apiKeysMetadata = await user.apiKeys(db);
+    const apiKeysMetadata = await user.apiKeys(db);
 
-  res.status(200).json({ apiKeysMetadata });
-});
+    res.status(200).json({ apiKeysMetadata });
+  },
+);
