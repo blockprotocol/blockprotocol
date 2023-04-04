6c1902706f09de348334a71429b03a29e0114d33
Nate Higgins <nate@nateh.uk>
2023-04-04 12:53:18 +0100

    Add copy

--- libs/wordpress-plugin/plugin/trunk/settings/react-promo.tsx
+++ libs/wordpress-plugin/plugin/trunk/settings/react-promo.tsx
@@ -28,6 +28,8 @@ export const PromoBigTitle = ({ children }: PropsWithChildren) => {
         fontSize: 42,
         lineHeight: 1,
         letterSpacing: "-0.03em",
+        marginBottom: 20,
+        marginTop: 0,
       }}
     >
       {children}
@@ -57,6 +59,8 @@ export const PromoSmallTitle = ({ children }: PropsWithChildren) => {
         letterSpacing: "0.05em",
         textTransform: "uppercase",
         color: "#0E1114",
+        marginBottom: 4,
+        marginTop: 0,
       }}
     >
       {children}
@@ -65,27 +69,20 @@ export const PromoSmallTitle = ({ children }: PropsWithChildren) => {
 };
 
 const PromoList = ({ children }: PropsWithChildren) => {
-  return <ul className={styles.promoList}>{children}</ul>;
+  return <ul className={styles.PromoList}>{children}</ul>;
 };
 
 const PromoListItem = ({ children }: PropsWithChildren) => {
   return (
-    <li
-      style={{
-        fontFamily: "'Inter'",
-        fontStyle: "normal",
-        fontWeight: 400,
-        fontSize: 15,
-        lineHeight: 1.4,
-        color: "black",
-      }}
-    >
+    <li className={styles.PromoListItem}>
       <FontAwesomeIcon
         icon={faArrowRight}
         style={{
           color: "#91A5BA",
           fontSize: 16,
           marginRight: 14,
+          position: "relative",
+          top: 2,
         }}
       />
       {children}
@@ -95,8 +92,8 @@ const PromoListItem = ({ children }: PropsWithChildren) => {
 
 export const ReactPromo = () => {
   return (
-    <div>
-      <h2>You can look forward to...</h2>
+    <div className={styles.PromoStack}>
+      <h2 className={styles.PromoHeading}>You can look forward to...</h2>
       <PromoCard>
         <PromoSmallTitle>Next Generation</PromoSmallTitle>
         <PromoBigTitle>
@@ -104,28 +101,101 @@ export const ReactPromo = () => {
         </PromoBigTitle>
         <PromoList>
           <PromoListItem>
-            The best AI text generation, style, tone, grammar and editing blocks
-            for WordPress
+            The best <strong>AI text</strong> generation, style, tone, grammar
+            and editing blocks for WordPress
           </PromoListItem>
           <PromoListItem>
-            Powerful AI image generation blocks let you illustrate posts in
-            seconds
+            Powerful <strong>AI image generation</strong> blocks let you
+            illustrate posts in seconds
           </PromoListItem>
           <PromoListItem>
-            Advanced AI image editing blocks let you remove watermarks from
-            photos, or remove/replace the backgrounds of product photos in
-            WooCommerce
+            Advanced <strong>AI image editing</strong> blocks let you remove
+            watermarks from photos, or remove/replace the backgrounds of product
+            photos in WooCommerce
           </PromoListItem>
           <PromoListItem>
-            Best-in-class AI chat block lets you persist chats, publish them on
-            your website, fork prior conversations with history intact, set the
-            AI’s response style and more
+            Best-in-class <strong>AI chat</strong> block lets you persist chats,
+            publish them on your website, fork prior conversations with history
+            intact, set the AI’s response style and more
           </PromoListItem>
           <PromoListItem>
             Switch between AI models and providers at will
           </PromoListItem>
           <PromoListItem>
-            Free credits for all models and providers
+            <strong>
+              <PromoPurpleText>Free credits</PromoPurpleText> for all models and
+              providers
+            </strong>
+          </PromoListItem>
+        </PromoList>
+      </PromoCard>
+      <PromoCard>
+        <PromoSmallTitle>Level up your SEO</PromoSmallTitle>
+        <PromoBigTitle>
+          <PromoPurpleText>Structured data</PromoPurpleText> in WordPress
+        </PromoBigTitle>
+        <PromoList>
+          <PromoListItem>
+            Turn WordPress into a powerful entity graph and unlock a whole range
+            of new functionality. Go beyond Custom Post Types and Advanced
+            Custom Fields with a graph of entities, all with their own
+            properties and connected by links
+          </PromoListItem>
+          <PromoListItem>
+            Improve your search rankings by leveraging structured data (SEO)
+            blocks that take context from their surroundings and dynamically
+            generating whole-page JSON-LD based on related information (e.g.
+            offers in WooCommerce, or tickets for an event)
+          </PromoListItem>
+          <PromoListItem>
+            Use WordPress as an app builder, not just as a site editor and CMS
+          </PromoListItem>
+        </PromoList>
+      </PromoCard>
+      <PromoCard>
+        <PromoSmallTitle>Supercharge Your Website</PromoSmallTitle>
+        <PromoBigTitle>
+          <PromoPurpleText>Powerful API services</PromoPurpleText> without fuss
+        </PromoBigTitle>
+        <PromoList>
+          <PromoListItem>
+            Access OpenAI, Mapbox, and other powerful APIs directly within
+            WordPress, with zero extra steps
+          </PromoListItem>
+          <PromoListItem>
+            No need to share card details, obtain your own API key, or even set
+            up an account with any provider directly. Everything is handled by
+            the Block Protocol.
+          </PromoListItem>
+          <PromoListItem>
+            Generous free allowances with all providers let you try out blocks
+            and use them in WordPress
+          </PromoListItem>
+          <PromoListItem>
+            Sync data from external apps into WordPress through authenticated
+            external service blocks.
+          </PromoListItem>
+        </PromoList>
+      </PromoCard>
+      <PromoCard>
+        <PromoSmallTitle>Tons more benefits</PromoSmallTitle>
+        <PromoBigTitle>
+          <PromoPurpleText>Instant access</PromoPurpleText> to an infinite
+          ecosystem
+        </PromoBigTitle>
+        <PromoList>
+          <PromoListItem>
+            The Block Protocol is an open-source ecosystem of blocks that
+            anybody can contribute to
+          </PromoListItem>
+          <PromoListItem>
+            New blocks are instantly available within WordPress to Block
+            Protocol users without having to update the plugin, or install
+            additional plugins (as is required for Gutenberg blocks)
+          </PromoListItem>
+          <PromoListItem>
+            Use Block Protocol blocks in any Þ-enabled environment, not just
+            WordPress
           </PromoListItem>
         </PromoList>
       </PromoCard>
