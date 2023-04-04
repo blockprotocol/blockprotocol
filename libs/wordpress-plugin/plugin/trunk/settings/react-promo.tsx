25fec99dfe65d7b263551d34bcd87db1704892fe
Nate Higgins <nate@nateh.uk>
2023-04-04 10:04:59 +0100

    Lint

---
+++ libs/wordpress-plugin/plugin/trunk/settings/react-promo.tsx
@@ -0,0 +1,3 @@
+export const ReactPromo = () => {
+  return <p>Test</p>;
+};
--- libs/wordpress-plugin/plugin/trunk/settings/settings.tsx
+++ libs/wordpress-plugin/plugin/trunk/settings/settings.tsx
@@ -1,8 +1,9 @@
 import { createRoot } from "react-dom/client";
+import { ReactPromo } from "./react-promo";
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("blockprotocol-settings-react-promo");
   if (root) {
-    createRoot(root).render(<p>@todo React promo (from JSX)</p>);
+    createRoot(root).render(<ReactPromo />);
   }
 });
