43a792065292c6b85948c6e59f3aba9aefecfadd
Nate Higgins <nate@nateh.uk>
2023-04-04 10:01:02 +0100

    Fix

--- libs/wordpress-plugin/plugin/trunk/settings/settings.tsx
+++ libs/wordpress-plugin/plugin/trunk/settings/settings.tsx
@@ -3,6 +3,6 @@ import { createRoot } from "react-dom/client";
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("blockprotocol-settings-react-promo");
   if (root) {
-    createRoot(root).render(<p>@todo React promo (from JSX)</p>, root);
+    createRoot(root).render(<p>@todo React promo (from JSX)</p>);
   }
 });
