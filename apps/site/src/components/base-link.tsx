a051c9b3bbdffd9fd88c42086f7867e910b032e3
Ciaran Morinan <c@hash.ai>
2023-04-13 08:50:36 +0100

    update 'is href external' check

--- apps/site/src/components/base-link.tsx
+++ apps/site/src/components/base-link.tsx
@@ -15,7 +15,7 @@ import { FRONTEND_URL } from "../lib/config";
 export const isHrefExternal = (href: string | UrlObject) =>
   typeof href === "string" &&
   (href.endsWith("/discord") ||
-    href.endsWith(".pdf") ||
+    /legal\/terms\/dpa\/attachment/.test(href) ||
     (!/^(mailto:|#|\/|https:\/\/blockprotocol\.org)/.test(href) &&
       !href.startsWith(FRONTEND_URL)));
 
