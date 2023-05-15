538d0b9e4e26c1a1a2f66e2602a57908c8decc60
Tim Diekmann <post@timdiekmann.de>
2023-05-15 15:27:14 +0200

    Remove `cargo-make` install script

--- apps/site/vercel.install.sh
+++ apps/site/vercel.install.sh
@@ -9,7 +9,6 @@ pushd "$(dirname "$0")/../.."
 curl --proto '=https' --tlsv1.2 -sSf https://just.systems/install.sh | sh -s -- --to /usr/local/bin
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --default-toolchain nightly-2022-10-28
 source "$HOME/.cargo/env"
-cargo install cargo-make
 
 # Change back to the project directory
 popd
