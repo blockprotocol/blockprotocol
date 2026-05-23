db3a2356ab42a58fa897e52dc4e5a5dca9648501
vilkinsons <6226576+vilkinsons@users.noreply.github.com>
2026-05-23 16:27:16 +0200

    fix linting

---
+++ libs/block-template-react/src/types/generated/block-entity.ts
@@ -0,0 +1,32 @@
+/**
+ * This file was automatically generated – do not edit it.
+ */
+
+import { Entity } from "@blockprotocol/graph";
+
+export type BlockEntity = Thing;
+
+export type BlockEntityOutgoingLinkAndTarget = ThingOutgoingLinkAndTarget;
+
+/**
+ * A word or set of words by which something is known, addressed, or referred to.
+ */
+export type NamePropertyValue = TextDataType;
+
+/**
+ * An ordered sequence of characters
+ */
+export type TextDataType = string;
+
+export type Thing = Entity<ThingProperties>;
+
+export type ThingOutgoingLinkAndTarget = never;
+
+export type ThingOutgoingLinksByLinkEntityTypeId = {};
+
+/**
+ * A generic thing
+ */
+export type ThingProperties = {
+  "https://blockprotocol.org/@blockprotocol/types/property-type/name/": NamePropertyValue;
+};
